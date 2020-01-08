47214a701cca5a329c425a90bb3fc2743b916904
Xin Wang <wangxin00163@yahoo.co.uk>
2020-01-08 14:49:01 +0000

    Feat/PPDSC-815: Integrate Applitools-React-Storybook plugin to test the components (#361)
    * feat: add applitools config and package for visual testing
    
    * fix: delete package-lock.json
    
    * fix: change to colon symbol
    
    Co-authored-by: Joon Park <parkjh@users.noreply.github.com>
    Co-authored-by: mdcruz <mdcruz@users.noreply.github.com>

---
+++ applitools.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  appName: 'DS Components',
+  showLogs: true,
+  batchName: 'DS Components',
+  storybookUrl: 'http://localhost:6006',
+  browser: [
+    {deviceName: 'iPhone 5/SE'},
+    {deviceName: 'iPhone 6/7/8 Plus'},
+    {deviceName: 'iPhone X'},
+    {deviceName: 'iPad'},
+    {deviceName: 'iPad Pro', screenOrientation: 'landscape'},
+    {deviceName: 'Pixel 2'},
+    {deviceName: 'Pixel 2 XL'},
+    {width: 800, height: 600, name: 'firefox'},
+    {width: 2880, height: 1800, name: 'chrome'},
+    {width: 1024, height: 768, name: 'ie11'},
+    {width: 800, height: 600, name: 'edge'},
+  ],
+  concurrency: 100,
+};
