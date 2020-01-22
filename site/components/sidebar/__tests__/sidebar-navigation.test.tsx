76083e244f7f38bbdbe00ac32cdb8169ae5d8d71
Lewis Haggar <lhaggar@users.noreply.github.com>
2020-01-22 15:47:42 +0000

    fix: mock out sidebar routes (#391)

--- site/components/sidebar/__tests__/sidebar-navigation.test.tsx
+++ site/components/sidebar/__tests__/sidebar-navigation.test.tsx
@@ -5,60 +5,106 @@ import {
 import {fireEvent} from '@testing-library/react';
 import {SidebarNav} from '..';
 
+jest.mock('../../../routes.json', () => [
+  {
+    title: 'Group 1',
+    id: '/group1',
+    subNav: [
+      {
+        title: 'Group 1 Page 1',
+        page: true,
+        id: '/group1/page1',
+      },
+    ],
+  },
+  {
+    title: 'Group 2',
+    id: '/group2',
+    subNav: [
+      {
+        title: 'Group 2 Page 1',
+        page: true,
+        id: '/group2/page1',
+      },
+      {
+        title: 'Group 2 Page 2',
+        page: true,
+        id: '/group2/page2',
+      },
+      {
+        title: 'Group 2b',
+        id: '/group2/group2b',
+        subNav: [
+          {
+            title: 'Group 2b Page 1',
+            page: true,
+            id: '/group2/group2b/page1',
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 describe('Sidebar navigation', () => {
   describe('on route level', () => {
     test('renders as expected', () => {
       const fragment = renderToFragmentWithTheme(SidebarNav, {path: '/'});
       expect(fragment).toMatchSnapshot();
     });
+
     test('renders correctly without given path', () => {
       const fragment = renderToFragmentWithTheme(SidebarNav);
       expect(fragment).toMatchSnapshot();
     });
   });
-  describe('on theming page', () => {
+
+  describe('on group 1 page', () => {
     test('renders as expected', () => {
       const fragment = renderToFragmentWithTheme(SidebarNav, {
-        path: '/guides/theming',
+        path: '/group1/page1',
       });
       expect(fragment).toMatchSnapshot();
     });
-    test('after click guides menu collapses', () => {
+
+    test('after click group1 menu collapses', () => {
       const {getByTestId, asFragment} = renderWithTheme(SidebarNav, {
-        path: '/guides/theming',
+        path: '/group1/page1',
       });
-      const guides = getByTestId('/guides');
-      fireEvent.click(guides as Element);
+      const group1 = getByTestId('/group1');
+      fireEvent.click(group1 as Element);
       expect(asFragment()).toMatchSnapshot();
     });
-    test('after enter key guides menu collapses', () => {
+
+    test('after enter key group1 menu collapses', () => {
       const {getByTestId, asFragment} = renderWithTheme(SidebarNav, {
-        path: '/guides/theming',
+        path: '/group1/page1',
       });
-      const guides = getByTestId('/guides');
-      fireEvent.focus(guides);
-      fireEvent.keyDown(guides, {
+      const group1 = getByTestId('/group1');
+      fireEvent.focus(group1);
+      fireEvent.keyDown(group1, {
         key: 'Enter',
         keyCode: 13,
       });
       expect(asFragment()).toMatchSnapshot();
     });
-    test('by clicking on components menu, guides menu collapses and components menu expands', () => {
+
+    test('by clicking on components menu, group1 menu collapses and group2 menu expands', () => {
       const {getByTestId, asFragment} = renderWithTheme(SidebarNav, {
-        path: '/guides/theming',
+        path: '/group1/page1',
       });
-      const components = getByTestId('/components');
-      fireEvent.click(components as Element);
+      const group2 = getByTestId('/group2');
+      fireEvent.click(group2 as Element);
       expect(asFragment()).toMatchSnapshot();
     });
   });
 
-  test('collapses successfully at 2nd level', () => {
+  test('collapses successfully at nested level', () => {
     const {getByTestId, asFragment} = renderWithTheme(SidebarNav, {
-      path: '/utils/code-helpers/css',
+      path: '/group2/group2b/page1',
     });
-    const elementsMenu = getByTestId('/utils/code-helpers');
-    fireEvent.click(elementsMenu as Element);
+    const nestedMenu = getByTestId('/group2/group2b');
+    fireEvent.click(nestedMenu as Element);
     expect(asFragment()).toMatchSnapshot();
   });
 });
