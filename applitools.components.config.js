44e6ae1dcb13c4926c11251266724ee4226e9c65
Joon Park <parkjh@users.noreply.github.com>
2020-03-11 14:57:22 +0000

    fix: ppdsc 941 applitools viewport updates (#455)

--- applitools.components.config.js
+++ applitools.components.config.js
@@ -4,13 +4,10 @@ module.exports = {
   storybookUrl: 'http://localhost:6006',
   browser: [
     {deviceName: 'iPhone 5/SE'},
-    {deviceName: 'iPhone X'},
-    {deviceName: 'iPad Pro', screenOrientation: 'landscape'},
-    {deviceName: 'Pixel 2'},
-    {width: 800, height: 600, name: 'firefox'},
-    {width: 2880, height: 1800, name: 'chrome'},
+    {width: 1024, height: 768, name: 'firefox'},
     {width: 1024, height: 768, name: 'ie11'},
-    {width: 800, height: 600, name: 'edge'},
+    {width: 1024, height: 768, name: 'chrome'},
+    {width: 1024, height: 768, name: 'safari'},
   ],
   concurrency: 100,
 };
