f9636dc6e182cf5d4d255847f8ad25f8d17b6341
Marco Vanali <Marco91Vanali@gmail.com>
2020-04-01 11:32:48 +0100

    Feat/956 Add amp validator test for amp consent (#498)
    * feat: ppdsc 956 add test for amp consent 4-8
    
    * feat: ppdsc 956 tested consent and passing
    
    * feat: ppdsc 956 tested consent and passing
    
    * feat: ppdsc 956 tested consent and passing

--- src/amp/consent/__tests__/consent.test.tsx
+++ src/amp/consent/__tests__/consent.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import amphtmlValidator from 'amphtml-validator';
 import {renderToFragment} from '../../../test/test-utils';
 import {Consent} from '../consent';
+import {Body} from '../../body';
+import {Head} from '../../head';
+import {renderToStaticMarkup} from '../../utils/render';
+import {createAmpScriptsObject} from '../../scripts/connect-amp-script';
 
 const sourcePointConfigBase = {
   accountId: 'test-accountid',
@@ -55,6 +60,39 @@ describe('Consent renders', () => {
         <Consent sourcePointConfig={extendedConfig} />,
       );
       expect(fragment).toMatchSnapshot();
+    });
+
+    it('should be a valid AMP component', () => {
+      const ampScriptsObject = createAmpScriptsObject();
+
+      const body = (
+        // @ts-ignore
+        <Body theme={{}} ampScriptsObject={ampScriptsObject}>
+          <Consent
+            sourcePointConfig={{
+              accountId: '259',
+              siteName: 'newskit.dev-news.co.uk',
+              privacyManagerId: '5e7b730dc905f8332c044b5e',
+            }}
+          />
+        </Body>
+      );
+
+      const head = (
+        <Head
+          title="AMP Consent"
+          canonical="http://example.com/my-amp-page/"
+          ampScriptsObject={ampScriptsObject}
+        >
+          <meta name="amp-consent-blocking" content="amp-ad" />
+        </Head>
+      );
+      const html = renderToStaticMarkup(body, head);
+
+      amphtmlValidator.getInstance().then(validator => {
+        const result = validator.validateString(html);
+        expect(result.status).toEqual('PASS');
+      });
     });
   });
 
