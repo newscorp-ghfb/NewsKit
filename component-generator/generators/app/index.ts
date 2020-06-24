e6d9bce1b1ec209f364ef8c7ce47ddef5752ca25
Gdrake <gdrake1488@gmail.com>
2020-06-24 16:25:59 +0100

    feat(PPDSC-1187): reading from ast tree

--- component-generator/generators/app/index.ts
+++ component-generator/generators/app/index.ts
@@ -1,4 +1,8 @@
 const Generator = require('yeoman-generator');
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
+const t = require('babel-types');
+const generate = require('babel-generator').default;
 
 module.exports = class extends Generator {
 
@@ -17,11 +21,70 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    this.log(`Writing files into: ${this.destinationRoot('./src')}`);
     const {componentNameInput} = this.answers;
     const componentNameCapitalized =
       componentNameInput.charAt(0).toUpperCase() + componentNameInput.slice(1);
     const componentNameLower = componentNameInput.toLowerCase();
+
+    this.log(`Writing files into: ${this.destinationRoot('./src')}`);
+    const source = this.fs.read(this.destinationPath('index.ts'));
+    // SourceType tells babylon to treat the source as a module and allow things like imports
+    const ast = parser.parse(source, { sourceType: 'module' });
+    /* this.log("++++++")
+    this.log(source)
+    this.log(ast) */
+
+    const isImportDeclaration = path => (
+      t.isImportDeclaration(path.node) || 
+      t.isImportSpecifier(path.parent) || 
+      t.isImportDeclaration(path.parent) || 
+      t.isImportSpecifier(path.parent) || 
+      t.isImportDefaultSpecifier(path.parent)
+    );
+
+    let lastExport = null;
+      t.exportDeclaration
+    const declaration = t.exportDeclaration(
+      [ t.exportSpecifier(t.identifier(`${componentNameLower}`)) ], // This is the imported name
+      t.stringLiteral(`./reducers/${componentNameLower}`), // This is the path to the source
+    );
+   
+   /*  this.log("----")
+    this.log(lastExport) */
+    traverse(ast, {
+
+      // Gets called when visiting *any* node
+      enter(path) {
+        
+        // If we've visited imports and the current node is not an import we insert our declaration *after* the last import.
+        if (lastExport) {
+         
+          lastExport.insertAfter(declaration);
+        }
+        
+      },
+      
+      // Remember exportDeclarations when visiting
+   
+      ExportDeclaration(path) {
+        console.log("export");
+        console.log(path);
+        lastExport = path;
+
+      },
+    
+    });
+    // Generate actually source code from modified AST
+    const {code} = generate(ast, { /* Options */ }, source);
+     /*   this.log("-=-=-=")
+       this.log(code) */
+    // Write source back to file
+    this.fs.write(this.destinationPath('test.ts', code));  
+ 
+    
+ 
+    return;
+   
     this.fs.copyTpl(
       this.templatePath('index.ts'),
       this.destinationPath(`${componentNameLower}/index.ts`),
@@ -58,8 +121,10 @@ module.exports = class extends Generator {
         componentFileName: componentNameLower,
       },
     );
+  /* 
     this.log(`Updating files into:`);
-      this.fs.write(this.destinationPath('gary.ts'), `export * from './{componentNameLower}';`);
+      this.fs.write(this.destinationPath('gary.ts'), `export * from './${componentNameLower}';`); */
   }
+  
   
 };
