6406463d100d1cc011a063762adc7092766a4fe1
Todor Radkov <todor.radkov@news.co.uk>
2020-06-25 18:05:51 +0300

    feat(PPDSC-1187): add automatic export in index.ts

--- component-generator/generators/app/index.ts
+++ component-generator/generators/app/index.ts
@@ -1,5 +1,5 @@
 const Generator = require('yeoman-generator');
-const parser = require("@babel/parser");
+const parser = require("babylon");
 const traverse = require("@babel/traverse").default;
 const t = require('babel-types');
 const generate = require('babel-generator').default;
@@ -28,63 +28,35 @@ module.exports = class extends Generator {
 
     this.log(`Writing files into: ${this.destinationRoot('./src')}`);
     const source = this.fs.read(this.destinationPath('index.ts'));
-    // SourceType tells babylon to treat the source as a module and allow things like imports
     const ast = parser.parse(source, { sourceType: 'module' });
-    /* this.log("++++++")
-    this.log(source)
-    this.log(ast) */
-
-    const isImportDeclaration = path => (
-      t.isImportDeclaration(path.node) || 
-      t.isImportSpecifier(path.parent) || 
-      t.isImportDeclaration(path.parent) || 
-      t.isImportSpecifier(path.parent) || 
-      t.isImportDefaultSpecifier(path.parent)
-    );
 
     let lastExport = null;
-      t.exportDeclaration
-    const declaration = t.exportDeclaration(
-      [ t.exportSpecifier(t.identifier(`${componentNameLower}`)) ], // This is the imported name
-      t.stringLiteral(`./reducers/${componentNameLower}`), // This is the path to the source
-    );
-   
-   /*  this.log("----")
-    this.log(lastExport) */
-    traverse(ast, {
+    let isInserted = false;
+    const exportAllDeclaration = t.exportAllDeclaration(t.stringLiteral(`./${componentNameLower}`));
 
+   
+    traverse(ast, {
+      ExportDeclaration(path) {
+        lastExport = path;
+      },
+    });
+    traverse(ast, {
       // Gets called when visiting *any* node
-      enter(path) {
-        
-        // If we've visited imports and the current node is not an import we insert our declaration *after* the last import.
-        if (lastExport) {
-         
-          lastExport.insertAfter(declaration);
+      enter(path) {        
+        if (lastExport && t.isExportAllDeclaration(path) && !isInserted) {
+          lastExport.insertAfter(exportAllDeclaration);
+          isInserted = true;
         }
         
       },
-      
-      // Remember exportDeclarations when visiting
-   
-      ExportDeclaration(path) {
-        console.log("export");
-        console.log(path);
-        lastExport = path;
 
-      },
     
     });
     // Generate actually source code from modified AST
     const {code} = generate(ast, { /* Options */ }, source);
-     /*   this.log("-=-=-=")
-       this.log(code) */
     // Write source back to file
-    this.fs.write(this.destinationPath('test.ts', code));  
- 
-    
- 
-    return;
-   
+    this.fs.write(this.destinationPath('index.ts'), code);
+
     this.fs.copyTpl(
       this.templatePath('index.ts'),
       this.destinationPath(`${componentNameLower}/index.ts`),
