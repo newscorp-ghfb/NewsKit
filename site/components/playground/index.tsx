b4385d756f8be4542d4e366e7a989b41603354c1
Daniel Georgiev <daniel.georgiev@news.co.uk>
2020-08-19 15:10:21 +0300

    Revert "feat(PPDSE-4): changed allign items on legacyBlock (#704)" (#711)
    This reverts commit 4ebff2fe58cef6da78589549a13fe3c07251966b.

--- site/components/playground/index.tsx
+++ site/components/playground/index.tsx
@@ -172,7 +172,7 @@ export const Playground: React.FC<
   const commonBlockProps: Partial<LegacyBlockProps> = {
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'stretch',
+    alignItems: 'center',
     flexDirection: 'column',
     border: 'solid 1px',
     borderColor: 'interface030',
