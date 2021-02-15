3a1dc7ab439baa86ca5b5e7602989252adaf0f2e
aysha.babur <aysha.babur@news.co.uk>
2021-02-15 14:48:23 +0000

    feat(PPDSE-301): feat/ppdsc301 added offset for lg

--- site/components/header-index/header-index.tsx
+++ site/components/header-index/header-index.tsx
@@ -1,26 +1,23 @@
-import { Cell, Grid, TextBlock, Image } from 'newskit';
+import {Cell, Grid, TextBlock, Image} from 'newskit';
 import React from 'react';
-import { HeaderIndexProps } from './types';
+import {HeaderIndexProps} from './types';
 // import {LinkStandalone as LinkNewskit} from 'newskit';
 // import {LinkProps} from './types';
 
 // TODO remove header-index.svg ?
 
-export const HeaderIndex: React.FC<HeaderIndexProps> = ({title, children, media}) => {
-  
-  return(
-    <Grid>
-      <Cell xs={12} md={8} mdOffset={1}>
-        <TextBlock>
-          {title}
-        </TextBlock>
-        <TextBlock>
-          {children}
-        </TextBlock>
-      </Cell>
-      <Cell xs={12} md={6} mdOffset={6}>
-        <Image src={media.src} alt={''}/>
-      </Cell>
-    </Grid>
-  )
-}
+export const HeaderIndex: React.FC<HeaderIndexProps> = ({
+  title,
+  children,
+  media,
+}) => (
+  <Grid>
+    <Cell xs={12} md={6} lg={5} mdOffset={6}>
+      <Image src={media.src} alt="" />
+    </Cell>
+    <Cell xs={12} md={8} lg={6} mdOffset={1} lgOffset={1}>
+      <TextBlock>{title}</TextBlock>
+      <TextBlock>{children}</TextBlock>
+    </Cell>
+  </Grid>
+);
