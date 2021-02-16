de08cf736c703a07a79c6dffe62747e1827b7f04
Stoyan Delev <mutebg@users.noreply.github.com>
2021-02-16 16:43:12 +0200

    fix(PPDSC-1457): add hover state on tag (#916)
    * fix(PPDSC-1457): add hover state on tag
    
    * fix(PPDSC-1457): improve styled component

--- src/tag/tag.tsx
+++ src/tag/tag.tsx
@@ -4,6 +4,11 @@ import {Flag} from '../flag';
 import {filterOutFalsyProperties} from '../utils/filter-object';
 import {as as emotionAs} from '../utils/component';
 import {useTheme} from '../theme';
+import {styled} from '../utils/style';
+
+const StyledFlag = styled(Flag)`
+  ${({href, disabled}: TagProps) => href && !disabled && {cursor: 'pointer'}}
+`;
 
 export const Tag: React.FC<TagProps> = ({
   overrides = {},
@@ -15,7 +20,7 @@ export const Tag: React.FC<TagProps> = ({
   const {size = TagSize.Medium} = props;
 
   return (
-    <Flag
+    <StyledFlag
       data-testid="tag"
       disabled={disabled}
       href={disabled ? undefined : href}
