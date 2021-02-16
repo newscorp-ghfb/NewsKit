bf17ea050bfbec3fdd3c2c45be07645079e070e3
Vanals <marco91vanali@gmail.com>
2021-02-16 14:00:15 +0000

    feat(PPDSE-301): added gray background to header index

--- site/components/header-index/header-index.tsx
+++ site/components/header-index/header-index.tsx
@@ -1,37 +1,38 @@
-import {Cell, Grid, TextBlock, Image, styled} from 'newskit';
+import {Cell, Grid, TextBlock, Image, styled, Block, getColorFromTheme} from 'newskit';
 import React from 'react';
 import {HeaderIndexProps} from './types';
-// import {LinkStandalone as LinkNewskit} from 'newskit';
-// import {LinkProps} from './types';
-
-// TODO remove header-index.svg ?
 
 export const HeaderIndex: React.FC<HeaderIndexProps> = ({
   title,
   children,
   media,
 }) => {
+
   const StyledGrid = styled(Grid)`
     align-items: center;
+    background-color: ${getColorFromTheme('interface020')};
   `;
+
   return (
     <StyledGrid xsRowGutter="space020" mdRowGutter="space030">
       <Cell lgOrder={2} xs={12} md={6} lg={5} mdOffset={6} lgOffset={0}>
-        <Image src={media.src} alt="" />
+        <Image src={media.src} alt=""/>
       </Cell>
       <Cell xs={12} md={8} lg={6} mdOffset={1}>
+        <Block spaceStack={{xs: 'space060', md: 'space070'}}>
+          <TextBlock
+            as="h1"
+            stylePreset="inkContrast"
+            typographyPreset={{
+              xs: 'editorialHeadline060',
+              md: 'editorialHeadline080',
+            }}
+          >
+            {title}
+          </TextBlock>
+        </Block>
         <TextBlock
-          as="h1"
-          stylePreset="inkContrast"
-          typographyPreset={{
-            xs: 'editorialHeadline060',
-            md: 'editorialHeadline080',
-          }}
-        >
-          {title}
-        </TextBlock>
-        <TextBlock
-          stylePreset="inkNegative"
+          stylePreset="inkBase"
           typographyPreset={{
             xs: 'editorialSubheadline010',
             md: 'editorialSubheadline020',
