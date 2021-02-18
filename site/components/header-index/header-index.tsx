61017f4ae6d161e0a75662b9f788543ccd76bc4a
Vanals <marco91vanali@gmail.com>
2021-02-18 13:45:45 +0000

    feat(PPDSE-301): added padding and margins

--- site/components/header-index/header-index.tsx
+++ site/components/header-index/header-index.tsx
@@ -21,33 +21,42 @@ export const HeaderIndex: React.FC<HeaderIndexProps> = ({
   `;
 
   return (
-    <StyledGrid xsRowGutter="space020" mdRowGutter="space030">
-      <Cell lgOrder={2} xs={12} md={6} lg={5} mdOffset={6} lgOffset={0}>
-        <Image src={media.src} alt="" />
-      </Cell>
-      <Cell xs={12} md={8} lg={6} mdOffset={1}>
-        <Block spaceStack={{xs: 'space060', md: 'space070'}}>
-          <TextBlock
-            as="h1"
-            stylePreset="inkContrast"
-            typographyPreset={{
-              xs: 'editorialHeadline060',
-              md: 'editorialHeadline080',
-            }}
-          >
-            {title}
-          </TextBlock>
+    <Block spaceStack={{xs: 'space080', lg: 'space090'}}>
+      <StyledGrid
+        xsRowGutter="space020"
+        mdRowGutter="space030"
+        xsMargin="space000"
+        xsColumnGutter="space050"
+      >
+        <Cell lgOrder={2} xs={12} md={6} lg={5} mdOffset={6} lgOffset={0}>
+          <Image src={media.src} alt="" />
+        </Cell>
+        <Block spaceStack={{xs: 'space080'}}>
+          <Cell xs={12} md={8} lg={6} mdOffset={1}>
+            <Block spaceStack={{xs: 'space060', md: 'space070'}}>
+              <TextBlock
+                as="h1"
+                stylePreset="inkContrast"
+                typographyPreset={{
+                  xs: 'editorialHeadline060',
+                  md: 'editorialHeadline080',
+                }}
+              >
+                {title}
+              </TextBlock>
+            </Block>
+            <TextBlock
+              stylePreset="inkBase"
+              typographyPreset={{
+                xs: 'editorialSubheadline010',
+                md: 'editorialSubheadline020',
+              }}
+            >
+              {children}
+            </TextBlock>
+          </Cell>
         </Block>
-        <TextBlock
-          stylePreset="inkBase"
-          typographyPreset={{
-            xs: 'editorialSubheadline010',
-            md: 'editorialSubheadline020',
-          }}
-        >
-          {children}
-        </TextBlock>
-      </Cell>
-    </StyledGrid>
+      </StyledGrid>
+    </Block>
   );
 };
--- src/image/types.ts
+++ src/image/types.ts
@@ -43,7 +43,10 @@ export interface ImageProps
     height?: HeightProperty<string>;
     width?: WidthProperty<string>;
     maxHeight?: MaxHeightProperty<string>;
+    // TODO can we set max width only for S?
     maxWidth?: MaxWidthProperty<string>;
+    // TODO add minHeight, only for  XL and L? can it take an object? it takes
+    // string for maxH and maxW atm
     stylePreset?: MQ<string>;
     caption?: CaptionOverrides;
   };
