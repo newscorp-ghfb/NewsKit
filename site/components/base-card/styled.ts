b79eacb97c1569e9b5d4c69d81ab33f1063007f6
Daniel Georgiev <Daniel.Georgiev@news.co.uk>
2021-03-16 15:15:29 +0200

    fix(PPDSE-336): updated motions

--- site/components/base-card/styled.ts
+++ site/components/base-card/styled.ts
@@ -2,11 +2,11 @@ import {styled, CardInset, getMotionFromTheme, Card} from 'newskit';
 
 export const StyledCardInteractive = styled(CardInset)`
   transition-property: box-shadow;
-  transition-duration: ${getMotionFromTheme('motionDuration020')};
+  transition-duration: ${getMotionFromTheme('motionDuration030')};
   transition-timing-function: ${getMotionFromTheme('motionEaseOut')};
 `;
 export const StyledBaseCardNonInteractive = styled(Card)`
   transition-property: box-shadow;
-  transition-duration: ${getMotionFromTheme('motionDuration020')};
+  transition-duration: ${getMotionFromTheme('motionDuration030')};
   transition-timing-function: ${getMotionFromTheme('motionEaseOut')};
 `;
