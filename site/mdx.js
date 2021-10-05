9d2b0bbb3ff524cd5322c7d810bf02401befe06e
Xin Wang <wangxin00163@yahoo.co.uk>
2021-10-05 09:15:45 +0100

    chore(PPDSC-1792): upgrade next.js to v11.1.2 (#1199)
    * chore: upgrade next.js
    
    * fix: move rehypeExtractDocTitle back
    
    * fix: add comment for mdx
    
    * fix: lint

--- site/mdx.js
+++ site/mdx.js
@@ -1,13 +1,12 @@
 /* eslint-disable import/order */
 /* eslint-env node */
 
-const rehypePrism = require('@mapbox/rehype-prism');
 const rehypeExtractDocTitle = require('./mdx-plugins/rehype-extract-doc-title');
 
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
   options: {
-    rehypePlugins: [[rehypeExtractDocTitle, {tag: 'h1'}], rehypePrism],
+    rehypePlugins: [[rehypeExtractDocTitle, {tag: 'h1'}]],
   },
 });
 
--- site/next.config.js
+++ site/next.config.js
@@ -3,63 +3,43 @@ const path = require('path');
 const {resolve, join: joinPath} = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
-const withImages = require('next-images');
-const withCSS = require('@zeit/next-css');
+// remove @next/mdx when doc pages are all tranformed to tsx
 const withMDX = require('./mdx');
 
-module.exports = withCSS(
-  withMDX(
-    withImages({
-      webpack: config => {
-        // enable transpile of files outside of directory - https://github.com/vercel/next.js/issues/5666#issuecomment-782922703
-        config.module.rules.forEach(rule => {
-          if (rule.test && rule.test.toString().includes('tsx|ts')) {
-            rule.include = [...rule.include, joinPath(__dirname, '../src')];
-          }
-        });
+module.exports = withMDX({
+  webpack: config => {
+    // enable transpile of files outside of directory - https://github.com/vercel/next.js/issues/5666#issuecomment-782922703
+    config.module.rules.forEach(rule => {
+      if (rule.test && rule.test.toString().includes('tsx|ts')) {
+        rule.include = [...rule.include, joinPath(__dirname, '../src')];
+      }
+    });
 
-        config.resolve.alias['@components/page-title'] = path.join(
-          __dirname,
-          'components/page-title.tsx',
-        );
+    config.resolve.alias['@components/page-title'] = path.join(
+      __dirname,
+      'components/page-title.tsx',
+    );
 
-        // references next polyfills example: https://github.com/zeit/next.js/tree/canary/examples/with-polyfills
-        const originalEntry = config.entry;
-        config.entry = async () => {
-          const entries = await originalEntry();
+    config.plugins = config.plugins || [];
+    config.plugins = [
+      ...config.plugins,
 
-          if (
-            entries['main.js'] &&
-            !entries['main.js'].includes('./helpers/polyfills.js')
-          ) {
-            entries['main.js'].unshift('./helpers/polyfills.js');
-          }
+      new CopyPlugin([
+        {
+          from: resolve(__dirname, '../fonts'),
+          to: resolve(__dirname, 'public/static/fonts'),
+        },
+      ]),
+    ];
 
-          return entries;
-        };
-
-        config.plugins = config.plugins || [];
-        config.plugins = [
-          ...config.plugins,
-
-          new CopyPlugin([
-            {
-              from: resolve(__dirname, '../fonts'),
-              to: resolve(__dirname, 'public/static/fonts'),
-            },
-          ]),
-        ];
-
-        return config;
-      },
-      pageExtensions: ['js', 'jsx', 'mdx', 'tsx', 'ts'],
-      trailingSlash: true, // added for static export https://stackoverflow.com/a/66573096/641979
-      ...(() => {
-        const basePath = process.env.BASE_PATH
-          ? {basePath: `/${process.env.BASE_PATH}`}
-          : {};
-        return basePath;
-      })(), // used to build in relative paths when site is located in a sub dir
-    }),
-  ),
-);
+    return config;
+  },
+  pageExtensions: ['js', 'jsx', 'mdx', 'tsx', 'ts'],
+  trailingSlash: true, // added for static export https://stackoverflow.com/a/66573096/641979
+  ...(() => {
+    const basePath = process.env.BASE_PATH
+      ? {basePath: `/${process.env.BASE_PATH}`}
+      : {};
+    return basePath;
+  })(), // used to build in relative paths when site is located in a sub dir
+});
