b985fba8d8ff41f6bd7a3f58f9f75f4cbd14a029
Srinivasan-Ramasamy <87307441+Srinivasan-Ramasamy@users.noreply.github.com>
2021-12-17 15:05:04 +0530

    docs(PPDSC-1926): About page (#1321)
    * docs(PPDSC-1926): About page

--- site/pages/about/introduction.tsx
+++ site/pages/about/introduction.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
-import {
-  Block,
-  getSizingCssFromTheme,
-  Grid,
-  Link,
-  Paragraph,
-  styled,
-} from 'newskit';
+import {Block, getSizingCssFromTheme, Grid, styled, Divider} from 'newskit';
 import Head from 'next/head';
 import Layout, {LayoutProps} from '../../components/layout';
 import {PageIntroduction} from '../../components/page-introduction';
 import {SectionIntroduction} from '../../components/section-introduction';
-import {Separator} from '../../components/separator';
 import {MediaList} from '../../components/media-list';
 import {ComponentPageCell} from '../../components/layout-cells';
+import {ContentText} from '../../components/text-section';
+import {Link} from '../../components/link';
 
 const principleCardoverrides = {
   title: {
@@ -38,9 +32,9 @@ const cards = [
     stylePrefix: 'principleCard',
     label: 'User focused',
     title:
-      'We don’t design for screens, we design for people; whether the product teams using our components or the customers using the end products.',
+      'We design for people. Whether the product teams using our components or the customers using the end products',
     description:
-      'We establish insights on our user needs through qualitative and quantitative data from research. We continually test our components and the overall design system to ensure it meets these needs.',
+      'We establish insights into our user needs through qualitative and quantitative data; we continually test our components and the overall design system and seek feedback to ensure it meets these needs.',
     overrides: {
       title,
       description,
@@ -54,9 +48,9 @@ const cards = [
     stylePrefix: 'principleCard',
     label: 'Inclusive',
     title:
-      'Every design decision has the potential to include or exclude users. To ensure our solutions meet the needs of as many users as possible we take an inclusive first, design approach. ',
+      'We’re inclusive first to ensure our solutions meet the needs of as many users as possible',
     description:
-      'The default components are accessible to WCAG 2.1 AA standards. We strive for clarity around everything we do, simplifying the complex. We use simple language, avoid acronyms and offer more detail when required. Our users should always be able to understand and act with confidence.',
+      'Every design decision has the potential to include or exclude users. The components are accessible to WCAG 2.1 AA standards. We strive for clarity around everything we do, simplifying the complex. We use simple language, avoid acronyms and offer more detail when required. Our users should always be able to understand and act with confidence.',
     overrides: {
       title,
       description,
@@ -70,9 +64,9 @@ const cards = [
     stylePrefix: 'principleCard',
     label: 'Essential',
     title:
-      'Every component serves a user’s need that is shared by multiple services or products.',
+      'Every component serves a user’s need that is shared by multiple services or products',
     description:
-      "We don't create new solutions to solve problems that already exist in the system unless they are intended to replace them. We never build something 'just in case'. Every design decision and component has a clear purpose and contributes to a larger purpose; if it doesn’t, it shouldn’t be there.",
+      "We never build something 'just in case'. Every design decision and component has a clear purpose and contributes to a larger goal; it shouldn't be there if it doesn't.",
     overrides: {
       title,
       description,
@@ -85,42 +79,9 @@ const cards = [
     },
     stylePrefix: 'principleCard',
     label: 'Collaborative',
-    title: 'Great ideas come from everywhere.',
+    title: 'Great ideas come from everywhere',
     description:
-      'Everyone is encouraged to contribute. We share everything we are doing at every step in the process: designs, code, ideas, successes or failures. We encourage and seek feedback, suggestions and contributions. We all take responsibility for the output of the team and reject ownership.',
-    overrides: {
-      title,
-      description,
-    },
-  },
-  {
-    media: {
-      src: 'static/about-principles-05.svg',
-      alt: '',
-    },
-    stylePrefix: 'principleCard',
-    label: 'Consistent',
-    title:
-      'Ensuring everything reusable and shareable instead of constantly reinventing different solutions to solve the same need. ',
-    description:
-      'We create familiarity across our products by using the same solution to the same problem.',
-    overrides: {
-      title,
-      description,
-    },
-  },
-  {
-    media: {
-      src: 'static/about-principles-06.svg',
-      alt: '',
-    },
-    stylePrefix: 'principleCard',
-    label: 'Robust',
-    title:
-      'Our users require a design system that they can trust and rely upon. ',
-    description:
-      'We thoroughly consider, design, develop and test solutions before they are released for users to consume. Whenever we make a change to the design system, we communicate this through well-structured documentation.',
-
+      'Everyone is encouraged to contribute. We share everything we are doing at every step: designs, code, ideas, successes or failures. We encourage and seek feedback, suggestions and contributions. We all take responsibility for the output of the team and reject ownership.',
     overrides: {
       title,
       description,
@@ -135,7 +96,7 @@ export default ({path, ...props}: LayoutProps) => (
       <title key="title">About | NewsKit design system</title>
       <meta
         name="Description"
-        content="NewsKit is News UK’s design system. It provides interactive building blocks and guidelines for crafting cohesive digital product interfaces and accelerating development. build better products faster."
+        content="NewsKit provides components, guidelines and standards to enable digital product teams to create high-quality, consistent products quickly. NewsKit is built on modular design principles and backed by best practice guidance for design and development."
       />
     </Head>
     <WrapperWithPadding />
@@ -143,48 +104,68 @@ export default ({path, ...props}: LayoutProps) => (
       <PageIntroduction
         type="About"
         name="Introduction"
-        introduction="NewsKit is News UK’s design system. It provides interactive building blocks and guidelines for crafting cohesive digital product interfaces and accelerating development.
-          build better products faster."
+        introduction="NewsKit provides components, guidelines and standards to enable digital product teams to create high-quality, consistent products quickly. NewsKit is built on modular design principles and backed by best practice guidance for design and development."
         hero={{
           illustration: 'about/introduction-hero-stepping-stones-illustration',
         }}
       />
-      <SectionIntroduction title="Introduction" cellProps={{lg: 8}}>
+      <ComponentPageCell>
+        <Block spaceStack="space050" />
+      </ComponentPageCell>
+      <SectionIntroduction title="Benefits" cellProps={{lg: 8}}>
         <>
-          <Paragraph>
-            The design system aims to improve the consistency and quality of
-            digital products and experiences, enabling the design and
-            development process to be more efficient and focused, establishing a
-            common language between designer and engineer, and providing clear,
-            considered guidance around design and engineering best practices.
-          </Paragraph>
-          <br />
-          <Paragraph>
-            NewsKit currently serves a wide range of designers and developers at
-            News UK building digital products and experiences with a view to
-            becoming publicly open source in the future.
-          </Paragraph>
-          <br />
-          <Paragraph>
-            It is maintained by a core centralised team at News UK with
-            contributions from other product teams to evolve and improve
-            NewsKit.
-          </Paragraph>
-          <br />
-          <Paragraph>
-            The UI library is built using{' '}
-            <Link href="https://nidigitalsolutions.jira.com/wiki/spaces/NPP/pages/2349139141/Designing+with+NewsKit+-+Web+Documentation">
-              Figma for design
+          <ContentText>
+            Design systems have become the gold standard for product
+            development. Some benefits include:
+          </ContentText>
+          <ContentText title="Speed to market">
+            Upfront research, design and development allows teams to build new
+            features or products quickly and with confidence. Rapid creation
+            translates to the increased pace of innovation.
+          </ContentText>
+          <ContentText title="Shared knowledge">
+            NewsKit is built through collaboration and is available to all.
+            Feedback from a diverse range of consumers produces a more robust,
+            considered and flexible product. In addition, NewsKit allows teams
+            to benefit from the research and experience of other teams and solve
+            problems once.
+          </ContentText>
+          <ContentText title="Best Practice">
+            All NewsKit components support keyboard navigation, screen readers
+            and touch screens, conforming to{' '}
+            <Link href="https://www.w3.org/TR/WCAG21/" target="_blank">
+              W3C&apos;s WCAG 2.1 guidelines
             </Link>{' '}
-            and{' '}
-            <Link href="https://nidigitalsolutions.jira.com/wiki/spaces/NPP/pages/2348942613/Engineering+with+NewsKit+-+Web+Documentation">
-              React for the codebase.
-            </Link>
-          </Paragraph>
+            for accessibility. In addition, the components take in SEO
+            considerations where relevant and are rigorously tested. Finally,
+            detailed guidance documentation ensures clarity.
+          </ContentText>
+          <ContentText title="Sustainability">
+            NewsKit allows teams to do more with less; components are maintained
+            centrally, enabling teams to achieve incremental upgrades and manage
+            their digital product needs as they grow.
+          </ContentText>
+          <ContentText title="Flexibility">
+            The powerful theming system allows styling to be tailored to the
+            needs of any brand. Themes can be applied globally or per component,
+            enabling visual differences between sections, sub-brands or
+            unlocking user preferences like dark mode or page density settings.
+            In addition, all components are designed to ensure they can be
+            applied to multiple use cases.
+          </ContentText>
+          <ContentText title="Consistency">
+            NewsKit establishes a common language between designer and engineer
+            and applies a systematic approach to all aspects of the interface.
+            Infinitely reusable components and clear guidance around design and
+            engineering best practices enable teams to build consistency across
+            products and teams.
+          </ContentText>
         </>
       </SectionIntroduction>
       <ComponentPageCell>
-        <Separator />
+        <Block spaceStack="space100">
+          <Divider />
+        </Block>
       </ComponentPageCell>
       <SectionIntroduction title="Principles" cellProps={{lg: 8}}>
         NewsKit design system and the NewsKit team is guided by a set of
