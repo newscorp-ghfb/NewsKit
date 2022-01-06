a7bff3eefba4711e54db9d5cd9ff56ad1c640110
Stoyan Delev <stoyan.delev@news.co.uk>
2022-01-06 10:51:18 +0200

    feat(PPDSC-1875): add text-input

--- src/theme/component-defaults.ts
+++ src/theme/component-defaults.ts
@@ -13,6 +13,7 @@ export const componentDefaults = {
         spaceInline: 'space020',
       },
     },
+
     medium: {
       stylePreset: 'assistiveText',
       typographyPreset: 'utilityBody020',
@@ -759,95 +760,7 @@ export const componentDefaults = {
       },
     },
   },
-  textInput: {
-    small: {
-      label: {
-        stylePreset: 'textInputLabel',
-        typographyPreset: 'utilityLabel030',
-        spaceStack: 'space040',
-        spaceInline: 'space010',
-      },
-      input: {
-        stylePreset: 'textInput',
-        spaceInset: 'spaceInset020',
-        minHeight: 'sizing060',
-        typographyPreset: 'utilityBody020',
-        spaceStack: 'space020',
-        leadingIcon: {
-          iconOffset: 'space030',
-          spaceInset: 'space070',
-        },
-        validationIcon: {
-          iconOffset: 'space030',
-          spaceInset: 'space070',
-          iconSize: 'iconSize020',
-        },
-      },
-      assistiveText: {
-        stylePreset: 'textInputAssistiveText',
-        typographyPreset: 'utilityLabel020',
-        minHeight: 'sizing030',
-      },
-    },
-    medium: {
-      label: {
-        stylePreset: 'textInputLabel',
-        typographyPreset: 'utilityLabel030',
-        spaceStack: 'space040',
-        spaceInline: 'space010',
-      },
-      input: {
-        stylePreset: 'textInput',
-        spaceInset: 'spaceInset030',
-        minHeight: 'sizing080',
-        typographyPreset: 'utilityBody020',
-        spaceStack: 'space020',
-        leadingIcon: {
-          iconOffset: 'space030',
-          spaceInset: 'space070',
-        },
-        validationIcon: {
-          iconOffset: 'space030',
-          spaceInset: 'space070',
-          iconSize: 'iconSize020',
-        },
-      },
-      assistiveText: {
-        stylePreset: 'textInputAssistiveText',
-        typographyPreset: 'utilityLabel020',
-        minHeight: 'sizing030',
-      },
-    },
-    large: {
-      label: {
-        stylePreset: 'textInputLabel',
-        typographyPreset: 'utilityLabel030',
-        spaceStack: 'space040',
-        spaceInline: 'space010',
-      },
-      input: {
-        stylePreset: 'textInput',
-        spaceInset: 'spaceInset030',
-        minHeight: 'sizing090',
-        typographyPreset: 'utilityBody030',
-        spaceStack: 'space020',
-        leadingIcon: {
-          iconOffset: 'space030',
-          spaceInset: 'space080',
-        },
-        validationIcon: {
-          iconOffset: 'space030',
-          spaceInset: 'space080',
-          iconSize: 'iconSize030',
-        },
-      },
-      assistiveText: {
-        stylePreset: 'textInputAssistiveText',
-        typographyPreset: 'utilityLabel030',
-        minHeight: 'sizing030',
-      },
-    },
-  },
+
   titleBar: {
     stylePreset: 'titleBar',
     spaceInset: {
--- src/theme/presets/style-presets.ts
+++ src/theme/presets/style-presets.ts
@@ -187,63 +187,7 @@ stylePresets.inputField = {
   },
 };
 
-// Text Input
-stylePresets.textInput = {
-  base: {
-    backgroundColor: '{{colors.transparent}}',
-    borderStyle: 'solid',
-    borderColor: '{{colors.interface040}}',
-    borderWidth: '{{borders.borderWidthDefault}}',
-    borderRadius: '{{borders.borderRadiusDefault}}',
-    color: '{{colors.inkBase}}',
-    textOverflow: 'ellipsis',
-    placeholderColor: '{{colors.inkSubtle}}',
-  },
-  focus: {
-    backgroundColor: '{{colors.interface020}}',
-    borderColor: '{{colors.interfaceBrand010}}',
-  },
-  hover: {
-    backgroundColor: '{{colors.interface020}}',
-  },
-  disabled: {
-    borderColor: '{{colors.interactiveDisabled010}}',
-    color: '{{colors.inkNonEssential}}',
-  },
-  invalid: {
-    caretColor: '{{colors.inkNegative}}',
-    borderColor: '{{colors.interactiveNegative030}}',
-  },
-  valid: {
-    caretColor: '{{colors.inkPositive}}',
-    borderColor: '{{colors.interactivePositive030}}',
-  },
-};
-
-stylePresets.textInputLabel = {
-  base: {
-    color: '{{colors.inkContrast}}',
-  },
-  disabled: {
-    color: '{{colors.inkNonEssential}}',
-  },
-};
-
-stylePresets.textInputAssistiveText = {
-  base: {
-    color: '{{colors.inkSubtle}}',
-  },
-  disabled: {
-    color: '{{colors.inkNonEssential}}',
-  },
-  invalid: {
-    color: '{{colors.inkNegative}}',
-  },
-  valid: {
-    color: '{{colors.inkPositive}}',
-  },
-};
-
+// ICONS:
 stylePresets.iconPositive = {
   base: {
     iconColor: '{{colors.inkPositive}}',
