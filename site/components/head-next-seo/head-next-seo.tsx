ccc41d50698b341f3180fc159704d94f95670ee2
Vanals <marco91vanali@gmail.com>
2022-01-11 11:58:20 +0000

    feat(PPDSC-1939): added OG image

--- site/components/head-next-seo/head-next-seo.tsx
+++ site/components/head-next-seo/head-next-seo.tsx
@@ -6,5 +6,13 @@ export function HeadNextSeo({title, description}: NextSeoProps) {
     ? `${title} | NewsKit design system`
     : 'NewsKit design system';
 
-  return <NextSeo title={nextSeoTitle} description={description} />;
+  return (
+    <NextSeo
+      title={nextSeoTitle}
+      description={description}
+      openGraph={{
+        images: [{url: 'static/favicon-32x32.png'}],
+      }}
+    />
+  );
 }
