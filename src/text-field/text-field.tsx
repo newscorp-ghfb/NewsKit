4ac0ba04ac0a587afe2f05754ca0b7c5bba0c2d5
Aysha <ayshababur@yahoo.com>
2022-01-13 15:44:06 +0000

    feat(PPDSC-1877): (PPDSC-1877)textfield stuff moved

--- src/text-field/text-field.tsx
+++ src/text-field/text-field.tsx
@@ -4,9 +4,14 @@ import {StyledInput} from './styled';
 import {WithEnhancers} from '../with-enhancers/with-enhancers';
 import {useTheme} from '../theme';
 import {getToken} from '../utils/get-token';
+import defaults from './defaults';
 import {getSingleStylePreset} from '../utils/style/style-preset';
+import {withOwnTheme} from '../utils/with-own-theme';
 
-export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
+export const ThemelessTextField = React.forwardRef<
+  HTMLInputElement,
+  TextFieldProps
+>(
   (
     {
       size = 'medium' as TextFieldSize,
@@ -92,3 +97,7 @@ export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
     );
   },
 );
+
+export const TextField = withOwnTheme(ThemelessTextField)({
+  defaults,
+});
