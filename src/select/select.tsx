e5dfea4930f0f33fe2dece11a0ec88e89b8e6d5a
Stoyan Delev <stoyan.delev@news.co.uk>
2022-01-24 16:50:01 +0200

    feat(PPNKD-348): add scrollIntoView

--- src/select/select.tsx
+++ src/select/select.tsx
@@ -145,6 +145,17 @@ export const Select = React.forwardRef<HTMLInputElement, SelectProps>(
       offsetLeft: left = 0,
     } = (selectRef && selectRef.current) || {};
 
+    useEffect(() => {
+      if (
+        isOpen &&
+        panelRef &&
+        panelRef.current &&
+        'scrollIntoView' in panelRef.current
+      ) {
+        panelRef.current?.scrollIntoView();
+      }
+    }, [isOpen, panelRef]);
+
     return (
       <>
         <SelectButton
