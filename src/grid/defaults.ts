fb9f9580e6db74d1bbeb1c242a97ee9c539e1636
Xin Wang <wangxin00163@yahoo.co.uk>
2022-01-25 11:01:08 +0000

    feat(PPDSC-1883): theme splitting for grid (#22)
    * fix: theme split for grid
    
    * fix: address reviews & upgrade applitools
    
    * fix: add package to fix puppeteer
    
    * fix: fix puppeteer issue
    
    * fix: update circle
    
    * fix: headless chrome in circle
    
    * fix: another try
    
    * fix: remove formatting

---
+++ src/grid/defaults.ts
@@ -0,0 +1,27 @@
+export default {
+  grid: {
+    maxWidth: '1920px',
+    columns: 12,
+    containerMargin: {
+      xs: 'space040',
+      sm: 'space040',
+      md: 'space040',
+      lg: 'space050',
+      xl: 'space050',
+    },
+    columnGutters: {
+      xs: 'space040',
+      sm: 'space040',
+      md: 'space040',
+      lg: 'space050',
+      xl: 'space050',
+    },
+    rowGutters: {
+      xs: 'space040',
+      sm: 'space040',
+      md: 'space040',
+      lg: 'space050',
+      xl: 'space050',
+    },
+  },
+};
--- src/theme/component-defaults.ts
+++ src/theme/component-defaults.ts
@@ -52,31 +52,6 @@ export const componentDefaults = {
       spaceInset: 'spaceInset020',
     },
   },
-  grid: {
-    maxWidth: '1920px',
-    columns: 12,
-    containerMargin: {
-      xs: 'space040',
-      sm: 'space040',
-      md: 'space040',
-      lg: 'space050',
-      xl: 'space050',
-    },
-    columnGutters: {
-      xs: 'space040',
-      sm: 'space040',
-      md: 'space040',
-      lg: 'space050',
-      xl: 'space050',
-    },
-    rowGutters: {
-      xs: 'space040',
-      sm: 'space040',
-      md: 'space040',
-      lg: 'space050',
-      xl: 'space050',
-    },
-  },
   headlineH1: {
     stylePreset: 'inkContrast',
     typographyPreset: {
