1c583d898d1055ae458cc24583fef5d0628a9b19
Stoyan Delev <stoyan.delev@news.co.uk>
2022-02-08 10:48:41 +0200

    feat(PPDSC-1977): add unit tests to dialog

--- src/dialog/base-dialog-tests.tsx
+++ src/dialog/base-dialog-tests.tsx
@@ -137,18 +137,32 @@ export const sharedDialogTests = (
     expect(fragment).toMatchSnapshot();
   });
 
-  // test.only('toggle aria-hidden to parent elements', () => {
-  //   const Component = () => (
-  //     <>
-  //       <p>other content</p>
-  //       <Dialog open onDismiss={() => {}}>
-  //         Dialog content
-  //       </Dialog>
-  //     </>
-  //   );
+  test('toggle aria-hidden to content outside dialog', () => {
+    const Component = () => {
+      const [open, setOpen] = React.useState(true);
 
-  //   const {getByTestId, asFragment} = renderWithTheme(Component);
+      return (
+        <>
+          <p data-testid="outside-content">other content</p>
+          <Dialog open={open} onDismiss={() => setOpen(false)}>
+            Dialog content
+          </Dialog>
+        </>
+      );
+    };
 
-  //   expect(asFragment()).toMatchSnapshot();
-  // });
+    const {getByTestId, asFragment} = renderWithTheme(Component);
+
+    // outside content is hidden for screen readers when dialog is open
+    expect(getByTestId('outside-content')).toHaveAttribute(
+      'aria-hidden',
+      'true',
+    );
+    expect(asFragment()).toMatchSnapshot();
+
+    // outside content should be visible when dialog is closed
+    fireEvent.click(getByTestId('button'));
+    expect(getByTestId('outside-content')).not.toHaveAttribute('aria-hidden');
+    expect(asFragment()).toMatchSnapshot();
+  });
 };
