f8ede21a0aabdff5b4a733db943f7ddb05f1ebe6
Aysha <ayshababur@yahoo.com>
2022-02-10 16:57:29 +0000

    feat(PPDSC-1810): utils page done

---
+++ site/public/static/examples/utils/get-from-theme/get-transition-preset-from-theme.tsx
@@ -0,0 +1,20 @@
+transitionPresets.backgroundColorChange = {
+  base: {
+    transitionProperty: 'background-color',
+    transitionDuration: '{{motions.motionDuration030}}',
+    transitionTimingFunction: '{{motions.motionTimingEaseOut}}',
+  },
+};
+
+stylePresets.p = {
+  base: {
+    backgroundColor: '{{colors.purple030}}',
+  },
+  hover: {
+    backgroundColor: '{{colors.purple070}}',
+  },
+};
+const button = styled.p`
+  background-color: green;
+  ${getTransitionPresetFromTheme('backgroundColorChange')}
+`;
