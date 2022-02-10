a65da5eb56e89f6bae82287262c685981faad72e
Stoyan Delev <stoyan.delev@news.co.uk>
2022-02-10 14:02:10 +0200

    fix(PPDSC-1994): adding packate to prevent future errors

--- src/test/test-framework-setup.ts
+++ src/test/test-framework-setup.ts
@@ -1,6 +1,28 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable no-console */
 import {matchers, createSerializer} from '@emotion/jest';
+import failOnConsole from 'jest-fail-on-console';
+
+failOnConsole({
+  silenceMessage: errorMessage => {
+    if (
+      errorMessage.includes(
+        'Warning: Encountered two children with the same key,',
+      ) &&
+      errorMessage.includes('mock-nk-1')
+    ) {
+      return true;
+    }
+
+    if (
+      errorMessage.includes(
+        'Error occurred attempting to open privacy manager modal. Is Sourcepoint CMP present on this page?',
+      )
+    ) {
+      return true;
+    }
+
+    return false;
+  },
+});
 
 expect.addSnapshotSerializer(createSerializer());
 expect.extend(matchers);
@@ -11,17 +33,17 @@ jest.mock('../utils/get-ssr-id', () => ({
 
 jest.mock('date-fns/format', () => () => 'Mock Date');
 
-const consoleError = console.error;
-console.error = (...args: any[]) => {
-  // We want to ignore errors about the duplicate key prop in react if it's due to the above mock.
-  if (
-    args[0].includes(
-      'Warning: Encountered two children with the same key, `%s`.',
-    ) &&
-    args[1].includes('mock-nk-1')
-  ) {
-    return;
-  }
-  // If you're reading this in the Jest error log, it's not where the error came from!
-  consoleError.apply(console, args as any);
-};
+// const consoleError = console.error;
+// console.error = (...args: any[]) => {
+//   // We want to ignore errors about the duplicate key prop in react if it's due to the above mock.
+//   if (
+//     args[0].includes(
+//       'Warning: Encountered two children with the same key, `%s`.',
+//     ) &&
+//     args[1].includes('mock-nk-1')
+//   ) {
+//     return;
+//   }
+//   // If you're reading this in the Jest error log, it's not where the error came from!
+//   consoleError.apply(console, args as any);
+// };
