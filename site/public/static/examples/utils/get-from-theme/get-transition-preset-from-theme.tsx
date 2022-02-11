7153a6e04a0d6c41a53a03ca0759c70fe24f0921
Aysha <ayshababur@yahoo.com>
2022-02-11 09:38:47 +0000

    feat(PPDSC-1810): testing

--- site/public/static/examples/utils/get-from-theme/get-transition-preset-from-theme.tsx
+++ site/public/static/examples/utils/get-from-theme/get-transition-preset-from-theme.tsx
@@ -1,19 +1,21 @@
-transitionPresets.backgroundColorChange = {
-  base: {
-    transitionProperty: 'background-color',
-    transitionDuration: '{{motions.motionDuration030}}',
-    transitionTimingFunction: '{{motions.motionTimingEaseOut}}',
-  },
-};
+import {styled, getTransitionPresetFromTheme} from 'newskit';
 
-stylePresets.p = {
-  base: {
-    backgroundColor: '{{colors.purple030}}',
-  },
-  hover: {
-    backgroundColor: '{{colors.purple070}}',
-  },
-};
+// transitionPresets.backgroundColorChange = {
+//   base: {
+//     transitionProperty: 'background-color',
+//     transitionDuration: '{{motions.motionDuration030}}',
+//     transitionTimingFunction: '{{motions.motionTimingEaseOut}}',
+//   },
+// };
+
+// stylePresets.p = {
+//   base: {
+//     backgroundColor: '{{colors.purple030}}',
+//   },
+//   hover: {
+//     backgroundColor: '{{colors.purple070}}',
+//   },
+// };
 const button = styled.p`
   background-color: green;
   ${getTransitionPresetFromTheme('backgroundColorChange')}
