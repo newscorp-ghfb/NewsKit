c5c1dd3341548496e32e53495ec512e13e38459f
Aysha <ayshababur@yahoo.com>
2022-02-11 09:40:55 +0000

    feat(PPDSC-1810): added example back

--- site/public/static/examples/utils/get-from-theme/get-transition-preset-from-theme.tsx
+++ site/public/static/examples/utils/get-from-theme/get-transition-preset-from-theme.tsx
@@ -1,21 +1,25 @@
-import {styled, getTransitionPresetFromTheme} from 'newskit';
+import {
+  styled,
+  getTransitionPresetFromTheme
+} from 'newskit';
 
-// transitionPresets.backgroundColorChange = {
-//   base: {
-//     transitionProperty: 'background-color',
-//     transitionDuration: '{{motions.motionDuration030}}',
-//     transitionTimingFunction: '{{motions.motionTimingEaseOut}}',
-//   },
-// };
 
-// stylePresets.p = {
-//   base: {
-//     backgroundColor: '{{colors.purple030}}',
-//   },
-//   hover: {
-//     backgroundColor: '{{colors.purple070}}',
-//   },
-// };
+transitionPresets.backgroundColorChange = {
+  base: {
+    transitionProperty: 'background-color',
+    transitionDuration: '{{motions.motionDuration030}}',
+    transitionTimingFunction: '{{motions.motionTimingEaseOut}}',
+  },
+};
+
+stylePresets.p = {
+  base: {
+    backgroundColor: '{{colors.purple030}}',
+  },
+  hover: {
+    backgroundColor: '{{colors.purple070}}',
+  },
+};
 const button = styled.p`
   background-color: green;
   ${getTransitionPresetFromTheme('backgroundColorChange')}
