0659a679fc4ec79d6d48e1ce0f1adaf8fa9b6fed
Stoyan Delev <stoyan.delev@news.co.uk>
2022-02-11 11:38:18 +0200

    fix(PPDSC-1980): reverse applitools config

--- applitools.components.config.js
+++ applitools.components.config.js
@@ -3,7 +3,7 @@ module.exports = {
   batchId: process.env.APPLITOOLS_BATCH_ID,
   storybookUrl: 'http://localhost:6006',
   browser: [
-    {deviceName: 'iPhone SE'},
+    {deviceName: 'iPhone 5/SE'},
     {width: 1024, height: 768, name: 'firefox'},
     {width: 1024, height: 768, name: 'chrome'},
     {width: 1024, height: 768, name: 'safari'},
