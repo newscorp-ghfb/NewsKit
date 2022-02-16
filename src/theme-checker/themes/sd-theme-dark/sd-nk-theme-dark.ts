92c957ddc89b3cb428fb6ea7349f9429ff5dc34c
Vanals <marco91vanali@gmail.com>
2022-02-16 10:55:19 +0000

    spike(PPDSC-1992): moved import under ts-ignore and comments

--- src/theme-checker/themes/sd-theme-dark/sd-nk-theme-dark.ts
+++ src/theme-checker/themes/sd-theme-dark/sd-nk-theme-dark.ts
@@ -1,15 +1,11 @@
-import {NkDocThemes} from 'package-name';
 import {createTheme} from '../../../theme';
 import {newskitLightTheme} from '../../..';
 
-// import theme from './theme.json';
-
 // TODO ts complaining?
-
 // TODO user can currently import the package without {} and use any name.
 // To hardcode the default name.
-
 // @ts-ignore
+import {NkDocThemes} from 'package-name';
 
 export const sdThemeDark = {
   ...NkDocThemes['NK-Dark'],
--- src/theme-checker/themes/sd-theme-light/sd-nk-theme-light.ts
+++ src/theme-checker/themes/sd-theme-light/sd-nk-theme-light.ts
@@ -1,14 +1,11 @@
-import {NkDocThemes} from 'package-name';
 import {createTheme} from '../../../theme';
 import {newskitLightTheme} from '../../..';
-// import theme from './theme.json';
 
 // TODO ts complaining?
-
 // TODO user can currently import the package without {} and use any name.
 // To hardcode the default name.
-
 // @ts-ignore
+import {NkDocThemes} from 'package-name';
 
 export const sdThemeLight = {
   ...NkDocThemes['NK-Light'],
