0667dabb7a08d5f5f162e487f24d57e8e8426ff4
Aysha <ayshababur@yahoo.com>
2022-03-03 16:12:59 +0000

    feat(PPDSC-2032): feat-ppdse-2032-added function

--- src/audio-player-composable/components/time-display.tsx
+++ src/audio-player-composable/components/time-display.tsx
@@ -1,19 +1,37 @@
 import React, {useContext} from 'react';
-import {styled} from '../../utils';
+import {SliderProps} from '../..';
+import {seekBarAriaValueText} from '../../audio-player/utils';
+import { getLabelMargin } from '../../slider/styled';
+import {MQ, styled} from '../../utils';
 import {AudioPlayerContext} from '../context';
 
-interface StyledSliderLabelProps {
-  duration?: number;
-  trackPositionArr?: number[];
+interface StyledLabelProps {
+  length?: number;
+  format?: object;
+  currentTime?: number[];
+  overrides?: {
+      typographyPreset: MQ<string>;
+      stylePreset: MQ<string>;
+    };
 }
+const calculateTime = (secs: number) => {
+  const minutes = Math.floor(secs / 60);
+  const seconds = Math.floor(secs % 60);
+  const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  const returnedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  return `${returnedMinutes}:${returnedSeconds}`;
+};
 
-const TimeDisplayLabel = styled.label<StyledSliderLabelProps>``;
+const TimeDisplayLabel = styled.label<StyledLabelProps>``;
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const TimeDisplay: any = () => {
+export const TimeDisplay = ({format, overrides}: StyledLabelProps) => {
   const {duration, trackPositionArr} = useContext(AudioPlayerContext);
+  const currentTime = calculateTime((trackPositionArr as unknown) as number);
+  const length = calculateTime((duration as unknown) as number);
   return (
-    <TimeDisplayLabel duration={duration} trackPositionArr={trackPositionArr}>
-      Time goes here {trackPositionArr} {duration}
+    <TimeDisplayLabel >
+      {currentTime} / {length}
     </TimeDisplayLabel>
   );
 };
