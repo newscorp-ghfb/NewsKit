e8cc2a88c127f525867b9b2c844c5a22ad949f7b
Marco Vanali <Marco91Vanali@gmail.com>
2022-03-03 11:21:16 +0000

    Feat/ppdsc 2011 audio player playpause (#95)
    * feat(PPDSC-2011): commented unused code
    
    * feat(PPDSC-2011): passed onWaiting and fixed snap
    
    * feat(PPDSC-2011): added tests renamed stuff
    
    * feat(PPDSC-2011): linted
    
    * feat(PPDSC-2011): added temp eslint ignores
    
    * feat(PPDSC-2011): made landmarks unique
    
    * feat(PPDSC-2011): improved testing, a pain
    
    * feat(PPDSC-2011): fixed types, using getters all over, removed usercomps
    
    * feat(PPDSC-2011): added landmark
    
    * feat(PPDSC-2011): updated snaps
    
    * feat(PPDSC-2011): skiped tests for later tickets
    
    * feat(PPDSC-2011): improved args for getter play btn props
    
    * feat(PPDSC-2011): improved test coverage
    
    * feat(PPDSC-2011): fixed last testr coverage
    
    * feat(PPDSC-2011): renamed customer to consumer

---
+++ src/audio-player-composable/context.tsx
@@ -0,0 +1,44 @@
+import {ReactJSXElement} from '@emotion/react/types/jsx-namespace';
+import React, {createContext, SyntheticEvent} from 'react';
+
+interface AudioPlayerProviderContext {
+  id: string;
+  src: string;
+  autoPlay?: boolean;
+  // Internal
+  audioRef: React.RefObject<HTMLAudioElement>;
+  playing: boolean;
+  canPause: boolean;
+  loading: boolean;
+  togglePlay: () => void;
+  audioEvents: {
+    onCanPlay: () => void;
+    onDurationChange: ({
+      target,
+    }: SyntheticEvent<HTMLAudioElement, Event>) => void;
+    onTimeUpdate: ({target}: SyntheticEvent<HTMLAudioElement, Event>) => void;
+    onWaiting: ({target}: SyntheticEvent<HTMLAudioElement, Event>) => void;
+    onEnded: ({target}: SyntheticEvent<HTMLAudioElement, Event>) => void;
+  };
+  duration: number;
+  trackPositionArr: number[];
+  onChangeSlider: (values: number[]) => void;
+  // Getter functions
+  getPlayPauseButtonProps: (args: {
+    onClick?: () => void;
+  }) => {
+    ariaLabel: string;
+    ariaPressed: boolean;
+    loading: boolean;
+    onClick: () => void;
+    playing: boolean;
+    canPause: boolean;
+    playStateIcon: ReactJSXElement;
+  };
+}
+
+export const AudioPlayerContext = createContext<
+  Partial<AudioPlayerProviderContext>
+>({});
+
+export const AudioPlayerProvider = AudioPlayerContext.Provider;
