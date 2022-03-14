779a2832ad3ebd1ab69a60a0aed4dbf59a7e4e47
Aysha <ayshababur@yahoo.com>
2022-03-14 10:27:12 +0000

    feat(PPDSC-2032): feat-ppdse-2032-updaated context

--- src/audio-player-composable/context.tsx
+++ src/audio-player-composable/context.tsx
@@ -54,4 +54,19 @@ export const AudioPlayerContext = createContext<
 
 export const AudioPlayerProvider = AudioPlayerContext.Provider;
 
-export const useAudioPlayerContext = () => useContext(AudioPlayerContext);
+export const useAudioPlayerContext = () => {
+  const context = useContext(AudioPlayerContext);
+
+  /* istanbul ignore if */
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    Object.keys(context).length === 0
+  ) {
+    // eslint-disable-next-line no-console
+    console.error(
+      'You are using a component which needs to be a child of <AudioPlayerComposable />',
+    );
+  }
+
+  return context;
+};
