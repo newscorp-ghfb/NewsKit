0cf0fbb3c2b7aa61f4e5a3774e0efeac11e9cfbe
Stoyan Delev <stoyan.delev@news.co.uk>
2022-03-15 08:11:02 +0200

    feat(PPDSC-2020): add rest props to radio-group

--- src/radio-button/types.ts
+++ src/radio-button/types.ts
@@ -19,4 +19,4 @@ export type RadioGroupProps = {
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   name?: string;
   children?: React.ReactNode;
-};
+} & React.HTMLAttributes<HTMLDivElement>;
