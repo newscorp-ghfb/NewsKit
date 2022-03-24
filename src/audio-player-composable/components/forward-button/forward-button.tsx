cd5601a699463d2b8f955d196a81769b435985a3
baburay23 <54642337+baburay23@users.noreply.github.com>
2022-03-24 14:06:40 +0000

    feat(PPDSC-2049): audio player replay forward buttons (#122)
    * feat(PPDSC-2049): feat-ppdse-2049-added-button
    
    * feat(PPDSC-2049): feat-ppdse-2049-working with onclick
    
    * feat(PPDSC-2049): feat-ppdse-2049-added-back button
    
    * feat(PPDSC-2049): feat-ppdse-2049 renamed button
    
    * feat(PPDSC-2049): feat-ppdse-2049 updated index file and types
    
    * feat(PPDSC-2049): feat-ppdse-2049 updated context /stories
    
    * feat(PPDSC-2049): feat-ppdse-2049 updated stories
    
    * feat(PPDSC-2049): feat-ppdse-2049 linted
    
    * feat(PPDSC-2049): feat-ppdse-2049 overrides added
    
    * feat(PPDSC-2049): feat-ppdse-2049-updated test id name
    
    * feat(PPDSC-2049): feat-ppdse-2049-at 100% cov
    
    * feat(PPDSC-2049): feat-ppdse-2049-updated aria-land
    
    * feat(PPDSC-2049): feat-ppdse-2049-exported
    
    * feat(PPDSC-2049): feat-ppdse-2049-changed up getters
    
    * feat(PPDSC-2049): feat-ppdse-2049-updated tests
    
    * feat(PPDSC-2049): feat-ppdse-2049-fixed grid
    
    * feat(PPDSC-2049): feat-ppdse-2049-updated tests
    
    * feat(PPDSC-2049): feat-ppdse-2049-added cypress tesr
    
    * feat(PPDSC-2049): feat-ppdse-2049-fixed story
    
    * feat(PPDSC-2049): feat-ppdse-2049-added space pn e2e
    
    * feat(PPDSC-2049): feat-ppdse-2049-adderessed comments
    
    * feat(PPDSC-2049): feat-ppdse-2049-fixed stories
    
    * feat(PPDSC-2049): feat-ppdse-2049-fixed
    
    * feat(PPDSC-2049): feat-ppdse-2049-updated snaps
    
    * feat(PPDSC-2049): feat-ppdse-2049-aria-landmark

---
+++ src/audio-player-composable/components/forward-button/forward-button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {ButtonOverrides, ButtonSize} from '../../../button/types';
+import {IconButton} from '../../../icon-button';
+import {useTheme} from '../../../theme';
+import {filterOutFalsyProperties} from '../../../utils/filter-object';
+import {withOwnTheme} from '../../../utils/with-own-theme';
+import {useAudioPlayerContext} from '../../context';
+import {AudioPlayerIconButtonProps} from '../../types';
+import defaults from './defaults';
+
+const ThemelessAudioPlayerForwardButton: React.FC<AudioPlayerIconButtonProps> = React.memo(
+  ({overrides, ...props}) => {
+    const {getForwardButtonProps} = useAudioPlayerContext();
+
+    const theme = useTheme();
+
+    const audioPlayerForwardOverrides: ButtonOverrides = {
+      ...theme.componentDefaults.audioPlayerForwardButton,
+      ...filterOutFalsyProperties(overrides),
+    };
+
+    const propsFromContex =
+      getForwardButtonProps! && getForwardButtonProps(props);
+    return (
+      <IconButton
+        data-testid="audio-player-forward-button"
+        size={ButtonSize.Medium}
+        overrides={audioPlayerForwardOverrides}
+        {...propsFromContex}
+      />
+    );
+  },
+);
+
+export const AudioPlayerForwardButton = withOwnTheme(
+  ThemelessAudioPlayerForwardButton,
+)({
+  defaults,
+});
---
+++ src/audio-player-composable/components/replay-button/replay-button.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {ButtonOverrides, ButtonSize} from '../../../button/types';
+import {IconButton} from '../../../icon-button';
+import {useTheme} from '../../../theme';
+import {filterOutFalsyProperties} from '../../../utils/filter-object';
+import {withOwnTheme} from '../../../utils/with-own-theme';
+import {useAudioPlayerContext} from '../../context';
+import {AudioPlayerIconButtonProps} from '../../types';
+import defaults from './defaults';
+
+const ThemelessAudioPlayerReplayButton: React.FC<AudioPlayerIconButtonProps> = React.memo(
+  ({overrides, ...props}) => {
+    const {getReplayButtonProps} = useAudioPlayerContext();
+
+    const theme = useTheme();
+
+    const audioPlayerReplayOverrides: ButtonOverrides = {
+      ...theme.componentDefaults.audioPlayerReplayButton,
+      ...filterOutFalsyProperties(overrides),
+    };
+
+    const propsFromContex =
+      getReplayButtonProps! && getReplayButtonProps(props);
+    return (
+      <IconButton
+        data-testid="audio-player-replay-button"
+        size={ButtonSize.Medium}
+        overrides={audioPlayerReplayOverrides}
+        {...propsFromContex}
+      />
+    );
+  },
+);
+
+export const AudioPlayerReplayButton = withOwnTheme(
+  ThemelessAudioPlayerReplayButton,
+)({
+  defaults,
+});
