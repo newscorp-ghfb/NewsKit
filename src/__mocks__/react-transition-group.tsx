906d418e9793adbe1153eeea6200055745ea782f
Paola Dimova <paola.dimova@news.co.uk>
2022-03-24 15:47:58 +0200

    refactor(PPDSC-2002): jest - mock cleanup

--- src/__mocks__/react-transition-group.tsx
+++ src/__mocks__/react-transition-group.tsx
@@ -1,40 +1,42 @@
 import React, {useEffect} from 'react';
-import {TransitionProps} from 'react-transition-group/Transition';
+import {CSSTransitionProps} from 'react-transition-group/CSSTransition';
 
-// const rtg = jest.createMockFromModule('react-transition-group') as any;
 const rtg = jest.requireActual('react-transition-group');
 rtg.config.disabled = true;
 
 const {CSSTransition} = rtg;
 
-const Helper = ({children, ...rest}: TransitionProps) => {
-  const callIfExist = (props: TransitionProps, method: string) =>
+// http://reactcommunity.org/react-transition-group/testing
+// https://github.com/reactjs/react-transition-group/issues/787
+// It looks like when transitions are disabled (actually disabling only the waiting time, not the transitions themselves)
+// only onEntered() and onExited() callbacks are being called
+// The following function is a hack-ish way to call the rest of the transition lifecycle methods.
+const InterceptedCSSTransition = ({children, ...rest}: CSSTransitionProps) => {
+  const callIfExist = (props: CSSTransitionProps, method: string) =>
     method in props && props[method]();
 
   const onEnter = React.useCallback(() => callIfExist(rest, 'onEnter'), [rest]);
-  const onExited = React.useCallback(() => callIfExist(rest, 'onExited'), [
+  const onEntering = React.useCallback(() => callIfExist(rest, 'onEntering'), [
+    rest,
+  ]);
+  const onExit = React.useCallback(() => callIfExist(rest, 'onExit'), [rest]);
+  const onExiting = React.useCallback(() => callIfExist(rest, 'onExiting'), [
     rest,
   ]);
 
   useEffect(() => {
     if (rest.in) {
       onEnter();
+      onEntering();
     } else {
-      onExited();
+      onExit();
+      onExiting();
     }
-  }, [rest.in, onEnter, onExited]);
-
-  // const Cloned = React.cloneElement(
-  //   CSSTransition,
-  //   [{...rest, ...{pesho: 'hello'}}],
-  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //   [...(children as any)],
-  // );
-  // return Cloned;
+  }, [rest.in, onEnter, onEntering, onExit, onExiting]);
 
   return <CSSTransition {...rest}>{children}</CSSTransition>;
 };
 
-rtg.CSSTransition = Helper;
+rtg.CSSTransition = InterceptedCSSTransition;
 
 module.exports = rtg;
