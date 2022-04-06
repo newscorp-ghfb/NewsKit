768c3b38bf4373f9f7e37884f4a24d93c8109276
Srinivasan-Ramasamy <Srinivasan.Ramasamy@news.co.uk>
2022-04-06 11:30:29 +0530

    docs(PPDSC-1903): select page initial ui changes

--- site/pages/components/select.tsx
+++ site/pages/components/select.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import {LinkInline, InlineMessage, Block} from 'newskit';
+import {Block, LinkInline, InlineMessage, IconFilledInfo} from 'newskit';
 import {MetaStatus} from '../../components/meta/types';
 import {LayoutProps} from '../../components/layout';
 import {ComponentPageTemplate} from '../../templates/component-page-template';
+import {getIllustrationComponent} from '../../components/illustrations/illustration-loader';
+import {Link} from '../../components/link';
+
+const infoIcon = (
+  <IconFilledInfo
+    overrides={{
+      size: 'iconSize020',
+    }}
+  />
+);
 
 const commonPropsRows = [
   {
@@ -289,6 +299,157 @@ const SelectComponent = (layoutProps: LayoutProps) => (
       codeUrl: 'https://github.com/newscorp-ghfb/newskit/tree/main/src/select',
       figmaUrl:
         'https://www.figma.com/file/FSbCQa6SzVR3K48ZWLeD77/NK-Web-Components?node-id=4603%3A124656',
+    }}
+    anatomy={{
+      introduction:
+        'The select component contains five required elements and two optional elements',
+      media: getIllustrationComponent('components/select/anatomy'),
+      rows: [
+        {
+          name: 'Input Container',
+          description: 'Interactable input area',
+          component: ['HTML Input', 'Block'],
+          optional: undefined,
+        },
+        {
+          name: 'Start Enhancer',
+          description:
+            'Used to add a component to the start of the input container. Eg. an Icon or Button',
+          component: 'React.ReactNode',
+          optional: true,
+        },
+        {
+          name: 'Input & Placeholder Text',
+          description:
+            'Input text - a value the user has entered into a input. Placeholder text - a short hint that describes the expected value of an input',
+          component: 'Text block',
+          optional: undefined,
+        },
+        {
+          name: 'Validation icon',
+          description:
+            'An icon used to indicate if the input is required and in a valid or invalid state. Validation is set on the Form',
+          component: 'Icon',
+          optional: undefined,
+        },
+        {
+          name: 'End Enhancer',
+          description:
+            'Used to add a component to the end of the input container. Eg. an Icon or Button',
+          component: 'React.Reactnode',
+          optional: true,
+        },
+        {
+          name: 'Panel',
+          description: 'Container that contained a list of option items',
+          component: 'Block',
+          optional: undefined,
+        },
+        {
+          name: 'Option Item',
+          description: 'Option item that can be selected from multiple options',
+          component: 'Div',
+          optional: undefined,
+        },
+      ],
+    }}
+    options={{
+      introduction:
+        'The Radio Button has options that can be used to provide an appropriate experience for different use cases.',
+      cards: [
+        {
+          title: 'Size',
+          description:
+            'There are three sizes of Select; small, medium, and large. The input container, placeholder/input text, start and end enhancers change size. Selects match the same height as the Button and other inputs such as the Text Field, to align when used together.',
+          media: getIllustrationComponent('components/select/options/size'),
+        },
+        {
+          title: 'Select for mobile',
+          description: (
+            <>
+              There is an option for Select options to appear in a Modal (with
+              overlay), which is intended for long lists of options eg.
+              presenting a list of countries to select from in a{' '}
+              <Link href="/components/radio-button/">Form.</Link>
+            </>
+          ),
+          media: getIllustrationComponent('components/select/options/mobile'),
+        },
+        {
+          title: 'Width',
+          description:
+            'The width of a Select can be customised appropriately for its context, using full-width or a fixed-width value.',
+          media: getIllustrationComponent('components/select/options/width'),
+        },
+        {
+          title: 'Min-height',
+          description:
+            'The minimum height of a Select input can be customised appropriately for its context. Additionally, the minimum height of the Select panel (containing the option items) can be customised appropriately for its context.',
+          media: getIllustrationComponent(
+            'components/select/options/min-height',
+          ),
+        },
+        {
+          title: 'Placeholder text',
+          description: (
+            <>
+              <Block spaceStack="space030">
+                Placeholder text can be displayed to provide the user with a
+                short hint that describes the content that is expected to be
+                inputted by the user (e.g. a sample value or a short description
+                of the expected format).
+                <br />
+                <br />
+                The short hint is displayed in the input container before the
+                user enters a value.
+              </Block>
+              <InlineMessage
+                icon={infoIcon}
+                role="region"
+                aria-label="Placeholder accessible"
+              >
+                Placeholder text is not accessible; use assistive text when
+                providing instructions on completing a Select for clarity.
+              </InlineMessage>
+            </>
+          ),
+          media: getIllustrationComponent(
+            'components/select/options/placeholder-text',
+          ),
+        },
+        {
+          title: 'Start enhancer',
+          description:
+            'The Select supports start enhancer property that allows for a component to be added to the start or end of the input container, for example, an icon.',
+          media: getIllustrationComponent(
+            'components/select/options/start-enhancer',
+          ),
+        },
+        {
+          title: 'End enhancer',
+          description:
+            'The Select supports end enhancer property that allows for a component to be added to the start or end of the input container, for example, an icon.',
+          media: getIllustrationComponent(
+            'components/select/options/end-enhancer',
+          ),
+        },
+        {
+          title: 'Pre-selected options',
+          description:
+            'A pre-selected option can be set on the Select. When specified, the option is displayed in the Select upon page load.',
+          media: getIllustrationComponent(
+            'components/select/options/pre-selected-option',
+          ),
+        },
+        {
+          title: 'Custom selected display',
+          description:
+            'The Select option item and the Select input can be defined independently. For example, a list of countries may be displayed in a text format in the Select option items and upon user selection, the selected item may display a country flag in the Select input.',
+          media: getIllustrationComponent(
+            'components/select/options/custom-selected-display',
+          ),
+        },
+      ],
     }}
     componentAPI={{
       introduction: (
