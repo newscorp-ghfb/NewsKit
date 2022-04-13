f3d1685adb08f2c3497e19d516f43732a0c7ca10
timbradbury <bradburyt8@gmail.com>
2022-04-13 11:29:23 +0100

    fix(PPDSC-2081): revert chang to html meta

--- site/components/html-meta.tsx
+++ site/components/html-meta.tsx
@@ -7,31 +7,31 @@ export const HTMLMeta = (): JSX.Element => (
       content="width=device-width, initial-scale=1"
       key="viewport"
     />
-    <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="static/favicon.ico" />
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="/static/apple-touch-icon.png"
+      href="static/apple-touch-icon.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="16x16"
-      href="/static/favicon-16x16.png"
+      href="static/favicon-16x16.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="32x32"
-      href="/static/favicon-32x32.png"
+      href="static/favicon-32x32.png"
     />
     <link
       rel="icon"
       type="image/png"
       sizes="192x192"
-      href="/static/android-chrome-192x192.png"
+      href="static/android-chrome-192x192.png"
     />
-    <link rel="manifest" href="/static/site.webmanifest" />
+    <link rel="manifest" href="static/site.webmanifest" />
     <meta name="msapplication-TileColor" content="#ffffff" />
     <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
     <meta name="theme-color" content="#ffffff" />
