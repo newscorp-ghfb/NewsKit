e5e45b9e393310f4ef9a21081bbd4a9f73258443
Stoyan Delev <stoyan.delev@news.co.uk>
2022-04-14 15:00:05 +0300

    feat(PPDSC-2005): add unit tests to layer

--- src/layer/__tests__/layer.test.tsx
+++ src/layer/__tests__/layer.test.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  renderInBody,
-  renderToFragmentInBody,
-  renderWithThemeInBody,
-} from '../../test/test-utils';
+import {renderInBody, renderToFragmentInBody} from '../../test/test-utils';
 import {Layer} from '../layer';
 import {LayerOrganizer} from '../layer-organizer';
 
@@ -23,7 +19,7 @@ describe('Layer', () => {
     const LayerExample = () => (
       <>
         <div>outside the layer</div>
-        <Layer>inside layer</Layer>
+        <Layer className="some-class-1 some-class-2">inside layer</Layer>
       </>
     );
     const fragment = renderToFragmentInBody(LayerExample);
@@ -43,7 +39,7 @@ describe('Layer', () => {
     expect(fragment).toMatchSnapshot();
   });
 
-  test.only('add zIndex to layer organizer', () => {
+  test('add zIndex to layer organizer', () => {
     const LayerExample = () => (
       <LayerOrganizer zIndex={100}>
         <Layer>layer</Layer>
@@ -53,11 +49,61 @@ describe('Layer', () => {
     expect(fragment).toMatchSnapshot();
   });
 
-  test('multiple layers at same level', () => {});
+  test('multiple layers at same level', () => {
+    const LayerExample = () => (
+      <>
+        <div>outside the layer</div>
+        <Layer>inside layer 1</Layer>
+        <Layer>inside layer 2</Layer>
+      </>
+    );
+    const fragment = renderToFragmentInBody(LayerExample);
+    expect(fragment).toMatchSnapshot();
+  });
 
-  test('nested layers', () => {});
+  test('nested layers', () => {
+    const LayerExample = () => (
+      <>
+        <div>outside the layer</div>
+        <Layer>
+          <div>inside layer lvl 1</div>
+          <Layer>nested layer lvl 2</Layer>
+        </Layer>
+      </>
+    );
+    const fragment = renderToFragmentInBody(LayerExample);
+    expect(fragment).toMatchSnapshot();
+  });
 
-  test('append layer', () => {});
+  test('append layer', () => {
+    const LayerExample = () => {
+      const ref = React.useRef<HTMLDivElement>(null);
+      return (
+        <>
+          <div ref={ref}>parent</div>
+          <Layer appendToRef={ref}>Child layer A</Layer>
+          <Layer appendToRef={ref}>Child layer B</Layer>
+        </>
+      );
+    };
+    const fragment = renderToFragmentInBody(LayerExample);
+    expect(fragment).toMatchSnapshot();
+  });
 
-  test('append nested layers', () => {});
+  test('append nested layers', () => {
+    const LayerExample = () => {
+      const ref = React.useRef<HTMLDivElement>(null);
+      return (
+        <>
+          <div ref={ref}>parent</div>
+          <Layer>
+            <Layer appendToRef={ref}>Child layer A</Layer>
+            <Layer appendToRef={ref}>Child layer B</Layer>
+          </Layer>
+        </>
+      );
+    };
+    const fragment = renderToFragmentInBody(LayerExample);
+    expect(fragment).toMatchSnapshot();
+  });
 });
