ebe1334a335daa9a3bf81cfda81345db019817a5
Srinivasan-Ramasamy <Srinivasan.Ramasamy@news.co.uk>
2022-04-20 10:26:40 +0530

    docs(PPDSC-2075): ui changes on option section

--- site/pages/components/button.tsx
+++ site/pages/components/button.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import {Block, InlineMessage, IconFilledInfo} from 'newskit';
+import {Link} from '../../components/link';
 import {MetaStatus} from '../../components/meta/types';
 import {LayoutProps} from '../../components/layout';
 import {ComponentPageTemplate} from '../../templates/component-page-template';
 import {getIllustrationComponent} from '../../components/illustrations/illustration-loader';
+
+const infoIcon = (
+  <IconFilledInfo
+    overrides={{
+      size: 'iconSize020',
+    }}
+  />
+);
 
 const ButtonComponent = (layoutProps: LayoutProps) => (
   <ComponentPageTemplate
@@ -62,6 +72,96 @@ const ButtonComponent = (layoutProps: LayoutProps) => (
         },
       ],
       media: getIllustrationComponent('components/button/anatomy-button'),
+    }}
+    options={{
+      introduction:
+        'The Button has options that can be used to provide an appropriate experience for different use cases.',
+      cards: [
+        {
+          title: 'Appearance',
+          description: (
+            <>
+              <Block spaceStack="space030">
+                By default, there are three styles of Button; solid, outlined,
+                and minimal.
+              </Block>
+              <InlineMessage
+                icon={infoIcon}
+                role="region"
+                aria-label="Inverse Buttons"
+                title="Note"
+              >
+                Inverse Buttons are intended for use on darker backgrounds.
+              </InlineMessage>
+            </>
+          ),
+          media: getIllustrationComponent(
+            'components/button/options/appearence',
+          ),
+        },
+        {
+          title: 'Intent',
+          description:
+            'The Button has four different intents: primary, secondary, positive, and negative. Each intent is used to communicate a specific semantic tone of the Button to the user.',
+          media: getIllustrationComponent('components/button/options/intent'),
+        },
+        {
+          title: 'Size',
+          description:
+            'There are three sizes of Button: small, medium (default), and large.',
+          media: getIllustrationComponent('components/button/options/size'),
+        },
+        {
+          title: 'Border radius and width',
+          description: (
+            <>
+              The Border Radius and Border Width applied to the Button can be
+              overridden and applied to the theme. <br />
+              <br />
+              <Link href="/theme/borders/">
+                For more information, refer to the Borders foundations.
+              </Link>
+            </>
+          ),
+          media: getIllustrationComponent(
+            'components/button/options/border-radius-and-width',
+          ),
+        },
+        {
+          title: 'Icons (leading & trailing)',
+          description:
+            'Icons can be displayed in a Button and can be positioned either before (leading) or after (trailing) the Label in the Button.',
+          media: getIllustrationComponent(
+            'components/button/options/icons-leading-trailing',
+          ),
+        },
+        {
+          title: 'Label',
+          description:
+            'Labels can be displayed in a Button. A Label can give more context to what the intended action the Button indicates.',
+          media: getIllustrationComponent('components/button/options/label'),
+        },
+        {
+          title: 'Icon Button',
+          description:
+            'The Icon Button component has the same options, behaviours, and properties as the Button component, but with a single icon positioned in the centre of the container.',
+          media: getIllustrationComponent('components/button/options/icons'),
+        },
+        {
+          title: 'Button as a link',
+          description: (
+            <>
+              Buttons have the ability to render as{' '}
+              <Link href="/components/link/">Links.</Link> In this case, the
+              styling of the link looks like a Button but with the behaviour of
+              a link.
+            </>
+          ),
+          media: getIllustrationComponent(
+            'components/button/options/button-link',
+          ),
+        },
+      ],
     }}
   />
 );
