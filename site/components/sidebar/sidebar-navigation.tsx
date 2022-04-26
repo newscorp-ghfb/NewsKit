dd3c1c38b481ef81c9acef016bf74ac8db0abb16
Aysha <ayshababur@yahoo.com>
2022-04-26 09:41:19 +0100

    feat(PPDSC-2064): feat-ppdse/2064 sidebar nav has menu

--- site/components/sidebar/sidebar-navigation.tsx
+++ site/components/sidebar/sidebar-navigation.tsx
@@ -1,24 +1,15 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 import {useRouter} from 'next/router';
-import {Divider, getSpacingCssFromTheme, styled} from 'newskit';
+import {
+  Divider,
+  getSpacingCssFromTheme,
+  Menu,
+  MenuGroup,
+  MenuItem,
+  styled,
+} from 'newskit';
 
-import {Link} from '../link';
 import routes from '../../routes';
-import {
-  StyledSectionContainer,
-  StyledNavigationWrapper,
-  StyledLinkItem,
-  StyledSidebarNav,
-  StyledSecondLevelHeader,
-  StyledNavigationSection,
-} from './styled';
-import {
-  PageLinkProps,
-  NavigationSectionProps,
-  SectionProps,
-  NavigationSectionType,
-  PageType,
-} from './types';
 import {Visible} from '../../../src/grid/visibility';
 import {MenuNavCollapsible} from '../menu-collapsible/menu-collapsible';
 
@@ -29,114 +20,101 @@ export const DesktopNavigationDivider = styled.div`
   ${getSpacingCssFromTheme('marginTop', 'space045')};
   ${getSpacingCssFromTheme('marginBottom', 'space060')};
 `;
-
-const StyledHeightDiv = styled.div``;
-const PageLink: React.FC<PageLinkProps> = ({page, active}) => {
-  const ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (active && ref && ref.current) {
-      ref.current.scrollIntoView({block: 'center'});
-    }
-  });
-
-  return (
-    // can add height here if needed
-    <StyledHeightDiv ref={ref}>
-      <Link
-        type="standalone"
-        href={page.id}
-        overrides={{stylePreset: 'linkNoUnderline'}}
-      >
-        <StyledLinkItem
-          data-testid={page.id}
-          $selected={active}
-          className={active ? 'selected' : undefined}
-        >
-          {page.title}
-        </StyledLinkItem>
-      </Link>
-    </StyledHeightDiv>
-  );
-};
-
-const NavigationSection: React.FC<NavigationSectionProps> = ({
-  navigationSection,
-  activePagePath,
-}) => (
-  <>
-    <StyledSecondLevelHeader id={navigationSection.id}>
-      {navigationSection.title}
-    </StyledSecondLevelHeader>
-    {navigationSection.subNav.map(page => (
-      <PageLink
-        key={page.id}
-        page={page}
-        active={activePagePath.includes(page.id)}
-      />
-    ))}
-    <Visible lg xl>
-      <DesktopNavigationDivider>
-        <Divider />
-      </DesktopNavigationDivider>
-    </Visible>
-  </>
-);
-
-const Section: React.FC<SectionProps> = ({section, activePagePath}) => (
-  <StyledSectionContainer>
-    <StyledNavigationSection>
-      {section.subNav.map(subNav => {
-        if ((subNav as NavigationSectionType).subNav) {
-          return (
-            <NavigationSection
-              key={subNav.id}
-              navigationSection={subNav as NavigationSectionType}
-              activePagePath={activePagePath}
-            />
-          );
-        }
-        return (
-          <PageLink
-            key={subNav.id}
-            page={subNav as PageType}
-            // TODO: can rollback to subNav.id === activePagePath after https://nidigitalsolutions.jira.com/browse/PPDSE-312
-            active={activePagePath.includes(subNav.id)}
-          />
-        );
-      })}
-    </StyledNavigationSection>
-  </StyledSectionContainer>
-);
-
-export const SidebarNav = () => {
+const Test = () => {
   const path = useRouter().pathname;
-
   const currentRoute = path.match(/\/[A-z\d-]*/g);
   const currentSection =
     currentRoute && routes.filter(({id}) => id === currentRoute[0]);
 
+  type NavProps = {
+    title: string;
+    id: string;
+    description?: string;
+    page?: boolean;
+  };
+
+  type SubNavProps = {
+    subNav?: NavProps[];
+  };
+
+  type SubNavItemProps = NavProps & SubNavProps;
+
+  const createMenuItem = (list: SubNavItemProps[]) => (
+    <>
+      {list &&
+        list.map(({title, id, subNav, page}) => (
+          <React.Fragment key={id}>
+            {page ? (
+              <>
+                <MenuItem
+                  href={id}
+                  overrides={{
+                    typographyPreset: 'utilityButton020',
+                    spaceInset: 'space030',
+                    stylePreset: 'sidebarNavItem',
+                  }}
+                  selected={path.includes(id)}
+                  size="small"
+                  className={path.includes(id) ? 'selected' : undefined}
+                >
+                  {title}
+                </MenuItem>
+              </>
+            ) : (
+              <>
+                <DesktopNavigationDivider>
+                  <Divider />
+                </DesktopNavigationDivider>
+                <MenuGroup
+                  title={title}
+                  overrides={{
+                    title: {
+                      typographyPreset: {
+                        xs: 'utilityHeading010',
+                        lg: 'utilityHeading030',
+                      },
+                      stylePreset: 'inkContrast',
+                      spaceInset: 'space030',
+                      spaceInline: 'space040',
+                    },
+                  }}
+                >
+                  {subNav && createMenuItem(subNav)}
+                </MenuGroup>
+              </>
+            )}
+          </React.Fragment>
+        ))}
+    </>
+  );
   return (
     <>
-      <StyledSidebarNav role="navigation" aria-label="Sidebar">
-        <>
-          <Visible xs sm md>
-            <MenuNavCollapsible path={path} menu={routes} />
-          </Visible>
-          <Visible lg xl>
-            <StyledNavigationWrapper role="list">
-              {currentSection &&
-                currentSection.map(section => (
-                  <Section
-                    key={section.id}
-                    section={section}
-                    activePagePath={path}
-                  />
-                ))}
-            </StyledNavigationWrapper>
-          </Visible>
-        </>
-      </StyledSidebarNav>
+      <Menu
+        aria-label="menu"
+        vertical
+        size="small"
+        align="start"
+        overrides={{spaceInline: 'space000'}}
+      >
+        {currentSection &&
+          currentSection.map(({title, subNav}) => (
+            <div key={title}> {subNav && createMenuItem(subNav)}</div>
+          ))}
+      </Menu>
+    </>
+  );
+};
+export const SidebarNav = () => {
+  const path = useRouter().pathname;
+
+  return (
+    <>
+      <Visible xs sm md>
+        <MenuNavCollapsible path={path} menu={routes} />
+      </Visible>
+      <Visible lg xl>
+        <Test />
+      </Visible>
     </>
   );
 };
