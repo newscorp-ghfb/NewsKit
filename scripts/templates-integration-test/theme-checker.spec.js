87e2b5613c40baebb03e1db686fd7d627f662311
Vanals <marco91vanali@gmail.com>
2022-05-03 16:23:51 +0100

    ci(PPDSC-2017): improve comment in makefile, yalc publish and spec fix

--- scripts/templates-integration-test/theme-checker.spec.js
+++ scripts/templates-integration-test/theme-checker.spec.js
@@ -1,6 +1,6 @@
 describe('Theme Checker', () => {
   it('Visits the home page and expect to find a play pause button', () => {
-    cy.visit();
+    cy.visit('/');
     cy.get('[data-testid="audio-player-play-button"]');
   });
 });
