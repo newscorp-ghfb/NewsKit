ee04f74a7459fe420cfb8aa1d917fa304d0c4dd2
Stoyan Delev <stoyan.delev@news.co.uk>
2022-05-09 16:01:38 +0200

    feat(PPDSC-2005): fix e2e tests

--- src/drawer/__tests__/drawer.stories.tsx
+++ src/drawer/__tests__/drawer.stories.tsx
@@ -47,42 +47,41 @@ const BoxWithContent = ({open}: {open?: () => void}) => (
 );
 
 const DrawerContent = () => (
-  <div>Content</div>
-  // <>
-  //   <p>
-  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet lorem
-  //     massa, et lacinia ipsum tristique id. Phasellus sed posuere lacus.
-  //     Pellentesque eu odio <Link href="/">Test link 1</Link> sapien. Donec
-  //     finibus pellentesque est porta dictum. Suspendisse venenatis vitae augue
-  //     nec hendrerit. In ut quam tempus, feugiat risus quis, porta eros. Aliquam
-  //     ultricies ac orci viverra gravida. Ut sodales odio tempor sodales viverra.
-  //     In condimentum tincidunt fermentum. Nullam imperdiet est vel tincidunt
-  //     suscipit. Vestibulum vel pulvinar nibh, at molestie lectus. Curabitur
-  //     ultricies massa eu sem varius volutpat. Ut vitae purus et enim imperdiet
-  //     finibus. Quisque posuere lacus a nunc tempor accumsan. Aliquam odio nunc,
-  //     interdum.
-  //   </p>
-  //   <TextInput label="First name" />
-  //   <TextInput label="Last name" />
-  //   <TextInput label="Phone number" />
-  //   <div>
-  //     <Link href="/">For more information...</Link>{' '}
-  //   </div>
-  //   <p>
-  //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id
-  //     scelerisque sapien. Praesent mollis vestibulum nunc at blandit. Donec
-  //     vitae venenatis mi. Aenean ut ornare diam, non facilisis diam.
-  //     Pellentesque consequat mi in imperdiet ultrices. Sed vitae erat ac urna{' '}
-  //     <Link href="/">Test link 2</Link> rutrum aliquet eu mattis ligula. Sed
-  //     dapibus, enim sed tristique gravida, nisl dolor malesuada lacus, quis
-  //     auctor dui mauris eu odio. Vivamus eu augue et enim varius viverra.
-  //     Vivamus ut tellus iaculis, ullamcorper ligula sit amet, posuere ipsum.
-  //   </p>
-  //   <div>
-  //     <Button>Remind me later</Button>
-  //     <Button>Ok</Button>
-  //   </div>
-  // </>
+  <>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet lorem
+      massa, et lacinia ipsum tristique id. Phasellus sed posuere lacus.
+      Pellentesque eu odio <Link href="/">Test link 1</Link> sapien. Donec
+      finibus pellentesque est porta dictum. Suspendisse venenatis vitae augue
+      nec hendrerit. In ut quam tempus, feugiat risus quis, porta eros. Aliquam
+      ultricies ac orci viverra gravida. Ut sodales odio tempor sodales viverra.
+      In condimentum tincidunt fermentum. Nullam imperdiet est vel tincidunt
+      suscipit. Vestibulum vel pulvinar nibh, at molestie lectus. Curabitur
+      ultricies massa eu sem varius volutpat. Ut vitae purus et enim imperdiet
+      finibus. Quisque posuere lacus a nunc tempor accumsan. Aliquam odio nunc,
+      interdum.
+    </p>
+    <TextInput label="First name" />
+    <TextInput label="Last name" />
+    <TextInput label="Phone number" />
+    <div>
+      <Link href="/">For more information...</Link>{' '}
+    </div>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id
+      scelerisque sapien. Praesent mollis vestibulum nunc at blandit. Donec
+      vitae venenatis mi. Aenean ut ornare diam, non facilisis diam.
+      Pellentesque consequat mi in imperdiet ultrices. Sed vitae erat ac urna{' '}
+      <Link href="/">Test link 2</Link> rutrum aliquet eu mattis ligula. Sed
+      dapibus, enim sed tristique gravida, nisl dolor malesuada lacus, quis
+      auctor dui mauris eu odio. Vivamus eu augue et enim varius viverra.
+      Vivamus ut tellus iaculis, ullamcorper ligula sit amet, posuere ipsum.
+    </p>
+    <div>
+      <Button>Remind me later</Button>
+      <Button>Ok</Button>
+    </div>
+  </>
 );
 
 export default {
