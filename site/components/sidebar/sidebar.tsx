faa9f597ff60a7d000b300acf0a386b3f025285c
Aysha <ayshababur@yahoo.com>
2022-05-30 16:56:28 +0100

    docs(PPDSC-2064): docs-ppdse-2064-removed mobile switch

--- site/components/sidebar/sidebar.tsx
+++ site/components/sidebar/sidebar.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {Block, Visible} from 'newskit';
 import {SidebarNav} from './sidebar-navigation';
-import {ThemeSwitch} from '../theme-switch';
 import {GitHubLaunch} from '../menu-collapsible/menu-collapsible';
 import {StyledSidebarDesktop, StyledDrawer} from './styled';
 
@@ -9,69 +8,54 @@ interface SidebarProps {
   sidebarOpen: boolean;
   handleSidebarClick: () => void;
   hideSidebar?: boolean;
-  toggleTheme: () => void;
-  themeMode: string;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({
   sidebarOpen,
-  toggleTheme,
-  themeMode,
   handleSidebarClick,
   hideSidebar,
-}) => {
-  const handleOnClickEvents = () => {
-    toggleTheme();
-    handleSidebarClick();
-  };
-  return (
-    <>
-      <Visible xs sm md>
-        <StyledDrawer
-          data-testid="sidebar"
-          aria-label="drawer menu on the left"
-          open={sidebarOpen}
-          onDismiss={handleSidebarClick}
-          placement="left"
-          closePosition="none"
-          hideOverlay
-          overrides={{
-            content: {
-              spaceInset: 'spaceInset000',
-            },
-            panel: {
-              stylePreset: 'sidebar',
+}) => (
+  <>
+    <Visible xs sm md>
+      <StyledDrawer
+        data-testid="sidebar"
+        aria-label="drawer menu on the left"
+        open={sidebarOpen}
+        onDismiss={handleSidebarClick}
+        placement="left"
+        closePosition="none"
+        hideOverlay
+        overrides={{
+          content: {
+            spaceInset: 'spaceInset000',
+          },
+          panel: {
+            stylePreset: 'sidebar',
 
-              minSize: '100%',
-            },
-          }}
-        >
-          <SidebarNav />
-          <Block marginInline="space060" marginBlock="space050">
-            <GitHubLaunch />
-            <Block marginBlock="space050" />
-            <ThemeSwitch
-              toggle={handleOnClickEvents}
-              themeMode={themeMode}
-              textTheme
-            />
-          </Block>
-        </StyledDrawer>
-      </Visible>
-      <Visible lg xl>
-        <StyledSidebarDesktop
-          open={sidebarOpen}
-          data-testid="sidebar"
-          role="complementary"
-          hideSidebar={hideSidebar}
-        >
-          <Block spaceInset="space010" />
+            minSize: '100%',
+          },
+        }}
+      >
+        <SidebarNav />
+        <Block marginInline="space060" marginBlock="space050">
+          <GitHubLaunch />
+          <Block marginBlock="space050" />
+        </Block>
+      </StyledDrawer>
+    </Visible>
+    <Visible lg xl>
+      <StyledSidebarDesktop
+        open={sidebarOpen}
+        data-testid="sidebar"
+        role="complementary"
+        hideSidebar={hideSidebar}
+      >
+        <Block spaceInset="space010" />
 
-          <SidebarNav />
-        </StyledSidebarDesktop>
-      </Visible>
-    </>
-  );
-};
+        <SidebarNav />
+      </StyledSidebarDesktop>
+    </Visible>
+  </>
+);
 
 export default Sidebar;
--- site/components/theme-switch.tsx
+++ site/components/theme-switch.tsx
@@ -1,49 +1,18 @@
 import * as React from 'react';
-import {FlagProps, GridLayout, IconButton, P} from 'newskit';
+import {FlagProps, IconButton} from 'newskit';
 import {IconFilledLight, IconFilledDark} from './icons';
 
 interface ThemeSwitchProps {
   toggle: () => void;
   themeMode: string;
-  textTheme?: boolean;
-  // handleSidebarClick?: () => void;
 }
 
 export const ThemeSwitch: React.FC<
   ThemeSwitchProps & {size?: FlagProps['size']}
-> = ({size, toggle, themeMode, textTheme}) => {
+> = ({size, toggle, themeMode}) => {
   const isDark = themeMode === 'dark';
 
-  return textTheme ? (
-    <>
-      <GridLayout columns="1fr 30px" onClick={toggle}>
-        <P overrides={{typographyPreset: 'utilityLabel030'}}>
-          {`${isDark ? 'Light' : 'Dark'}  Theme`} {textTheme}
-        </P>
-        <IconButton
-          size={size}
-          eventContext={{
-            currentThemeMode: themeMode,
-          }}
-          eventOriginator="theme-switch-button"
-          data-testid="theme-switch-button"
-          onClick={toggle}
-          title={`${isDark ? 'Disable' : 'Enable'} Dark Theme`}
-          aria-label={`${isDark ? 'Disable' : 'Enable'} Dark Theme`}
-          aria-pressed={isDark ? 'true' : 'false'}
-          overrides={{
-            stylePreset: 'buttonLightDarkToggle',
-          }}
-        >
-          {isDark ? (
-            <IconFilledLight overrides={{size: 'iconSize010'}} />
-          ) : (
-            <IconFilledDark overrides={{size: 'iconSize010'}} />
-          )}
-        </IconButton>
-      </GridLayout>
-    </>
-  ) : (
+  return (
     <IconButton
       size={size}
       eventContext={{
