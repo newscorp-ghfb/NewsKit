d5e03aa77de5094b89e49569005439c90b7c521b
Srinivasan-Ramasamy <Srinivasan.Ramasamy@news.co.uk>
2022-06-01 17:36:16 +0530

    docs(PPDSC-2172): test fix

--- site/components/site-header.tsx
+++ site/components/site-header.tsx
@@ -115,28 +115,21 @@ const SiteHeader = React.forwardRef<HeaderRef, HeaderProps>(
 
     const renderNavItems = (items: NavItemProps[], currentRoute: string) =>
       items.map(({title, id}) => (
-        <>
-          <MenuItem
-            data-testid="styled-indicator"
-            key={id}
-            href={id}
-            size="small"
-            selected={currentRoute.split('/')[1].includes(id.split('/')[1])}
-            overrides={{
-              stylePreset: 'linkTopNavigation',
-              minHeight: '80px',
+        <MenuItem
+          data-testid="styled-indicator"
+          key={id}
+          href={id.replace('/', '')}
+          size="small"
+          selected={currentRoute.split('/')[1].includes(id.split('/')[1])}
+          overrides={{
+            stylePreset: 'linkTopNavigation',
+            minHeight: '80px',
 
-              marginInline: '10px',
-            }}
-          >
-            {title}
-          </MenuItem>
-          <br />
-          <a href="/theme/overview">/theme/overview</a>{' '}
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <br />
-          <a href="theme/overview">theme/overview</a>
-        </>
+            marginInline: '10px',
+          }}
+        >
+          {title}
+        </MenuItem>
       ));
 
     return (
