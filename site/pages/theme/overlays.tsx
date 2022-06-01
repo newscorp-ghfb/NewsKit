ca258a73ff1140eac16c90137df81798310bf90c
Khadir <khadirrashid8@gmail.com>
2022-06-01 13:54:16 +0100

    docs(PPDSE-2173): docs/ppdse 2173 added images

--- site/pages/theme/overlays.tsx
+++ site/pages/theme/overlays.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
+import {getIllustrationComponent} from '../../components/illustrations/illustration-loader';
+import {UsageKind} from '../../components/usage-card';
 import {FoundationPageTemplate} from '../../templates/foundation-page-template';
 import {LayoutProps} from '../../components/layout';
+import {MediaList} from '../../components/media-list';
+import {
+  ContentPrimary,
+  ContentSection,
+} from '../../components/content-structure';
+import {ComponentPageCell} from '../../components/layout-cells';
+
+const DO_AND_DONT = [
+  {
+    description:
+      'Overlay contrasts should be distinct and clear. They can be used to obscure page content and emphasise an element for greater legibility. I.e. the Modal.',
+    kind: UsageKind.DO,
+    media: getIllustrationComponent('theme/overlays/do'),
+  },
+  {
+    description:
+      'Ensure overlays used do not obscure the legibility of important UI elements like text and icons, and contrasts are distinct and clear.',
+    kind: UsageKind.DONT,
+    media: getIllustrationComponent('theme/overlays/dont'),
+  },
+];
 
 const Overlays = (layoutProps: LayoutProps) => (
   <FoundationPageTemplate
@@ -20,7 +43,25 @@ const Overlays = (layoutProps: LayoutProps) => (
         'Overlays are used for styling UI elements. They can be decorative, but often have a functional use like communicating state on images or increasing the background contrast when a component is layered above the interface, e.g. a Modal.',
     }}
   >
-    Hello
+    <ComponentPageCell>
+      <ContentSection sectionName="usgae">
+        <ContentPrimary
+          toc="Usage"
+          id="usage"
+          headline="Usage"
+          description="The following guidance describes how and when to appropriately use overlays."
+          showSeparator
+        >
+          <MediaList
+            gridProps={{xsRowGutter: 'space050'}}
+            cardType="usage"
+            layout="2-span"
+            cards={DO_AND_DONT}
+          />
+        </ContentPrimary>
+      </ContentSection>
+    </ComponentPageCell>
   </FoundationPageTemplate>
 );
+
 export default Overlays;
