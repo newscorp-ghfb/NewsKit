85355a37945be2b63188f06cfffacdf260ff979a
Srinivasan-Ramasamy <Srinivasan.Ramasamy@news.co.uk>
2022-06-03 10:10:03 +0530

    docs(PPDSC-2172): test changes

--- site/pages/patterns/overview.tsx
+++ site/pages/patterns/overview.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {styled, getColorFromTheme} from 'newskit';
 import {Link} from '../../components/link';
-import {MediaItem, MediaList} from '../../components/media-list';
 import {LayoutProps} from '../../components/layout';
 import {PatternPageTemplate} from '../../templates/pattern-page-template/pattern-page-template';
 import {
@@ -10,52 +9,46 @@ import {
   ContentSecondary,
 } from '../../components/content-structure';
 import {ComponentPageCell} from '../../components/layout-cells';
-import {Illustration} from '../../components/illustrations/illustration-loader';
+// import {Illustration} from '../../components/illustrations/illustration-loader';
+// import { MediaList } from '../../components/media-list';
 
-const featureCardOverrides = {
-  title: {
-    typographyPreset: 'editorialHeadline030',
-  },
-  description: {
-    typographyPreset: 'editorialParagraph020',
-  },
-};
+// const featureCardoverrides = {
+//   title: {
+//     typographyPreset: 'editorialHeadline030',
+//   },
+//   description: {
+//     typographyPreset: 'editorialParagraph020',
+//   },
+// };
 
-const PRINCIPLE_CARDS = [
-  {
-    media: () => (
-      <Illustration path="guides/design-overview/design-made-easy" />
-    ),
-    title: 'Design made easy',
-    description:
-      'NewsKit makes it simple to design experiences that are not only aesthetically pleasing, but that are fully responsive, promote accessibility and align to the components in code. NewsKit allows designers to free up time to concentrate on the important stuff - like the end user experience!',
-    stylePrefix: 'featureCard',
-    overrides: featureCardOverrides,
-  },
-  {
-    media: () => (
-      <Illustration path="guides/design-overview/complete-flexibility" />
-    ),
-    title: 'Complete flexibility',
-    description: (
-      <>
-        NewsKit is fully customisable, with the flexibility to apply a single
-        brand, or sub-brands style, with an extensive{' '}
-        <Link
-          overrides={{stylePreset: 'inkInverse'}}
-          href="https://www.figma.com/file/oSjjLxC27fa6Jh6AHM7ja9/NK-NewsKit-Theme?node-id=1%3A393"
-          target="_blank"
-        >
-          theming system
-        </Link>
-        . Additionally the Figma components have a range of variants and layer
-        options to allow for further cutomisation.
-      </>
-    ),
-    stylePrefix: 'featureCard',
-    overrides: featureCardOverrides,
-  },
-] as MediaItem[];
+// const FORMS_PATTERN_CARDS = [
+//   {
+//     media: () => <Illustration path="patterns/overview/why-use/consistency" />,
+//     title: 'Consistency',
+//     description:
+//       'All our brands have some form of data capture. Aligning our approach means there is one source of truth when building forms and capturing customer information. ',
+//     stylePrefix: 'featureCard',
+//     overrides: featureCardoverrides,
+//   },
+//   {
+//     media: () => <Illustration path="patterns/overview/why-use/ease-of-use" />,
+//     title: 'Ease of use',
+//     description:
+//       'Customer data is only useful if it’s accurate. If we make our forms straightforward, it’s more likely our users will complete the form successfully.',
+//     stylePrefix: 'featureCard',
+//     overrides: featureCardoverrides,
+//   },
+//   {
+//     media: () => (
+//       <Illustration path="patterns/overview/why-use/best-practice" />
+//     ),
+//     title: 'Best practice',
+//     description:
+//       'Digital forms have been around for decades. We can rely on proven industry best practice and shared learning to supplement our own research.',
+//     stylePrefix: 'featureCard',
+//     overrides: featureCardoverrides,
+//   },
+// ];
 
 const StyledHeading = styled.span`
   color: ${getColorFromTheme('inkBrand010')};
@@ -108,11 +101,12 @@ const Overview = (layoutProps: LayoutProps) => (
           toc="Why use the Forms pattern"
           headline="Why use the Forms pattern?"
         >
-          <MediaList
+          test
+          {/* <MediaList
             layout="3-span"
             cardType="feature"
-            cards={PRINCIPLE_CARDS}
-          />
+            cards={FORMS_PATTERN_CARDS}
+          /> */}
         </ContentPrimary>
 
         <ContentSecondary
