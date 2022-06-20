fb6c84685dccfda35c0582d7d0ff872fced4b8d9
Stoyan Delev <stoyan.delev@news.co.uk>
2022-06-20 14:07:38 +0300

    feat(PPDSC-2156): add accordion group

---
+++ src/accordion/accordion-group.tsx
@@ -0,0 +1,74 @@
+import React, {useCallback} from 'react';
+import {useControlled} from '../utils/hooks';
+import {AccordionGroupProps} from './types';
+
+const unifyValue = (
+  value: number | number[] | undefined,
+): number[] | undefined => {
+  if (value !== undefined && !Array.isArray(value)) {
+    return [value];
+  }
+  return value;
+};
+
+export const AccordionGroup = React.forwardRef<
+  HTMLInputElement,
+  AccordionGroupProps
+>(
+  (
+    {children, defaultExpanded = [], onChange, expanded, single, ...restProps},
+    ref,
+  ) => {
+    const [expandedList, setExpandedState] = useControlled<number[]>({
+      controlledValue: unifyValue(expanded),
+      defaultValue: unifyValue(defaultExpanded),
+    }) as [number[], (state: number[]) => void];
+
+    const handleChange = useCallback(
+      (isExpanded: boolean, index: number) => {
+        let newExpandedState: number[] = [];
+
+        // in the case only 1 accordion is allowed to be open
+        if (single) {
+          // replace current the expanded accordion with the new one
+          if (isExpanded && !expandedList?.includes(index)) {
+            newExpandedState = [index];
+          } else {
+            // close all when you click on the same accordion
+            newExpandedState = [];
+          }
+
+          //
+        } else {
+          // eslint-disable-next-line no-lonely-if
+          if (isExpanded) {
+            // add to the list of expanded accordions
+            newExpandedState = [...expandedList, index];
+          } else {
+            // remove from the list of expanded accordions
+            newExpandedState = expandedList?.filter(i => i !== index);
+          }
+        }
+
+        setExpandedState(newExpandedState);
+
+        if (onChange) {
+          onChange(newExpandedState);
+        }
+      },
+      [setExpandedState, expandedList, single, onChange],
+    );
+
+    return (
+      <div ref={ref} {...restProps}>
+        {React.Children.map(children, (child, index) =>
+          // @ts-ignore
+          React.cloneElement(child, {
+            expanded: expandedList.includes(index),
+            onChange: (isExpanded: boolean) => handleChange(isExpanded, index),
+          }),
+        )}
+      </div>
+    );
+  },
+);
