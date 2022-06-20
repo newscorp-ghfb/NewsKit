f835f18397afe5429ff2ae87d5b3f3e3f5c105e6
mikef <mstuartflood@gmail.com>
2022-06-20 10:22:52 +0100

    feat(PPDSC-2120): remove unneeded props from tooltip

--- src/tooltip/types.ts
+++ src/tooltip/types.ts
@@ -2,7 +2,10 @@ import {FloatingElementProps} from '../base-floating-element';
 
 export type TriggerType = 'hover' | 'focus';
 
-export type TooltipProps = Omit<FloatingElementProps, 'trigger'> & {
+export type TooltipProps = Omit<
+  FloatingElementProps,
+  'trigger' | 'onDismiss' | 'restoreFocusTo'
+> & {
   trigger?: TriggerType | TriggerType[];
   asLabel?: boolean;
 };
