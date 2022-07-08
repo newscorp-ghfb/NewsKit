892784877d6c1e05df8900fa648a489a9c0e602b
Aysha <ayshababur@yahoo.com>
2022-07-08 13:49:14 +0100

    chore(PPDSC-2171): feat-ppdse-2171-stack added

--- src/stack/stack.tsx
+++ src/stack/stack.tsx
@@ -45,6 +45,7 @@ const wrapChild = (
   list: StackProps['list'],
   inline: NonNullable<StackProps['inline']>,
   as?: StackProps['as'],
+  ref?: React.RefObject<HTMLDivElement>,
 ) => (
   child: React.ReactNode & {
     props?: StackChildProps;
@@ -72,7 +73,7 @@ const wrapChild = (
   // eslint-disable-next-line @typescript-eslint/no-use-before-define
   if (hasMatchingDisplayNameWith(child, Stack)) {
     // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    const stack = <Stack inline={inline} as={as} {...child.props} />;
+    const stack = <Stack ref={ref} inline={inline} as={as} {...child.props} />;
 
     return list || hasSpace ? (
       <ChildContainer {...renderAs} {...childProps}>
@@ -103,7 +104,7 @@ const wrapChild = (
 
   // If child is anything else
   return list || hasSpace ? (
-    <ChildContainer {...renderAs} {...childProps}>
+    <ChildContainer ref={ref} {...renderAs} {...childProps}>
       {child}
     </ChildContainer>
   ) : (
@@ -111,52 +112,57 @@ const wrapChild = (
   );
 };
 
-export const Stack: React.FC<StackProps> = ({
-  spaceStack = DEFAULT_PROPS.spaceStack,
-  spaceInline = DEFAULT_PROPS.spaceInline,
-  flow = DEFAULT_PROPS.flow,
-  wrap = DEFAULT_PROPS.wrap,
-  stackDistribution = DEFAULT_PROPS.stackDistribution,
-  flexGrow = DEFAULT_PROPS.flexGrow,
-  flexShrink = DEFAULT_PROPS.flexShrink,
-  flowReverse = DEFAULT_PROPS.flowReverse,
-  inline = DEFAULT_PROPS.inline,
-  as,
-  list,
-  ariaLabel,
-  children,
-  role,
-  height,
-  ...props
-}) => {
-  const MasterContainer = list
-    ? StyledMasterContainerList
-    : StyledMasterContainer;
+export const Stack = React.forwardRef<HTMLDivElement, StackProps>(
+  (
+    {
+      spaceStack = DEFAULT_PROPS.spaceStack,
+      spaceInline = DEFAULT_PROPS.spaceInline,
+      flow = DEFAULT_PROPS.flow,
+      wrap = DEFAULT_PROPS.wrap,
+      stackDistribution = DEFAULT_PROPS.stackDistribution,
+      flexGrow = DEFAULT_PROPS.flexGrow,
+      flexShrink = DEFAULT_PROPS.flexShrink,
+      flowReverse = DEFAULT_PROPS.flowReverse,
+      inline = DEFAULT_PROPS.inline,
+      as,
+      list,
+      ariaLabel,
+      children,
+      role,
+      height,
+      ...props
+    },
+    ref,
+  ) => {
+    const MasterContainer = list
+      ? StyledMasterContainerList
+      : StyledMasterContainer;
 
-  return (
-    <MasterContainer
-      {...getAsProp(as, list)}
-      spaceStack={spaceStack}
-      spaceInline={spaceInline}
-      flow={flow}
-      $wrap={wrap}
-      flexGrow={flexGrow}
-      flexShrink={flexShrink}
-      flowReverse={flowReverse}
-      stackDistribution={stackDistribution}
-      inline={inline}
-      aria-label={ariaLabel}
-      role={role}
-      $height={height}
-      {...props}
-    >
-      {children &&
-        React.Children.map(
-          children,
-          wrapChild(spaceStack, spaceInline, flow, wrap, list, inline, as),
-        )}
-    </MasterContainer>
-  );
-};
-
+    return (
+      <MasterContainer
+        ref={ref}
+        {...getAsProp(as, list)}
+        spaceStack={spaceStack}
+        spaceInline={spaceInline}
+        flow={flow}
+        $wrap={wrap}
+        flexGrow={flexGrow}
+        flexShrink={flexShrink}
+        flowReverse={flowReverse}
+        stackDistribution={stackDistribution}
+        inline={inline}
+        aria-label={ariaLabel}
+        role={role}
+        $height={height}
+        {...props}
+      >
+        {children &&
+          React.Children.map(
+            children,
+            wrapChild(spaceStack, spaceInline, flow, wrap, list, inline, as),
+          )}
+      </MasterContainer>
+    );
+  },
+);
 Stack.displayName = 'Stack';
