6d8b2f148c5b65190dedffbb892eafe7793ffb88
timbradbury <bradburyt8@gmail.com>
2022-07-08 09:17:01 +0100

    fix(PPDSC-2254): push unsaved audio file change

--- src/audio-player-composable/__tests__/audio-player-composable-e2e.stories.tsx
+++ src/audio-player-composable/__tests__/audio-player-composable-e2e.stories.tsx
@@ -33,7 +33,7 @@ export const AudioPlayerE2E = () => {
     <div data-testid="audio-player-inline">
       <StorybookSubHeading>Audio player for e2e tests</StorybookSubHeading>
       <AudioPlayerComposable
-        src="/audio_file_1.mp3"
+        src="https://ncu-newskit-docs.s3.eu-west-1.amazonaws.com/storybook-assets/audio_file_1.mp3"
         ariaLandmark="audio player e2e tests"
       >
         <GridLayout rowGap="space020" areas={fullAudioPlayerAreas}>
