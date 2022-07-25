1a186586315b95629ed0b5fd8cd305c6aa21b43d
Srinivasan-Ramasamy <Srinivasan.Ramasamy@news.co.uk>
2022-07-25 12:51:03 +0530

    docs(PPDSC-2269): ui update on api section

--- site/pages/components/tooltip.tsx
+++ site/pages/components/tooltip.tsx
@@ -253,6 +253,12 @@ const TooltipComponent = (layoutProps: LayoutProps) => (
         },
         {
           description:
+            'The Tooltip can be applied to the Icon Button in order to provide additional context relating to the intended action or destination for users.',
+          kind: UsageKind.DO,
+          media: getIllustrationComponent('components/tooltip/usage/do1'),
+        },
+        {
+          description:
             'Avoid covering the element that the Tooltip is attributed to, as it will lose its context.',
           kind: UsageKind.DONT,
           media: getIllustrationComponent('components/tooltip/usage/dont5'),
@@ -380,6 +386,250 @@ const TooltipComponent = (layoutProps: LayoutProps) => (
           </UnorderedList>
         </>
       ),
+    }}
+    componentAPI={{
+      components: [
+        {
+          propsSummary:
+            'The Tooltip has a range of props that can be used to define an appropriate experience for different use cases.',
+          overridesSummary:
+            'The Popover has a range of predefined elements and attributes that can be overridden to define their appearance.',
+          propsRows: [
+            {
+              name: 'children',
+              type: 'React.ReactElement & { ref?: React.Ref<unknown>; };',
+              description: 'Tooltip reference element.',
+              required: true,
+            },
+            {
+              name: 'content',
+              type: 'React.ReactNode',
+              description: 'Default content value.',
+              required: true,
+            },
+            {
+              name: 'asLabel',
+              type: 'boolean',
+              default: 'false',
+              description: (
+                <>
+                  Set to true if the tooltip acts as an accessible label for the
+                  child component.
+                  <br />
+                  <br />
+                  By default the title acts as an accessible description for the
+                  child.
+                </>
+              ),
+              required: undefined,
+            },
+            {
+              name: 'trigger',
+              type: 'string | string[]',
+              default: `['hover', 'focus']`,
+              description: `Defines how the Tooltip is triggered`,
+              required: undefined,
+            },
+            {
+              name: 'open',
+              type: 'boolean',
+              default: 'false',
+              description: 'If true, the Tooltip is shown',
+              required: undefined,
+            },
+            {
+              name: 'fallbackBehaviours',
+              type: `'flip' | 'shift' | ['flip', 'shift']`,
+              default: `‘flip', 'shift’`,
+              description: (
+                <>
+                  Behavior the Tooltip should follow for fallback.
+                  <br />
+                  <br />
+                  <Link
+                    href="https://floating-ui.com/docs/react-dom-interactions"
+                    target="_blank"
+                  >
+                    For more information refer to the documentation from
+                    Floating UI.
+                  </Link>
+                </>
+              ),
+              required: undefined,
+            },
+            {
+              name: 'boundary',
+              type: `'clippingAncestors' | Element | Array<Element>;`,
+              default: `'clippingAncestors'`,
+              description: (
+                <>
+                  Describes the clipping element(s) that overflow will be
+                  checked relative to.
+                  <br />
+                  <br />
+                  <Link
+                    href="https://floating-ui.com/docs/react-dom-interactions"
+                    target="_blank"
+                  >
+                    For more information refer to the documentation from
+                    Floating UI.
+                  </Link>
+                </>
+              ),
+              required: undefined,
+            },
+            {
+              name: 'placement',
+              type: [
+                'top',
+                'top-start',
+                'top-end',
+                'right',
+                'right-start',
+                'right-end',
+                'bottom',
+                'bottom-start',
+                'bottom-end',
+                'left',
+                'left-start',
+                'left-end',
+              ],
+              default: `top`,
+              description:
+                'Defines the placement of the Tooltip, with the optional indicator to denote the direction of the context it is attributed.',
+              required: undefined,
+            },
+            {
+              name: 'hidePointer',
+              type: 'boolean',
+              default: 'false',
+              description: 'If provided, hides the pointer.',
+              required: undefined,
+            },
+          ],
+          overridesRows: [
+            {
+              attribute: 'maxWidth',
+              type: 'MQ<string>',
+              description: 'Overrides the maxWidth property of the Tooltip.',
+            },
+            {
+              attribute: 'minWidth',
+              type: 'MQ<string>',
+              description: 'Overrides the minWidth property of the Tooltip.',
+            },
+            {
+              attribute: 'zIndex',
+              type: 'number',
+              default: '80',
+              description: 'Overrides the zIndex of the Tooltip.',
+            },
+            {
+              attribute: 'distance',
+              type: 'MQ<string>',
+              default: 'space020',
+              description:
+                'Overrides the distance between the Tooltip and the item it is attributed to.',
+            },
+            {
+              attribute: 'stylePreset',
+              type: 'MQ<string>',
+              description: 'Overrides the stylePreset applied to the Tooltip.',
+            },
+            {
+              attribute: 'transitionPreset',
+              type: 'MQ<string>',
+              default: 'fade',
+              description: 'Overrides the transitionPrese of the Tooltip.',
+            },
+            {
+              attribute: 'panel.paddingBlock',
+              type: 'MQ<string>',
+              default: 'spaceInset020',
+              description: 'Overrides the inset space of the Tooltip panel.',
+            },
+            {
+              attribute: 'panel.paddingInline',
+              type: 'MQ<string>',
+              default: 'spaceInset020',
+              description: 'Overrides the inset space of the Tooltip panel.',
+            },
+            {
+              attribute: 'panel.stylePreset',
+              type: 'MQ<string>',
+              default: 'tooltipPanel',
+              description:
+                'Overrides the stylePreset applied to the Tooltip panel.',
+            },
+            {
+              attribute: 'panel.typographyPreset',
+              type: 'MQ<string>',
+              default: 'utilityLabel010',
+              description:
+                'Overrides the typographyPreset applied to the Tooltip panel.',
+            },
+            {
+              attribute: 'panel.paddingBlockStart',
+              type: '',
+              default: '',
+              description: '',
+            },
+            {
+              attribute: 'panel.paddingInlineStart',
+              type: '',
+              default: '',
+              description: '',
+            },
+            {
+              attribute: 'panel.paddingInlineEnd',
+              type: '',
+              default: '',
+              description: '',
+            },
+            {
+              attribute: 'pointer.size',
+              type: 'MQ<string>',
+              default: 'sizing010',
+              description: 'Overrides the size of the Tooltip pointer.',
+            },
+            {
+              attribute: 'pointer.stylePreset',
+              type: 'MQ<string>',
+              default: 'tooltipPointer',
+              description: 'Overrides the stylePreset of the Tooltip pointer.',
+            },
+            {
+              attribute: 'pointer.edgeOffset',
+              type: 'MQ<string>',
+              default: 'space020',
+              description:
+                'Overrides the edgeOffset of the Tooltip indicator. The edgeOffset is the padding between the indicator and the edges of the popover container.',
+            },
+          ],
+          propsFooter: (
+            <>
+              <InlineMessage
+                icon={infoIcon}
+                role="region"
+                aria-label="ForwardRef"
+                title="Note"
+                overrides={{
+                  marginBlockStart: 'space070',
+                }}
+              >
+                If the Tooltip is wrapping a functional component, ensure that
+                the functional component accepts a ref using{' '}
+                <Link
+                  href="https://reactjs.org/docs/forwarding-refs.html"
+                  target="_blank"
+                >
+                  forwardRef.
+                </Link>
+              </InlineMessage>
+            </>
+          ),
+        },
+      ],
     }}
     compliance={{
       states: true,
