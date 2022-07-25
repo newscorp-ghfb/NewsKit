85a3ad29792d42695c6f23db3e21dd9b345f41a0
Paola <paola.dimova@news.co.uk>
2022-07-25 16:27:32 +0300

    feat(PPDSC-2005) Layer manager (#146)
    * fix(PPDSC-1972): select panel - add zindex
    
    * fix(PPDSC-1972): merge - update snapshots
    
    * fix(PPDSC-1972): failing tests - mock datetime
    
    * fix(PPDSC-1972): add comment
    
    * feat(PPDSC-0000): wip
    
    * feat(PPDSC-2005): add layer manager
    
    * feat(PPDSC-2005): delete portal component
    
    * feat(PPDSC-2005): add layers
    
    * feat(PPDSC-2005): update layer organizer
    
    * feat(PPDSC-2005): cleanup
    
    * feat(PPDSC-2005): clean up & new story
    
    * feat(PPDSC-2005): fixx select and inline prop on modal/drawer
    
    * feat(PPDSC-2005): fix unit tests
    
    * feat(PPDSC-2005): add comments
    
    * feat(PPDSC-2005): test LM on docs site
    
    * feat(PPDSC-2005): add tests
    
    * feat(PPDSC-2005): add unit tests to layer
    
    * feat(PPDSC-2005): fix unit tests
    
    * feat(PPDSC-2005): fix e2e tests
    
    * feat(PPDSC-2005): add srr
    
    * feat(PPDSC-2005): custom portal
    
    * feat(PPDSC-2005): add portal
    
    * feat(PPDSC-2005): improvements
    
    * feat(PPDSC-2005): update layer manager
    
    * feat(PPDSC-2005): fix ts
    
    * feat(PPDSC-2005): fix e2e tests
    
    * feat(PPDSC-2005): fix coverage
    
    * feat(PPDSC-2005): fix snapshots
    
    * feat(PPDSC-2005): fix LO
    
    * feat(PPDSC-2005): fix e2e tests
    
    * feat(PPDSC-2005): clean up
    
    * feat(PPDSC-2005): clean up
    
    * feat(PPDSC-2005): clean up
    
    * feat(PPDSC-2005): fix snapshots
    
    * feat(PPDSC-2005): add NewskitProvider
    
    * feat(PPDSC-2005): add select story
    
    * feat(PPDSC-2005): add warnings
    
    * feat(PPDSC-2005): remove mediaQueryProvider from select
    
    * feat(PPDSC-2005): update test-utils with newskitprovider
    
    * feat(PPDSC-2005): update newskit/theme provider docs
    
    * feat(PPDSC-2005): update snapshots
    
    * feat(PPDSC-2005): fix coverage
    
    * feat(PPDSC-2005): fix types and tests cleanup
    
    * feat(PPDSC-2005): update e2e tests
    
    * feat(PPDSC-2005): fix snapshots
    
    * feat(PPDSC-2005): fix coverage
    
    * feat(PPDSC-2005): fix unit tests
    
    * feat(PPDSC-2005): ignore test-utils from coverage
    
    * feat(PPDSC-2005): address comments
    
    * feat(PPDSC-2005): add docs
    
    * feat(PPDSC-2005): update snapshots
    
    * feat(PPDSC-2005): remove comment
    
    * feat(PPDSC-2005): update select snapshots
    
    * feat(PPDSC-2005): update docs snapshots
    
    * feat(PPDSC-2005): update mediaQueruContext
    
    * feat(PPDSC-2005): update docs
    
    * feat(PPDSC-2005): rename file
    
    * feat(PPDSC-2005): rename NewskitProvider to NewsKitProvider
    
    * feat(PPDSC-2005): fix typo
    
    * feat(PPDSC-2005): add waitBeforeCapture
    
    Co-authored-by: Stoyan Delev <mutebg@users.noreply.github.com>
    Co-authored-by: Stoyan Delev <stoyan.delev@news.co.uk>

--- site/public/static/examples/theming/nested-theme-provider.tsx
+++ site/public/static/examples/theming/nested-theme-provider.tsx
@@ -1,14 +1,18 @@
-import { ThemeProvider, newskitDarkTheme, newskitLightTheme } from 'newskit';
+import { NewsKitProvider, ThemeProvider, newskitDarkTheme, newskitLightTheme } from 'newskit';
 
 render() {
   return (
-    <ThemeProvider theme={newskitLightTheme}>
+    <NewsKitProvider theme={newskitLightTheme}>
       <Component>Light theme style!</Component>
 
       <ThemeProvider theme={newskitDarkTheme}>
         <Component>Dark theme style!</Component>
+
+        <ThemeProvider theme={otherTheme}>
+          <Component>Other theme style!</Component>
+        </ThemeProvider>
+
       </ThemeProvider>
-    </ThemeProvider>
+    </NewsKitProvider>
   )
 }
-
