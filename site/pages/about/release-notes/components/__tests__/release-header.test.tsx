3bcc6b1965c15176d50af5bacc3d29193d430958
mikef <mstuartflood@gmail.com>
2022-07-29 12:56:51 +0100

    docs(PPDSC-2204): component tests added

---
+++ site/pages/about/release-notes/components/__tests__/release-header.test.tsx
@@ -0,0 +1,13 @@
+import {renderWithTheme} from 'newskit/test/test-utils';
+import ReleaseHeader from '../release-header';
+
+describe('ReleaseHeader', () => {
+  test('renders correctly', () => {
+    const {asFragment} = renderWithTheme(ReleaseHeader, {
+      change_level: 'patch',
+      tag_name: 'v5.7.0',
+      published_at: '2022-07-18T11:41:49Z',
+    });
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
