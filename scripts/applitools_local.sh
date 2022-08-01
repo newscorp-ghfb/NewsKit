caec9be8f64f578a972a8be6bfd777f740065b8e
mikef <mstuartflood@gmail.com>
2022-08-01 14:33:44 +0100

    feat(PPDSC-2313): script to run applitools locally
    - Shell script to set env variables (inc asking for api key)
    - Add scripts to package json to run comp and doc tests

---
+++ scripts/applitools_local.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# this script sets the environment variables required to run Applitools tests locally
+
+USERNAME=$(git config user.name)
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+COMMIT_SHA=$(git rev-parse HEAD)
+STATUS=$(git status)
+
+# the commit needs to be pushed (so that the Applitools / GitHub integration can retrieve its info)
+if [[ "$STATUS" != *"Your branch is up to date with 'origin/${BRANCH_NAME}'."* ]]; then
+  echo "Please make sure you are up-to-date with remote before running Applitools tests."
+  exit 1
+fi
+
+# ask the user to input their API key
+read -p "Enter your API key (you can find this in the Applitools UI): " apiKey
+
+# the batch id must be set to the commit sha for the integration to work
+export APPLITOOLS_BATCH_ID=$(git rev-parse HEAD);
+# batch name can be anything (make it easy to identify)
+export APPLITOOLS_BATCH_NAME="LOCAL RUN by ${USERNAME} for commit ${COMMIT_SHA} on ${BRANCH_NAME}";
+# set to the user's API key
+export APPLITOOLS_API_KEY=$apiKey
+# make sure the batch closes after running
+export APPLITOOLS_DONT_CLOSE_BATCHES=false
+
+if [[ "$1" == "comps" ]]; then
+  echo "Running component tests..."
+  yarn test:visual:comps:ci
+else
+  echo "Running doc site tests..."
+  yarn e2e:visual:docs:ci
+fi
+
+# todo: move to scripts
+# todo: update readme (also explain how to run subset)
+# todo: test git logic on new branch (inc that batch detail are set on applitools)
