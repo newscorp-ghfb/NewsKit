a53de1d6c1c3c45da411b5b204120c7531ab0e97
baburay23 <54642337+baburay23@users.noreply.github.com>
2022-08-09 14:40:50 +0100

    Feat/ppdsc 2143 volume control horizontal expand (#306)
    * feat(PPDSC-2143): chore-ppdse-2143-added expand
    
    * feat(PPDSC-2143): chore-ppdse-2143-added types
    
    * feat(PPDSC-2143): chore-ppdse-2143-layout options working
    
    * feat(PPDSC-2143): chore-ppdse-2143-tidied up
    
    * feat(PPDSC-2143): chore-ppdse-2143-transition ahrd coded
    
    * feat(PPDSC-2143): chore-ppdse-2143-added stories
    
    * feat(PPDSC-2143): chore-ppds-2143-motion working
    
    * feat(PPDSC-2143): chore-ppds-2143-horizonal working
    
    * feat(PPDSC-2143): chore-ppds-2143-added visible
    
    * feat(PPDSC-2143): chore-ppds-2143-tideid up
    
    * feat(PPDSC-2143): chore-ppds-2143-updated snapshots and sotires
    
    * feat(PPDSC-2143): chore-ppds-2143-added tests for styled.ts
    
    * feat(PPDSC-2143): chore-ppds-2143-tests 100%
    
    * feat(PPDSC-2143): chore-ppds-2143-updated stories
    
    * feat(PPDSC-2143): chore-ppds-2143-updated tests
    
    * feat(PPDSC-2143): chore-ppds-2143-removed comments
    
    * feat(PPDSC-2143): chore-ppds-2143-fixed aria-landmark
    
    * feat(PPDSC-2143): chore-ppds-2143-updated docs
    
    * feat(PPDSC-2143): chore-ppds-2143-addressed comments
    
    * feat(PPDSC-2143): chore-ppds-2143-added comment
    
    * feat(PPDSC-2143): chore-ppds-2143-added for open

--- src/audio-player-composable/components/volume-control/defaults.ts
+++ src/audio-player-composable/components/volume-control/defaults.ts
@@ -1,36 +1,26 @@
-const defaults = {
-  spaceBetween: 'space030',
-  slider: {
-    track: {
-      stylePreset: 'audioPlayerVolumeControlTrack',
-      size: 'sizing010',
-      length: '130px',
-    },
-    indicator: {
-      stylePreset: 'audioPlayerVolumeControlIndicator',
-    },
-    thumb: {
-      stylePreset: 'audioPlayerVolumeControlThumb',
-      size: 'sizing040',
-    },
-    feedback: {
-      size: 'sizing060',
-    },
-  },
-  button: {
-    stylePreset: 'iconButtonMinimalPrimary',
-  },
-};
-
 export default {
   audioPlayerVolumeControl: {
-    horizontal: {
-      stylePreset: 'audioPlayerVolumeControlHorizontalContainer',
-      ...defaults,
+    transitionPreset: 'widthChange',
+    spaceBetween: 'space030',
+    slider: {
+      track: {
+        stylePreset: 'audioPlayerVolumeControlTrack',
+        size: 'sizing010',
+        length: '130px',
+      },
+      indicator: {
+        stylePreset: 'audioPlayerVolumeControlIndicator',
+      },
+      thumb: {
+        stylePreset: 'audioPlayerVolumeControlThumb',
+        size: 'sizing040',
+      },
+      feedback: {
+        size: 'sizing060',
+      },
     },
-    vertical: {
-      stylePreset: 'audioPlayerVolumeControlVerticalContainer',
-      ...defaults,
+    button: {
+      stylePreset: 'iconButtonMinimalPrimary',
     },
   },
 };
--- src/audio-player-composable/components/volume-control/volume-control.tsx
+++ src/audio-player-composable/components/volume-control/volume-control.tsx
@@ -1,4 +1,5 @@
 import React, {useCallback, useEffect, useState} from 'react';
+import {CSSTransition} from 'react-transition-group';
 import {ScreenReaderOnly} from '../../../screen-reader-only';
 import {Slider} from '../../../slider';
 import {withOwnTheme} from '../../../utils/with-own-theme';
@@ -10,12 +11,18 @@ import {BreakpointKeys, useTheme} from '../../../theme';
 import stylePresets from './style-presets';
 import {AudioPlayerVolumeControlProps} from './types';
 import {GridLayoutItem} from '../../../grid-layout';
-import {StyledGridLayout, StyledVolumeSliderContainer} from './styled';
+import {
+  StyledGridLayout,
+  StyledVolumeSliderContainer,
+  VolumeControlContainer,
+} from './styled';
 import {useReactKeys} from '../../../utils/hooks';
 import {deepMerge} from '../../../utils/deep-merge';
 import {mergeBreakpointObject} from '../../../utils/merge-breakpoint-object';
 import {filterOutFalsyProperties} from '../../../utils/filter-object';
 import {ButtonSize} from '../../../button/types';
+import {getTransitionDuration} from '../../../utils';
+import {getTransitionClassName} from '../../../utils/get-transition-class-name';
 
 const ThemelessAudioPlayerVolumeControl = React.forwardRef<
   HTMLDivElement,
@@ -25,8 +32,7 @@ const ThemelessAudioPlayerVolumeControl = React.forwardRef<
   const {
     volume,
     onChange,
-    layout = 'horizontal',
-    collapsed = false,
+    layout = 'horizontalExpandable',
     keyboardShortcuts,
     overrides,
     initialVolume,
@@ -48,7 +54,7 @@ const ThemelessAudioPlayerVolumeControl = React.forwardRef<
   const buttonOverrides = {
     ...deepMerge(
       mergeBreakpointObject(Object.keys(theme.breakpoints) as BreakpointKeys[]),
-      theme.componentDefaults.audioPlayerVolumeControl[layout].button,
+      theme.componentDefaults.audioPlayerVolumeControl.button,
       filterOutFalsyProperties(overrides.button),
     ),
   };
@@ -56,7 +62,7 @@ const ThemelessAudioPlayerVolumeControl = React.forwardRef<
   const sliderOverrides = {
     ...deepMerge(
       mergeBreakpointObject(Object.keys(theme.breakpoints) as BreakpointKeys[]),
-      theme.componentDefaults.audioPlayerVolumeControl[layout].slider,
+      theme.componentDefaults.audioPlayerVolumeControl.slider,
       filterOutFalsyProperties(overrides.slider),
     ),
   };
@@ -73,51 +79,80 @@ const ThemelessAudioPlayerVolumeControl = React.forwardRef<
   const gridAreas = layout === 'vertical' ? verticalAreas : `muteButton slider`;
   const gridColumns = layout === 'vertical' ? '1fr' : 'auto 1fr';
   const [volumeSliderInstructionId] = useReactKeys(1);
+  // opens volume control on hover/focus when layout = horizontalExpandable.
+  const [open, setOpen] = useState(false);
+  // makes sure feedback element is fully visible and not cropped when slider is open and user hover/focus on it.
+  const [isVisible, setIsVisible] = React.useState(false);
 
   return (
-    <StyledGridLayout
-      ref={ref}
-      columns={gridColumns}
-      areas={gridAreas}
-      justifyItems={layout === 'vertical' ? 'center' : 'start'}
-      alignItems="center"
+    <VolumeControlContainer
+      onFocus={() => setOpen(true)}
+      onBlur={() => setOpen(false)}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
       layout={layout}
-      collapsed={collapsed}
-      overrides={overrides}
     >
-      <GridLayoutItem area="muteButton">
-        <MuteButton
-          volume={volume}
-          unMutedVolume={unMutedVolume}
-          onChange={onChange}
-          size={muteButtonSize || ButtonSize.Medium}
-          muteKeyboardShortcuts={keyboardShortcuts?.muteToggle}
-          overrides={buttonOverrides}
-        />
-      </GridLayoutItem>
-      {!collapsed && (
-        <GridLayoutItem area="slider">
-          <StyledVolumeSliderContainer layout={layout} overrides={overrides}>
-            <Slider
-              vertical={layout === 'vertical'}
-              min={0}
-              max={1}
-              step={0.1}
-              values={[volume]}
-              onChange={onSliderChange}
-              ariaLabel="Volume Control"
-              ariaValueText={`volume level ${[volume][0] * 10} of 10`}
-              dataTestId="volume-control-slider"
-              ariaDescribedBy={volumeSliderInstructionId}
-              overrides={sliderOverrides}
-            />
-            <ScreenReaderOnly id={volumeSliderInstructionId} aria-hidden="true">
-              Use the arrow keys to adjust volume
-            </ScreenReaderOnly>
-          </StyledVolumeSliderContainer>
+      <StyledGridLayout
+        ref={ref}
+        columns={gridColumns}
+        areas={gridAreas}
+        justifyItems={layout === 'vertical' ? 'center' : 'start'}
+        alignItems="center"
+        layout={layout}
+        overrides={overrides}
+      >
+        <GridLayoutItem area="muteButton">
+          <MuteButton
+            volume={volume}
+            unMutedVolume={unMutedVolume}
+            onChange={onChange}
+            size={muteButtonSize || ButtonSize.Medium}
+            muteKeyboardShortcuts={keyboardShortcuts?.muteToggle}
+            overrides={buttonOverrides}
+          />
         </GridLayoutItem>
-      )}
-    </StyledGridLayout>
+        <GridLayoutItem area="slider">
+          <CSSTransition
+            in={open}
+            timeout={getTransitionDuration(
+              `audioPlayerVolumeControl`,
+              '',
+            )({theme, overrides})}
+            classNames="nk-vc"
+            appear
+            onEnter={() => setIsVisible(true)}
+            onExited={() => setIsVisible(false)}
+          >
+            {(state: string) => (
+              <StyledVolumeSliderContainer
+                className={getTransitionClassName('nk-vc', state)}
+                layout={layout}
+                overrides={overrides}
+                open={open}
+                visible={isVisible}
+              >
+                <Slider
+                  vertical={layout === 'vertical'}
+                  min={0}
+                  max={1}
+                  step={0.1}
+                  values={[volume]}
+                  onChange={onSliderChange}
+                  ariaLabel="Volume Control"
+                  ariaValueText={`volume level ${[volume][0] * 10} of 10`}
+                  dataTestId="volume-control-slider"
+                  ariaDescribedBy={volumeSliderInstructionId}
+                  overrides={sliderOverrides}
+                />
+              </StyledVolumeSliderContainer>
+            )}
+          </CSSTransition>
+          <ScreenReaderOnly id={volumeSliderInstructionId} aria-hidden="true">
+            Use the arrow keys to adjust volume
+          </ScreenReaderOnly>
+        </GridLayoutItem>
+      </StyledGridLayout>
+    </VolumeControlContainer>
   );
 });
 
