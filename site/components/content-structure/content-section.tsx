f248714ca91e4476524aa8d2569eef5b90faaf18
mstuartf <mstuartflood@gmail.com>
2022-08-09 10:52:12 +0100

    docs(PPDSC-2204): release notes (#319)
    * docs(PPDSC-2204): add release notes page
    
    - Fetch release notes markdown from github api
    - Parse markdown for display
    - Remove comments and invalid link structures
    - Add hyperlinks to tickets, PRs, profiles and release comparisons
    - Infer release type by comparing semver against previous release
    
    * docs(PPDSC-2204): temporarily show a major release for review
    
    * docs(PPDSC-2204): fix github profile links
    
    * docs(PPDSC-2204): format date
    
    * docs(PPDSC-2204): add need help card
    
    * docs(PPDSC-2204): format notes typography
    
    * docs(PPDSC-2204): add github link
    
    * docs(PPDSC-2204): use page template
    
    * docs(PPDSC-2204): restructure so build passes
    
    * docs(PPDSC-2204): component tests added
    
    * docs(PPDSC-2204): restructure dirs
    
    * docs(PPDSC-2204): api call tests
    
    * docs(PPDSC-2204): markdown text colour
    
    * docs(PPDSC-2204): tidy up markdown
    
    * docs(PPDSC-2204): add margin between header and notes
    
    * docs(PPDSC-2204): replace anchors for link components
    
    * docs(PPDSC-2204): format links correctly
    
    * docs(PPDSC-2204): correct badge colours
    
    * docs(PPDSC-2204): update badge colours
    
    * docs(PPDSC-2204): update icon import
    
    * docs(PPDSC-2204): remove fake major release
    
    * docs(PPDSC-2204): update final release info

--- site/components/content-structure/content-section.tsx
+++ site/components/content-structure/content-section.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import {ContentSectionProps} from './types';
 
-export const ContentSection: React.FC<ContentSectionProps> = ({children}) => (
-  <section>{children}</section>
+export const ContentSection: React.FC<ContentSectionProps> = ({
+  children,
+  toc,
+  id,
+}) => (
+  <section id={id} data-toc-indexed={toc}>
+    {children}
+  </section>
 );
