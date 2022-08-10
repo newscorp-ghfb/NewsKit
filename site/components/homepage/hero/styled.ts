ecf458a4125b85d8b5c4b4bf3f523fc3c5688a1b
evgenitsn <evgeni.nikolov@news.co.uk>
2022-08-10 14:31:23 +0300

    docs(PPDSC-2299): hero background updates

--- site/components/homepage/hero/styled.ts
+++ site/components/homepage/hero/styled.ts
@@ -8,10 +8,20 @@ export const HeroGridContainer = styled(GridLayoutItem)<{
   background-repeat: no-repeat;
 
   ${getMediaQueryFromTheme('md')} {
-    background-image: ${({themeMode}) =>
-      themeMode === 'light'
-        ? 'url(static/landing/landing-background.svg)'
-        : 'url(static/landing/landing-background-dark.svg)'};
+    ${({theme}) => {
+      const heroGradientBackground = theme.colors.interfaceSkeleton010;
+      const heroGradientColor = theme.colors.interfaceBrand030;
+      return `background-image: radial-gradient(
+        circle at -120% 160%,
+        ${heroGradientColor} 0%,
+        ${heroGradientBackground}00 70%
+      ),
+      radial-gradient(
+        circle at 180% -70%,
+        ${heroGradientColor} 0%,
+        ${heroGradientBackground} 60%
+      );`;
+    }};
   }
 `;
 
