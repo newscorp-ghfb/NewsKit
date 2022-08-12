65acac7d15e516417b2ec19653b9ff27f67c3fab
mikef <mstuartflood@gmail.com>
2022-08-12 10:30:58 +0100

    feat(PPDSC-2242): optional standfirst explicit children
    - Make children optional in order for tests to compile.
    - Requited after the explicit children change in React 18.

--- src/standfirst/types.ts
+++ src/standfirst/types.ts
@@ -3,7 +3,7 @@ import {MQ} from '../utils/style';
 import {LogicalProps} from '../utils/logical-properties';
 
 export interface StandfirstProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span';
   overrides?: {
     styledText?: {
