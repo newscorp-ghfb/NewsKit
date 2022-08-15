550dd6bee622637b08629a80e42f889494b2e697
mikef <mstuartflood@gmail.com>
2022-08-15 16:35:32 +0100

    feat(PPDSC-2242): add undefined error text

--- src/form/form-entry.tsx
+++ src/form/form-entry.tsx
@@ -80,6 +80,6 @@ export const FormEntry = ({name, rules, children}: FormEntryProps) => {
     onChange: eventHandlerOnChange,
     state,
     ref: inputRef,
-    error: errorText as string,
+    error: errorText as string | undefined,
   });
 };
