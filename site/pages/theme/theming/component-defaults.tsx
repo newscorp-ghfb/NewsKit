77958711f953dc8258230029679ade1ea2cb7b2b
Srinivasan-Ramasamy <Srinivasan.Ramasamy@news.co.uk>
2022-08-19 11:38:51 +0530

    docs(PPDSC-2291): componen defaul ui changes

--- site/pages/theme/theming/component-defaults.tsx
+++ site/pages/theme/theming/component-defaults.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {InlineMessage, toNewsKitIcon} from 'newskit';
 import {Info as FilledInfo} from '@emotion-icons/material/Info';
+import {Code} from '../../../components/code';
 import {Link} from '../../../components/link';
 import {InlineCode} from '../../../components/markdown-elements';
 import {LayoutProps} from '../../../components/layout';
@@ -8,6 +9,9 @@ import {ComponentPageCell} from '../../../components/layout-cells';
 import {
   ContentSection,
   ContentPrimary,
+  ContentSecondary,
+  ContentTertiary,
+  ContentColSpan,
 } from '../../../components/content-structure';
 import {FoundationPageTemplate} from '../../../templates/foundation-page-template';
 
@@ -76,8 +80,200 @@ const ComponentDefaults = (layoutProps: LayoutProps) => (
               </InlineMessage>
             </>
           }
+          childrenColSpan={ContentColSpan.TEXT}
           showSeparator
         />
+      </ContentSection>
+
+      <ContentSection sectionName="overriding theme">
+        <ContentPrimary
+          id="overriding-theme"
+          toc="Overriding theme"
+          headline="Overriding component defaults at a theme level"
+          description={
+            <>
+              To override at the theme level, update a theme or{' '}
+              <Link href="/theme/theming/creating-a-theme/">
+                create a theme
+              </Link>{' '}
+              with the changes under the overrides property (similar to changing
+              other areas of the theme). Components or interface elements that
+              are utilising the updated property will reflect these changes.
+            </>
+          }
+          childrenColSpan={ContentColSpan.TEXT}
+        />
+        <ContentSecondary
+          headline="Example"
+          description={
+            <>
+              Here is an example of overriding the component defaults for the
+              medium-sized <Link href="/components/button/">button</Link>{' '}
+              component, changing the default{' '}
+              <Link href="/theme/presets/typography-presets/">
+                typography presets
+              </Link>{' '}
+              and{' '}
+              <Link href="/theme/presets/style-presets/">style presets:</Link>
+            </>
+          }
+          showSeparator
+        >
+          <Code>
+            {`import {createTheme} from 'newskit';
+
+const theme = createTheme({
+  name: 'newskit-component-defaults-change',
+  overrides: {
+    componentDefaults: {
+      button: {
+        medium: {
+          typographyPreset: 'utilityButton010',
+          stylePreset: 'buttonOutlinedPrimary',
+        },
+      },
+    }
+  },
+});`}
+          </Code>
+          <InlineMessage
+            icon={infoIcon}
+            role="region"
+            aria-label="component defaults"
+            overrides={{
+              marginBlockStart: 'space070',
+            }}
+          >
+            Some components, like the button, will have variants in their
+            component defaults such as size.
+          </InlineMessage>
+        </ContentSecondary>
+      </ContentSection>
+
+      <ContentSection sectionName="overriding component">
+        <ContentPrimary
+          id="overriding-component"
+          toc="Overriding component"
+          headline="Overriding component defaults at the component level"
+        />
+        <ContentSecondary headline="Example">
+          <Code>
+            {`import {Button} from 'newskit';
+
+// Overrides:
+<Button
+  size="medium"
+  overrides={{
+    typographyPreset: 'utilityButton010',
+    stylePreset: 'buttonOutlinedPrimary',
+  }}
+>
+  Content
+</Button>;
+
+`}
+          </Code>
+          <InlineMessage
+            icon={infoIcon}
+            title="Note"
+            role="region"
+            aria-label="overriding component"
+            overrides={{
+              marginBlockStart: 'space070',
+            }}
+          >
+            These overrides will only apply to that specific instance of the
+            component.
+          </InlineMessage>
+          <InlineMessage
+            icon={infoIcon}
+            role="region"
+            aria-label="variants"
+            overrides={{
+              marginBlockStart: 'space070',
+            }}
+          >
+            The variant (in this case, “medium”) is not included in the
+            overrides. Variants are not specified in the component overrides
+            prop, only the theme. Some components which are built using multiple
+            components may have a nested structure to their component defaults.
+          </InlineMessage>
+        </ContentSecondary>
+        <ContentSecondary
+          description={
+            <>
+              The example below shows overriding the{' '}
+              <Link href="/components/button/">button</Link> component
+              appearance:
+            </>
+          }
+          showSeparator
+        >
+          <Code>
+            {`// Component Defaults:
+byline: {
+  stylePreset: 'inkSubtle',
+  typographyPreset: 'utilityMeta020',
+  spaceStack: 'space020',
+  link: {
+    stylePreset: 'bylineLink',
+    typographyPreset: 'utilityMeta020',
+  },
+  divider: {
+    stylePreset: 'bylineDivider',
+    spaceInline: 'space020',
+  },
+},
+
+// Overrides
+<Byline
+  overrides={{
+    stylePreset: 'bylineCustom',
+    typographyPreset: 'bylineCustom',
+    spaceStack: 'space030',
+    link: {
+      stylePreset: 'bylineLinkCustom',
+      typographyPreset: 'bylineLinkCustom',
+    },
+    divider: {
+      stylePreset: 'bylineDividerCustom',
+      spaceInline: 'space030',
+    },
+  }}
+/>`}
+          </Code>
+        </ContentSecondary>
+      </ContentSection>
+
+      <ContentSection sectionName="breakpoint behaviour">
+        <ContentSecondary
+          headline="Breakpoint behaviour in component defaults"
+          description={
+            <>
+              Part of the functionality of the defaults and overrides system,
+              enables overriding of design tokens at specific breakpoints. For
+              example, setting different typography presets at different
+              breakpoints, can make the text larger on bigger devices than on
+              smaller devices.
+              <br /> <br />
+              Properties that support this are documented as using the generic{' '}
+              <InlineCode>MQ</InlineCode> type, i.e.{' '}
+              <InlineCode>MQ&#60;string&#62;</InlineCode>. This means passing a
+              single string token to be used at all times, or a combination at
+              breakpoints.
+            </>
+          }
+          childrenColSpan={ContentColSpan.TEXT}
+        />
+        <ContentTertiary showSeparator>
+          <Code>
+            {`typographyPreset: {
+    xs: 'editorialHeadline010',
+    md: 'editorialHeadline010',
+    lg: 'editorialHeadline010',
+  }`}
+          </Code>
+        </ContentTertiary>
       </ContentSection>
     </ComponentPageCell>
   </FoundationPageTemplate>
