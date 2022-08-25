cf138b12729dc271721b46fb82b5773f3fa0e7b2
Stoyan Delev <stoyan.delev@news.co.uk>
2022-08-25 08:48:22 +0300

    chore(PPDSC-2369): fix story

--- src/menu/__tests__/menu.stories.tsx
+++ src/menu/__tests__/menu.stories.tsx
@@ -12,7 +12,6 @@ import {MenuItemAlign, MenuItemSize} from '../types';
 import {styled, getColorCssFromTheme} from '../../utils';
 import {getSSRId} from '../../utils/get-ssr-id';
 import {createCustomThemeWithBaseThemeSwitch} from '../../test/theme-select-object';
-import {LinkInline} from '../../link';
 
 // eslint-disable-next-line no-script-url
 const href = 'javascript:;';
@@ -247,9 +246,7 @@ export const StoryMenuItemsStates = () => (
     <StorybookHeading>Menu items - states</StorybookHeading>
     <StorybookSubHeading>Menu items horizontal</StorybookSubHeading>
     <Menu aria-label={`Menu ${getSSRId()}`}>
-      <MenuItem href="" as="div">
-        <LinkInline href="/">Menu item as div</LinkInline>
-      </MenuItem>
+      <MenuItem href={href}>Menu item 1</MenuItem>
       <MenuItem href={href} selected>
         Menu item 2
       </MenuItem>
