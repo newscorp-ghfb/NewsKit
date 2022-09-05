84656458de472b4c51ad09b4e1668fadef72ccad
Srinivasan-Ramasamy <Srinivasan.Ramasamy@news.co.uk>
2022-09-05 14:54:18 +0530

    docs(PPDSC-2289): option section ui changes

--- site/pages/components/text-block.tsx
+++ site/pages/components/text-block.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import {Link} from '../../components/link';
 import {MetaStatus} from '../../components/meta/types';
+import {InlineCode} from '../../components/markdown-elements';
 import {LayoutProps} from '../../components/layout';
 import {ComponentPageTemplate} from '../../templates/component-page-template';
 import {getIllustrationComponent} from '../../components/illustrations/illustration-loader';
@@ -35,6 +37,62 @@ const TextBlockComponent = (layoutProps: LayoutProps) => (
           name: 'Text block',
           description: 'The text container',
           optional: undefined,
+        },
+      ],
+    }}
+    options={{
+      introduction:
+        'The text block has options that can be used to provide an appropriate experience for different use cases.',
+      cards: [
+        {
+          title: 'Render as',
+          description:
+            'By using Render as, the text block can be converted to another heading element or div.',
+          media: getIllustrationComponent(
+            'components/text-block/options/render',
+          ),
+        },
+        {
+          title: 'Text crop',
+          description: (
+            <>
+              Text cropping functionality can be disabled on the text block.{' '}
+              <Link href="/theme/foundation/fonts/#textcrop">Read more.</Link>
+            </>
+          ),
+          media: getIllustrationComponent(
+            'components/text-block/options/text-crop',
+          ),
+        },
+        {
+          title: 'Styling',
+          description: (
+            <>
+              Styling can be applied to the text block by using{' '}
+              <Link href="/theme/presets/style-presets/">style presets</Link>{' '}
+              and{' '}
+              <Link href="/theme/presets/typography-presets/">
+                typography presets.
+              </Link>
+            </>
+          ),
+          media: getIllustrationComponent(
+            'components/text-block/options/styling',
+          ),
+        },
+        {
+          title: 'Drop cap',
+          description: (
+            <>
+              The drop cap can be applied to a paragraph to increase the size of
+              the first <InlineCode>initial</InlineCode> letter. It has a number
+              of defaults which define the space around, and the visual
+              appearance of the drop cap.
+            </>
+          ),
+          media: getIllustrationComponent(
+            'components/text-block/options/drop-cap',
+          ),
         },
       ],
     }}
