5c25cd7ccdf8aaf6a2f7c180b35c89f2e35f30d5
mikef <mstuartflood@gmail.com>
2022-09-13 08:03:27 +0100

    fix(PPDSC-2312): audio player test improvements
    Main issues:
    - Player sometimes still in loading state when snapshot taken.
    - Arbitrary waits.
    - Buffered amount changes, making tests flaky.
    - Animations making screenshots inconsistent.
    - Time elapsed in autoplay tests not consistent.
    
    Fixes (to avoid ignore regions in tests):
    - Make sure all players in a test story are loaded before taking the snapshot using oncanplay event.
    - Prefetch src for all players so that content is fully buffered. Add separate tests for loading / unbuffered state.
    - Disable CSS animations to make screenshots consistent.
    - Remove visual tests for autoplay behaviour as this is not necessary. Also remove duplicate autoplay story.
    - Remove all arbitrary waits and ignore regions in tests.

---
+++ src/utils/audio-tests.tsx
@@ -0,0 +1,81 @@
+/* istanbul ignore file */
+
+import * as React from 'react';
+import {ComponentType, useEffect, useState} from 'react';
+import {AudioPlayerComposableProps} from '../audio-player-composable/types';
+import {AudioPlayerProps} from '../audio-player';
+
+const isVisualTest = process.env.APPLITOOLS_BATCH_ID !== undefined;
+
+type Props = AudioPlayerProps | AudioPlayerComposableProps;
+type Component =
+  | ComponentType<AudioPlayerProps>
+  | ComponentType<AudioPlayerComposableProps>;
+
+export const DISABLE_ANIMATIONS_SCRIPT =
+  'document.getElementsByTagName("circle")[0].style.animation = "none";';
+
+// We need a way to make sure all Audio Players are no longer in loading state
+// before taking the snaphot. One way is to conditionally show / hide an invisible
+// element on the page based on loading state, and pass this to the waitBeforeCapture
+// config for the test. But because Applitools only supports one selector in
+// waitBeforeCapture (and because runBefore doesn't work as documented), we cannot
+// have an element for each Player in a story. Instead, we need to count the number
+// of players in a story, and conditionally render the element when all are loaded.
+export const useAllPlayersCanPlayCheck = (nbPlayers: number) => {
+  const [nbLoaded, setNbLoaded] = useState<number>(0);
+  const onCanPlay = () => setNbLoaded(latest => latest + 1);
+  return {
+    allPlayersCanPlay: nbLoaded >= nbPlayers,
+    onCanPlay,
+  };
+};
+
+// The length of the buffer bar is controlled by the browser. The behaviour
+// is inconsistent, which makes tests flaky. As a solution, we prefetch src for
+// all Audio Players when running in visual tests so that the progress bar is
+// always full. There are separate visual tests for the loading / unbuffered state.
+const AudioPlayerBuffered = ({
+  comp: Comp,
+  props: {src, children, ...rest},
+}: {
+  comp: Component;
+  props: Props;
+}) => {
+  const [prefetchedSrc, setPrefetchedSrc] = useState<string | undefined>();
+  useEffect(() => {
+    fetch(src!)
+      .then(response => response.blob())
+      .then(b => URL.createObjectURL(b))
+      .then(url => setPrefetchedSrc(url));
+  }, [src]);
+  if (!prefetchedSrc) {
+    return null;
+  }
+  return (
+    <Comp src={prefetchedSrc} {...rest}>
+      {children}
+    </Comp>
+  );
+};
+
+// This is a wrapper component that allows us to use the prefetching logic in
+// the AudioPlayer and AudioPlayerComposable tests.
+export const VisualTestAudioPlayer = ({
+  comp: Comp,
+  props: {src, live, children, ...rest},
+}: {
+  comp: Component;
+  props: Props;
+}) => {
+  if (src && !live && isVisualTest) {
+    return (
+      <AudioPlayerBuffered comp={Comp} props={{src, live, children, ...rest}} />
+    );
+  }
+  return (
+    <Comp src={src!} live={live} {...rest}>
+      {children}
+    </Comp>
+  );
+};
