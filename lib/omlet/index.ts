12a3c889d9fa3b93f24988ffd18b3ac2614af80c
Aysha <ayshababur@yahoo.com>
2022-09-27 09:55:36 +0100

    feat(PPDSC-2415): feat-2415-updated npx command

--- lib/omlet/index.ts
+++ lib/omlet/index.ts
@@ -8,6 +8,20 @@ const projectsToAnalyse = {
     'input-flag-path': 'src/components/**/{index, styled}.{jsx,js,ts,tsx}',
     'ignore-flag-path': 'src/components/**/*.{stories,test}.{jsx,js,ts,tsx}',
   },
+  sun: {
+    'git-url': 'https://github.com/newsuk/nu-sun-titan',
+    'input-flag-path':
+      'packages/nuk-ge-sun-web-component-library/src/**/*/*.{jsx,js,ts,tsx}',
+    'ignore-flag-path':
+      'packages/shared-components/src/**/*.{stories,test}.{jsx,js,ts,tsx}',
+  },
+  // render: {
+  //   'git-url': 'https://github.com/newscorp-ghfb/ncu-newskit-render',
+  //   'input-flag-path': 'packages/shared-components/src/**/*.{jsx,js,ts,tsx}',
+  //   'input-flag-path-2': 'packages/checkout/src/**/*.{jsx,js,ts,tsx}',
+  //   'ignore-flag-path':
+  //     'packages/shared-components/src/**/*.{stories,test}.{jsx,js,ts,tsx}',
+  // },
 };
 
 const gitClonePromisified = (project, projectFolderName) =>
@@ -47,10 +61,14 @@ const omletScript = async () => {
     } catch (error) {
       console.error(error);
     }
-
+    console.log(
+      `repos/${projectFolderName}/${projectsToAnalyse[project]['input-flag-path']}`,
+      'hello',
+    );
     // Running Omlet analyse
+    // '/${projectsToAnalyse[project]['input-flag-path-two']}'
     shell.exec(
-      `npx omlet analyze -i 'repos/${projectFolderName}/${projectsToAnalyse[project]['input-flag-path']}' `,
+      `npx omlet analyze -i '${projectsToAnalyse[project]['input-flag-path']}' -r 'repos/${projectFolderName}'`,
     );
   });
 };
