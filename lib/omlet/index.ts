df48c405309d62cacab326d681c706c827e5dd9a
Aysha <ayshababur@yahoo.com>
2022-09-29 10:24:11 +0100

    feat(PPDSC-2415): feat-2415-fixed paths

--- lib/omlet/index.ts
+++ lib/omlet/index.ts
@@ -2,47 +2,41 @@ const shell = require('shelljs');
 const fs = require('fs-extra');
 const path = require('path');
 
+// name of project should match github repo name ie factiva-design-system
 const projectsToAnalyse = {
-  // TODO refactor using the real folder name. Use this key name instead of grabbing the folder name from git url
-  factiva: {
+  'factiva-design-system': {
     'git-url': 'https://github.com/newscorp-ghfb/factiva-design-system',
-    // TODO is path exhaustive? only "components"? need more complex path? array of multiple paths? ask patrick insight
     'input-flag-path': 'src/components/**/{index, styled}.{jsx,js,ts,tsx}',
-    // TODO is path exhaustive? only "components"? need more complex path? array of multiple paths? ask patrick insight
-    // test ignore when data can be deleted
     'ignore-flag-path': 'src/components/**/*.{stories,test}.{jsx,js,ts,tsx}',
   },
-  // sun: {
-  //   'git-url': 'https://github.com/newsuk/nu-sun-titan',
-  //   'packages': {
-  //     "nuk-ge-sun-web-component-library": {
-  //       // TODO is path exhaustive? only "components"? need more complex path? array of multiple paths? ask patrick insight
-  //       // see article-header for example
-  //       'input-flag-path': 'src/components/**/*.{jsx,js,ts,tsx}',
-  //       'ignore-flag-path': 'src/components/**/*.{stories,test,spec}.{jsx,tsx}',
-  //     },
-  //   },
-  // },
-  // render: {
-  //   // TODO remove it and change logic to.. "If includes "packages"
-  //   'git-url': 'https://github.com/newscorp-ghfb/ncu-newskit-render',
-  //   'packages': {
-  //     "shared-components": {
-  //       'input-flag-path': 'src/**/*.{jsx,js,ts,tsx}',
-  //     },
-  //     "checkout": {
-  //       'input-flag-path': 'src/**/*.{jsx,js,ts,tsx}',
-  //       // TODO check ignore path is ok
-  //       // 'ignore-flag-path': 'packages/checkout/src/**/*.{stories,test}.{jsx,js,ts,tsx}',
-  //     },
-  //   }
-  // },
+  'nu-sun-titan': {
+    'git-url': 'https://github.com/newsuk/nu-sun-titan',
+    packages: {
+      'nuk-ge-sun-web-component-library': {
+        'input-flag-path': 'src/components/**/*.{jsx,js,ts,tsx}',
+        'ignore-flag-path': 'src/components/**/*.{stories,test,spec}.{jsx,tsx}',
+      },
+    },
+  },
+  'ncu-newskit-render': {
+    'git-url': 'https://github.com/newscorp-ghfb/ncu-newskit-render',
+    packages: {
+      'shared-components': {
+        'input-flag-path': 'src/**/*.{jsx,js,ts,tsx}',
+        'ignore-flag-path': 'src/**/**/*.{stories,test}.{jsx,js,ts,tsx}',
+      },
+      checkout: {
+        'input-flag-path': 'src/**/**/**/*.{jsx,js,ts,tsx}',
+        'ignore-flag-path': 'src/**/*.{stories,test}.*',
+      },
+    },
+  },
 };
 
-const gitClonePromisified = (project, projectFolderName) =>
+const gitClonePromisified = project =>
   new Promise((resolve, reject) => {
     shell.exec(
-      `git clone ${projectsToAnalyse[project]['git-url']} repos/${projectFolderName}`,
+      `git clone ${projectsToAnalyse[project]['git-url']} repos/${project}`,
       {},
       (value, error) => {
         if (error) {
@@ -54,7 +48,6 @@ const gitClonePromisified = (project, projectFolderName) =>
   });
 
 const omletScript = async () => {
-
   // Creating repos folder
   await fs.promises
     .mkdir(path.join(process.cwd(), 'repos'), {recursive: true})
@@ -62,38 +55,41 @@ const omletScript = async () => {
 
   // Looping into "projectsToAnalyse"
   Object.keys(projectsToAnalyse).forEach(async project => {
-    const gitUrlSplit = projectsToAnalyse[project]['git-url'].split('/');
-    const projectFolderName = gitUrlSplit[gitUrlSplit.length - 1];
-    const isMonorepo = 'packages' in projectsToAnalyse[project]
+    const isMonorepo = 'packages' in projectsToAnalyse[project];
 
     // Removing old project folder, so we can pull an updated one
     await fs
-      .remove(path.join(process.cwd(), `repos/${projectFolderName}`))
-      .then(console.log(`Old ${projectFolderName} has been deleted ✅`));
+      .remove(path.join(process.cwd(), `repos/${project}`))
+      .then(console.log(`Old ${project} has been deleted ✅`));
     //
 
     // Git Cloning the repo
     try {
-      await gitClonePromisified(project, projectFolderName);
+      await gitClonePromisified(project);
     } catch (error) {
       console.error(error);
     }
 
     if (isMonorepo) {
-      // Object.keys(projectsToAnalyse[project]['packages']).forEach(subProject => {   
-      //   const subProjectObject = projectsToAnalyse[project]['packages'][subProject]
-      //   const inputFlagPath = subProjectObject['input-flag-path']
-      //   const ignoreFlagPath = subProjectObject['ignore-flag-path']
-
-      //   // -r flag is necessary for avoiding Omlet to take the name from the package.json existing in the path where the script is being run from.
-      //   shell.exec(
-      //     `npx omlet analyze -i '${inputFlagPath}' --ignore ${ignoreFlagPath} -r 'repos/${projectFolderName}/packages/${subProject}'`,
-      //   );
-      // })
-
+      Object.keys(projectsToAnalyse[project]['packages']).forEach(
+        subProject => {
+          const subProjectObject =
+            projectsToAnalyse[project]['packages'][subProject];
+          const inputFlagPath = subProjectObject['input-flag-path'];
+          const ignoreFlagPath = subProjectObject['ignore-flag-path'];
+          // -r flag is necessary for avoiding Omlet to take the name from the package.json existing in the path where the script is being run from.
+          shell.exec(
+            `npx omlet analyze -i '${inputFlagPath}' --ignore ${ignoreFlagPath} -r 'repos/${project}/packages/${subProject}'`,
+          );
+        },
+      );
     } else {
+      const ignoreFlag =
+        'ignore-flag-path' in projectsToAnalyse[project]
+          ? `--ignore '${projectsToAnalyse[project]['ignore-flag-path']}'`
+          : '';
       shell.exec(
-        `npx omlet analyze -i '${projectsToAnalyse[project]['input-flag-path']}' -r 'repos/${projectFolderName}'`,
+        `npx omlet analyze -i '${projectsToAnalyse[project]['input-flag-path']}' ${ignoreFlag} -r 'repos/${project}'`,
       );
     }
   });
