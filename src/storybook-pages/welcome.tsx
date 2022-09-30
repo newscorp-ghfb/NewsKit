29f30511f4d123f7f78480b9122a010b5dbe50b8
evgenitsn <evgeni.nikolov@news.co.uk>
2022-09-30 09:15:22 +0300

    feat(PPDSC-2418): address comments

--- src/storybook-pages/welcome.tsx
+++ src/storybook-pages/welcome.tsx
@@ -4,12 +4,10 @@ import {Divider} from '../divider';
 import {LinkStandalone} from '../link';
 import {Block} from '../block';
 import {Image} from '../image';
-import {
-  getBorderCssFromTheme,
-  getColorCssFromTheme,
-  getSpacingCssFromTheme,
-  styled,
-} from '../utils';
+import {getColorCssFromTheme, styled} from '../utils';
+import stylePresets from './style-presets';
+import {withOwnTheme} from '../utils/with-own-theme';
+import {GridLayout, GridLayoutItem} from '../grid-layout';
 
 const links = [
   {label: 'NewsKit website', url: 'https://newskit.co.uk/'},
@@ -27,13 +25,6 @@ const links = [
   },
 ];
 
-const WelcomeBanner = styled(Block)`
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-image: url('/welcome-banner.svg');
-  ${getBorderCssFromTheme('borderRadius', 'borderRadiusRounded020')};
-`;
-
 const WelcomeTitle = styled(H1)`
   max-width: 200px;
 `;
@@ -43,21 +34,22 @@ const WelcomeDescription = styled(P)`
 `;
 
 const WelcomeContainer = styled(Block)`
-  /* Welcome screen is not themeable so it's only in newskit light mode. */
+  /* Welcome screen is not themeable so its only appearance is in newskit light theme. */
   /* Doing this to stretch the internals of the Welcome screen from side to side to avoid cases where the dark theme is selected and there are dark borders around it. */
   width: 100vw;
   height: 100vh;
   box-sizing: border-box;
-  ${getColorCssFromTheme('backgroundColor', 'white')};
-  ${getSpacingCssFromTheme('paddingInline', 'space050')}
-  ${getSpacingCssFromTheme('paddingBlock', 'space050')}
-  margin-top: 0;
-  margin-left: -8px;
+  ${getColorCssFromTheme('backgroundColor', 'inkLight010')};
 `;
 
-export const Welcome = () => (
-  <WelcomeContainer marginBlockStart="space050" marginInline="space040">
-    <WelcomeBanner
+export const ThemelessWelcome = () => (
+  <WelcomeContainer
+    paddingInline="space050"
+    paddingBlock="space050"
+    marginInlineStart="-8px"
+  >
+    <Block
+      stylePreset="welcomeBanner"
       marginBlockEnd="space100"
       paddingInline="space080"
       paddingBlock="space080"
@@ -73,7 +65,7 @@ export const Welcome = () => (
       <WelcomeDescription overrides={{typographyPreset: 'utilityBody030'}}>
         An open source design system built by News UK, for everyone
       </WelcomeDescription>
-    </WelcomeBanner>
+    </Block>
     <H2
       overrides={{
         typographyPreset: 'utilitySubheading050',
@@ -83,21 +75,23 @@ export const Welcome = () => (
       Explore
     </H2>
     <Divider overrides={{marginBlockEnd: 'space060'}} />
-    {links.map(({label, url}) => (
-      <Block key={url} marginBlockEnd="space040">
-        <LinkStandalone
-          target="_blank"
-          overrides={{typographyPreset: 'utilityBody030'}}
-          href={url}
-        >
-          {label}
-        </LinkStandalone>
-      </Block>
-    ))}
+    <GridLayout rowGap="space040">
+      {links.map(({label, url}) => (
+        <GridLayoutItem key={url}>
+          <LinkStandalone
+            target="_blank"
+            overrides={{typographyPreset: 'utilityBody030'}}
+            href={url}
+          >
+            {label}
+          </LinkStandalone>
+        </GridLayoutItem>
+      ))}
+    </GridLayout>
     <LinkStandalone
       external={false}
       href="https://badge.fury.io/js/newskit"
-      overrides={{marginBlockStart: 'space050'}}
+      overrides={{marginBlockStart: 'space070'}}
     >
       <Image
         src="https://badge.fury.io/js/newskit.svg"
@@ -106,3 +100,8 @@ export const Welcome = () => (
     </LinkStandalone>
   </WelcomeContainer>
 );
+
+export const Welcome = withOwnTheme(ThemelessWelcome)({
+  defaults: {},
+  stylePresets,
+});
