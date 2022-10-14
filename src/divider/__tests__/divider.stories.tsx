71aeb51f6b5fece84f408289b792e0736b894926
Aysha <ayshababur@yahoo.com>
2022-10-14 13:14:53 +0100

    docs(PPDSC-2445): docs/PPDSE-2445 addressed comments

--- src/divider/__tests__/divider.stories.tsx
+++ src/divider/__tests__/divider.stories.tsx
@@ -9,12 +9,12 @@ import {StackChild} from '../../stack-child';
 import {getSizingCssFromTheme, getColorCssFromTheme} from '../../utils/style';
 import {compileTheme, createTheme} from '../../theme';
 
-const Box = styled.div<{marginRight?: boolean; transparent?: boolean}>`
+const Box = styled.div<{isMarginRight?: boolean; transparent?: boolean}>`
   display: inline-block;
   width: 48px;
   ${getSizingCssFromTheme('height', 'sizing100')};
-  ${({marginRight}) =>
-    marginRight ? ' margin-right:10px' : ' margin-right:0px'};
+  ${({isMarginRight}) =>
+    isMarginRight ? ' margin-right:10px' : ' margin-right:0px'};
   ${({transparent}) =>
     transparent
       ? getColorCssFromTheme('backgroundColor', 'transparent')
@@ -71,131 +71,123 @@ export const StoryDefault = () => (
 StoryDefault.storyName = 'Default';
 
 export const StoryHorizontal = () => (
-  <>
-    <StorybookPage columns={dividerCols}>
-      <StorybookCase title="Default">
-        <Divider />
-      </StorybookCase>
-      <StorybookCase title="In vertical block">
-        <BlockForDivider />
-        <Block paddingBlock="space010" />
-        <Divider />
-        <Block paddingBlock="space010" />
-        <BlockForDivider />
-      </StorybookCase>
+  <StorybookPage columns={dividerCols}>
+    <StorybookCase title="Default">
+      <Divider />
+    </StorybookCase>
+    <StorybookCase title="In vertical block">
+      <BlockForDivider />
+      <Block paddingBlock="space010" />
+      <Divider />
+      <Block paddingBlock="space010" />
+      <BlockForDivider />
+    </StorybookCase>
 
-      <StorybookCase title="In vertical stack without spacing">
-        <Stack stackDistribution="center" flow="vertical-center">
-          <BlockForDivider />
+    <StorybookCase title="In vertical stack without spacing">
+      <Stack stackDistribution="center" flow="vertical-center">
+        <BlockForDivider />
+        <Divider />
+        <BlockForDivider />
+      </Stack>
+    </StorybookCase>
+    <StorybookCase title="In vertical stack with spacing">
+      <StackForHorizontalDivider
+        stackDistribution="center"
+        flow="vertical-center"
+        spaceInline="space030"
+      >
+        <BlockForDivider>Block 1</BlockForDivider>
+
+        <StackChild alignSelf="stretch">
           <Divider />
-          <BlockForDivider />
-        </Stack>
-      </StorybookCase>
-      <StorybookCase title="In vertical stack with spacing">
-        <StackForHorizontalDivider
-          stackDistribution="center"
-          flow="vertical-center"
-          spaceInline="space030"
-        >
-          <BlockForDivider>Block 1</BlockForDivider>
-
-          <StackChild alignSelf="stretch">
-            <Divider />
-          </StackChild>
-          <BlockForDivider>Block 2</BlockForDivider>
-        </StackForHorizontalDivider>
-      </StorybookCase>
-    </StorybookPage>
-  </>
+        </StackChild>
+        <BlockForDivider>Block 2</BlockForDivider>
+      </StackForHorizontalDivider>
+    </StorybookCase>
+  </StorybookPage>
 );
 StoryHorizontal.storyName = 'Horizontal';
 
 export const StoryVertical = () => (
-  <>
-    <StorybookPage columns={verticalCols}>
-      <StorybookCase title="Default">
-        <Box transparent>
-          <Divider vertical />
-        </Box>
-      </StorybookCase>
-      <StorybookCase title="In horizontal block">
-        <Box marginRight />
-        <ContainerInline spaceInline="space020">
-          <Divider vertical />
-        </ContainerInline>
+  <StorybookPage columns={verticalCols}>
+    <StorybookCase title="Default">
+      <Box transparent>
+        <Divider vertical />
+      </Box>
+    </StorybookCase>
+    <StorybookCase title="In horizontal block">
+      <Box isMarginRight />
+      <ContainerInline spaceInline="space020">
+        <Divider vertical />
+      </ContainerInline>
+      <Box />
+    </StorybookCase>
+    <StorybookCase title="In horizontal Stack without spacing">
+      <Stack flow="horizontal-center" stackDistribution="flex-start">
         <Box />
-      </StorybookCase>
-      <StorybookCase title="In horizontal Stack without spacing">
-        <Stack flow="horizontal-center" stackDistribution="flex-start">
-          <Box />
-          <Divider vertical />
-          <Box />
-        </Stack>
-      </StorybookCase>
+        <Divider vertical />
+        <Box />
+      </Stack>
+    </StorybookCase>
 
-      <StorybookCase title="In horizontal Stack with spacing">
-        <Stack
-          flow="horizontal-center"
-          stackDistribution="flex-start"
-          spaceInline="space020"
-        >
-          <Box />
-          <StackChild alignSelf="stretch">
-            <Divider vertical />
-          </StackChild>
-          <Box />
-        </Stack>
-      </StorybookCase>
-    </StorybookPage>
-  </>
+    <StorybookCase title="In horizontal Stack with spacing">
+      <Stack
+        flow="horizontal-center"
+        stackDistribution="flex-start"
+        spaceInline="space020"
+      >
+        <Box />
+        <StackChild alignSelf="stretch">
+          <Divider vertical />
+        </StackChild>
+        <Box />
+      </Stack>
+    </StorybookCase>
+  </StorybookPage>
 );
 StoryVertical.storyName = 'Vertical';
 
 export const StoryResponsive = () => (
-  <>
-    <StorybookPage>
-      <StorybookCase title="Horizontal XS - Vertical MD - Vertical XL">
-        <div style={{height: 50}}>
-          <Divider vertical={{xs: false, sm: true, md: false, xl: true}} />
-        </div>
-      </StorybookCase>
-    </StorybookPage>
-  </>
+  <StorybookPage>
+    <StorybookCase title="Horizontal XS - Vertical MD - Vertical XL">
+      <div style={{height: 50}}>
+        <Divider vertical={{xs: false, sm: true, md: false, xl: true}} />
+      </div>
+    </StorybookCase>
+  </StorybookPage>
 );
 StoryResponsive.storyName = 'Breakpoint';
 
 export const StoryLogicalPropsOverrides = () => (
-  <>
-    <StorybookPage columns={dividerCols}>
-      <StorybookCase title="Padding overrides">
-        <BlockForDivider paddingInline="space030" />
-        <Divider
-          overrides={{paddingBlock: 'space030', paddingInline: 'space030'}}
-        />
-        <BlockForDivider paddingInline="space030" />
-      </StorybookCase>
-      <StorybookCase title="Margin overrides">
-        <BlockForDivider marginInline="space030" />
-        <Divider
-          overrides={{marginBlock: 'space030', marginInline: 'space030'}}
-        />
-        <BlockForDivider marginInline="space030" />
-      </StorybookCase>
-      <StorybookCase title="Styling">
-        <ThemeProvider theme={myCustomTheme}>
-          <Box transparent>
-            <Divider vertical overrides={{stylePreset: 'customDivider'}} />
-          </Box>
-        </ThemeProvider>
-      </StorybookCase>
-    </StorybookPage>
-  </>
+  <StorybookPage columns={dividerCols}>
+    <StorybookCase title="Padding overrides">
+      <BlockForDivider paddingInline="space030" />
+      <Divider
+        overrides={{paddingBlock: 'space030', paddingInline: 'space030'}}
+      />
+      <BlockForDivider paddingInline="space030" />
+    </StorybookCase>
+    <StorybookCase title="Margin overrides">
+      <BlockForDivider marginInline="space030" />
+      <Divider
+        overrides={{marginBlock: 'space030', marginInline: 'space030'}}
+      />
+      <BlockForDivider marginInline="space030" />
+    </StorybookCase>
+    <StorybookCase title="Styling">
+      <ThemeProvider theme={myCustomTheme}>
+        <Box transparent>
+          <Divider vertical overrides={{stylePreset: 'customDivider'}} />
+        </Box>
+      </ThemeProvider>
+    </StorybookCase>
+  </StorybookPage>
 );
 StoryLogicalPropsOverrides.storyName = 'Overrides';
 
 export default {
   title: 'Components/Divider',
-  component: 'Divider',
+  component: Divider,
   parameters: {
     nkDocs: {
       title: 'Divider',
