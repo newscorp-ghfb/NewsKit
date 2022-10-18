bb12db811beca4cfed4ef0ba399570e0a025fdf2
Xin00163 <wangxin00163@yahoo.co.uk>
2022-10-18 15:04:41 +0100

    fix(PPDSC-2416): update comments

--- site/utils/google-sheet/index.ts
+++ site/utils/google-sheet/index.ts
@@ -1,19 +1,23 @@
 import {google} from 'googleapis';
 
+// The sheet name
 const RANGE = 'Homepage';
+// Define the required scopes. In our case we only need read access.
+const SCOPE = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
 
 export async function getSheet() {
   try {
-    const scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+    // generate a JWT
     const jwt = new google.auth.JWT(
       process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
       undefined,
       // we need to replace the escaped newline characters
       // https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
       process.env.GOOGLE_SHEETS_PRIVATE_KEY!.replace(/\\n/g, '\n'),
-      scopes,
+      SCOPE,
     );
 
+    // initialize the Google Sheets API
     const sheets = google.sheets({version: 'v4', auth: jwt});
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId: process.env.SPREADSHEET_ID,
