cde4109a396a8e371b7e01432e88fd38b942bc8b
Stoyan Delev <stoyan.delev@news.co.uk>
2022-10-20 15:20:35 +0300

    fix(PPDSC-2494): fix areas with no space

--- src/grid-layout/__tests__/utils.test.tsx
+++ src/grid-layout/__tests__/utils.test.tsx
@@ -1,24 +1,35 @@
 import {areaToValidCSS, capitalize, extractAreas} from '../utils';
 
-test('capitalize should capitalize strings', () => {
-  expect(capitalize('component')).toEqual('Component');
-});
+const areasWithNoSpace = `
+A
+B
+C
+D`;
 
-test('should extract areas from string', () => {
-  expect(
-    extractAreas(`
+describe('GridLayout utils', () => {
+  test('capitalize should capitalize strings', () => {
+    expect(capitalize('component')).toEqual('Component');
+  });
+
+  test('should extract areas from string', () => {
+    expect(
+      extractAreas(`
     "A B"
     "A C"
     "A D"
     "A E"
   `),
-  ).toEqual(['A', 'B', 'A', 'C', 'A', 'D', 'A', 'E']);
-});
+    ).toEqual(['A', 'B', 'A', 'C', 'A', 'D', 'A', 'E']);
+  });
 
-test('should convert areas to valid CSS', () => {
-  // white space in the string is intentional, the function should handle that too
-  expect(
-    areaToValidCSS(`
+  test('should extract areas from string without space', () => {
+    expect(extractAreas(areasWithNoSpace)).toEqual(['A', 'B', 'C', 'D']);
+  });
+
+  test('should convert areas to valid CSS', () => {
+    // white space in the string is intentional, the function should handle that too
+    expect(
+      areaToValidCSS(`
 
     "A B"
     "A C"
@@ -26,11 +37,11 @@ test('should convert areas to valid CSS', () => {
     "A E"
 
   `),
-  ).toEqual(`"A B"\n"A C"\n"A D"\n"A E"`);
+    ).toEqual(`"A B"\n"A C"\n"A D"\n"A E"`);
 
-  // white space in the string is intentional, the function should handle that too
-  expect(
-    areaToValidCSS(`
+    // white space in the string is intentional, the function should handle that too
+    expect(
+      areaToValidCSS(`
 
     A B
     A C
@@ -38,5 +49,6 @@ test('should convert areas to valid CSS', () => {
     A E
 
   `),
-  ).toEqual(`"A B"\n"A C"\n"A D"\n"A E"`);
+    ).toEqual(`"A B"\n"A C"\n"A D"\n"A E"`);
+  });
 });
