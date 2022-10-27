51c479355a75e931455df368f9fbba6f5da95e4b
Luke Finch <lukechristianfinch@gmail.com>
2022-10-27 09:53:00 +0100

    fix(PPDSC-2387): match files with main

--- src/stack/stack.tsx
+++ src/stack/stack.tsx
@@ -11,7 +11,10 @@ import {
 import {StackChild, StackChildProps} from '../stack-child';
 import {hasMatchingDisplayNameWith, as as asUtil} from '../utils/component';
 import {MQ} from '../utils/style/types';
-import {extractLogicalPropsFromOverrides} from '../utils/logical-properties';
+import {
+  extractLogicalPropsFromOverrides,
+  omitLogicalPropsFromOverrides,
+} from '../utils/logical-properties';
 
 const getAsProp = (as: StackProps['as'], list: StackProps['list']) =>
   !list && as ? asUtil(as) : null;
@@ -102,8 +105,12 @@ const wrapChild = (
     );
   }
 
+  // If child is anything else
   return list || hasSpace ? (
-    <ChildContainer {...renderAs} {...childProps}>
+    <ChildContainer
+      {...renderAs}
+      {...omitLogicalPropsFromOverrides(childProps)}
+    >
       {child}
     </ChildContainer>
   ) : (
