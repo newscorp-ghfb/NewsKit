ff8e6d3c31adbcb91d6b699b2cf3463db26decbc
Rashika <Rashika.Mayilvahanan@news.co.uk>
2022-11-07 16:43:56 +0530

    docs(PPDSC-2388): revert fix

--- cypress/site/functional/section-navigation.spec.js
+++ cypress/site/functional/section-navigation.spec.js
@@ -2,14 +2,14 @@
 
 describe('section navigation', () => {
   it('should contain the correct section navigation links', () => {
-    cy.mockConsentAndVisit('/components/heading');
+    cy.mockConsentAndVisit('/components/text-input');
     cy.get('[data-testid="section-navigation"]')
       .find('li > a')
       .each(el => {
         cy.wrap(el).should(
           'have.attr',
           'href',
-          `/components/heading/#${el.text().toLowerCase()}`,
+          `/components/text-input/#${el.text().toLowerCase()}`,
         );
       });
   });
