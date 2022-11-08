36b9252d114485fa1d37a43e16aa0c3e7aba360a
Rashika <Rashika.Mayilvahanan@news.co.uk>
2022-11-08 12:05:00 +0530

    docs(PPDSC-2388): fixing build error

--- cypress/site/functional/section-navigation.spec.js
+++ cypress/site/functional/section-navigation.spec.js
@@ -2,14 +2,14 @@
 
 describe('section navigation', () => {
   it('should contain the correct section navigation links', () => {
-    cy.mockConsentAndVisit('/components/volume-control');
+    cy.mockConsentAndVisit('/components/text-input');
     cy.get('[data-testid="section-navigation"]')
       .find('li > a')
       .each(el => {
         cy.wrap(el).should(
           'have.attr',
           'href',
-          `/components/volume-control/#${el.text().toLowerCase()}`,
+          `/components/text-input/#${el.text().toLowerCase()}`,
         );
       });
   });
