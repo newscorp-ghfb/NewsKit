4f63651198a02e33bee8821a2c64aeccdd5e5dea
Luke Finch <lukechristianfinch@gmail.com>
2022-11-09 13:04:49 +0000

    docs(PPSDC-2426): remove extra block

--- site/templates/template-sections/introduction-section.tsx
+++ site/templates/template-sections/introduction-section.tsx
@@ -18,8 +18,6 @@ export const IntroductionSection: React.FC<IntroductionSectionProps> = ({
   meta,
 }) => (
   <Cell xs={12}>
-    <Block />
-
     <StyledSection id="introduction" data-toc-indexed="Introduction">
       <Grid xsMargin="space000" xsRowGutter="space000">
         <PageIntroduction {...pageIntroduction} />
