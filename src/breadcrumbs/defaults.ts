b97152daf5384c4c5160345b7830251260ab4c94
Aysha <ayshababur@yahoo.com>
2022-11-10 13:21:51 +0000

    feat(PPDSC-2088): feat/PPDSC-2088-updated defaults

--- src/breadcrumbs/defaults.ts
+++ src/breadcrumbs/defaults.ts
@@ -2,18 +2,15 @@ export default {
   breadcrumbItem: {
     small: {
       typographyPreset: 'utilityButton010',
-
       stylePreset: 'breadcrumbItem',
       size: 'iconSize020',
     },
     medium: {
       typographyPreset: 'utilityButton020',
-
       stylePreset: 'breadcrumbItem',
     },
     large: {
       typographyPreset: 'utilityButton030',
-
       stylePreset: 'breadcrumbItem',
     },
   },
