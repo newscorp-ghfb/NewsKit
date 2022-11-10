c66a6d87a738f25a8945fbc235145593477066e9
Aysha <ayshababur@yahoo.com>
2022-11-10 16:48:21 +0000

    feat(PPDSC-2088): feat/PPDSC-2088-updated stories

--- src/breadcrumbs/__tests__/breadcrumbs.stories.tsx
+++ src/breadcrumbs/__tests__/breadcrumbs.stories.tsx
@@ -2,33 +2,23 @@ import React from 'react';
 import {Breadcrumbs} from '../breadcrumbs';
 import {BreadcrumbItem} from '../breadcrumb-item';
 import {StorybookPage} from '../../test/storybook-comps';
-import {IconFilledAccountBalance} from '../../icons';
+import {IconFilledAccountBalance, IconFilledAddCircle} from '../../icons';
 
 // eslint-disable-next-line no-script-url
 const href = 'javascript:;';
 
 export const StoryDefault = () => (
-  <StorybookPage>
-    <Breadcrumbs size="small">
+  <StorybookPage columns={{md: 'auto'}}>
+    <Breadcrumbs showTrailingSeparator size="small">
       <BreadcrumbItem selected href={href}>
-        hello
+        Breadcrumb item
       </BreadcrumbItem>
-      <BreadcrumbItem href={href}>bye</BreadcrumbItem>
-      <BreadcrumbItem href={href}>hello</BreadcrumbItem>
-      <BreadcrumbItem href={href}>bye</BreadcrumbItem>
+      <BreadcrumbItem href={href}>Breadcrumb item</BreadcrumbItem>
+      <BreadcrumbItem href={href}>Breadcrumb item </BreadcrumbItem>
     </Breadcrumbs>
     <Breadcrumbs
-      size="small"
       overrides={{
-        separator: props => (
-          <IconFilledAccountBalance
-            {...props}
-            overrides={{
-              size: 'iconSize020',
-              stylePreset: 'inkSubtle',
-            }}
-          />
-        ),
+        separator: props => <IconFilledAccountBalance {...props} />,
       }}
     >
       <BreadcrumbItem href={href}>hello</BreadcrumbItem>
@@ -39,6 +29,41 @@ export const StoryDefault = () => (
   </StorybookPage>
 );
 StoryDefault.storyName = 'Default';
+
+export const StoryVariations = () => (
+  <StorybookPage columns={{md: 'auto'}}>
+    <Breadcrumbs>
+      <BreadcrumbItem selected href={href}>
+        Breadcrumb item
+      </BreadcrumbItem>
+      <BreadcrumbItem href={href}>Breadcrumb item</BreadcrumbItem>
+      <BreadcrumbItem href={href}>
+        <IconFilledAddCircle overrides={{stylePreset: 'inkSubtle'}} />
+        Breadcrumb item
+      </BreadcrumbItem>
+    </Breadcrumbs>
+    {/* <Breadcrumbs
+        size="small"
+        overrides={{
+          separator: props => (
+            <IconFilledAccountBalance
+              {...props}
+              overrides={{
+                size: 'iconSize020',
+                stylePreset: 'inkSubtle',
+              }}
+            />
+          ),
+        }}
+      >
+        <BreadcrumbItem href={href}>hello</BreadcrumbItem>
+        <BreadcrumbItem href={href}>bye</BreadcrumbItem>
+        <BreadcrumbItem href={href}>hello</BreadcrumbItem>
+        <BreadcrumbItem href={href}>bye</BreadcrumbItem>
+      </Breadcrumbs> */}
+  </StorybookPage>
+);
+StoryVariations.storyName = 'Variations';
 
 export default {
   title: 'Components/Breadcrumbs',
