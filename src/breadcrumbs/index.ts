35a2ed34a10fefdd082902c9b298a0d42d2a3242
Aysha <ayshababur@yahoo.com>
2022-11-15 15:23:38 +0000

    feat(PPDSC-2088): feat/PPDSC-2088-updated index

---
+++ src/breadcrumbs/index.ts
@@ -0,0 +1,7 @@
+export {Breadcrumbs} from './breadcrumbs';
+export {BreadcrumbItem} from './breadcrumb-item';
+export type {
+  BreadcrumbsProps,
+  BreadcrumbItemProps,
+  BreadcrumbItemSize,
+} from './types';
