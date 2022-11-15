e05b2f6bd08abe71e661b4e10e80f59fb865cfd4
Xin00163 <wangxin00163@yahoo.co.uk>
2022-11-15 13:09:37 +0000

    fix(PPDSC-2540): tests

--- site/utils/google-sheet/__tests__/google-sheet.test.tsx
+++ site/utils/google-sheet/__tests__/google-sheet.test.tsx
@@ -1,7 +1,7 @@
 import {getValueFromCMS, formatSheetData} from '../utils';
 
 describe('formatSheetData', () => {
-  it('should convert google sheet data to an object', async () => {
+  it('should convert google sheet data to an object', () => {
     const values = [
       ['HeroCardTitle', 'Latest Blog'],
       ['', 'NewsKit'],
@@ -12,7 +12,7 @@ describe('formatSheetData', () => {
       '': 'NewsKit',
     });
   });
-  it('should remove the item whose value is undefined', async () => {
+  it('should remove the item whose value is undefined', () => {
     const values = [['HeroCardTitle', 'Latest Blog'], ['NewsKit']];
     const result = formatSheetData(values);
     expect(result).toEqual({
@@ -28,15 +28,15 @@ describe('getValueFromCMS', () => {
       "How an audio player component tells the story of NewsKit Design System's changing strategy",
     '': 'NewsKit',
   };
-  it('should return the correct content', async () => {
+  it('should return the correct content', () => {
     const result = getValueFromCMS(content, 'HeroCardTitle', 'NewsKit');
     expect(result).toEqual(content.herocardtitle);
   });
-  it('should return feedback content if key is not found', async () => {
+  it('should return feedback content if key is not found', () => {
     const result = getValueFromCMS(content, 'LatestBlog', 'NewsKit');
     expect(result).toEqual('NewsKit');
   });
-  it('should still return the correct content when case is not the same', async () => {
+  it('should still return the correct content when case is not the same', () => {
     const result = getValueFromCMS(content, 'Herocardtitle', 'NewsKit');
     expect(result).toEqual(content.herocardtitle);
   });
