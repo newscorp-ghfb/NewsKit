f96aa1b4baec6a9ef804970872b2d89c27f2c78c
Aysha <ayshababur@yahoo.com>
2022-11-18 09:30:00 +0000

    feat(PPDSC-2088): feat/PPDSC-2088-updated types

--- src/breadcrumbs/types.ts
+++ src/breadcrumbs/types.ts
@@ -2,8 +2,16 @@ import {MQ} from '../utils';
 import {LogicalProps} from '../utils/logical-properties';
 
 import {EventData} from '../instrumentation';
+import {Override} from '../utils/overrides';
+import {NewsKitIconProps} from '../icons';
 
 export type BreadcrumbSize = 'small' | 'medium' | 'large';
+
+interface SeparatorProps {
+  separator?: {
+    paddingInline: LogicalProps;
+  } & Override<NewsKitIconProps>;
+}
 
 export interface BreadcrumbsProps extends React.AriaAttributes {
   children: Exclude<React.ReactNode, 'undefined'>;
@@ -11,7 +19,7 @@ export interface BreadcrumbsProps extends React.AriaAttributes {
   showTrailingSeparator?: boolean;
   overrides?: {
     stylePreset?: MQ<string>;
-    separator?: React.ComponentType;
+    separator?: SeparatorProps;
   } & LogicalProps;
 }
 
