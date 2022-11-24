32996732282ac850c70a5abec17d69471e91e27f
mikef <mstuartflood@gmail.com>
2022-11-24 17:55:31 +0000

    chore(PPDSC-2481): add marketwatch and wsj themes

--- src/test/theme-select-object.ts
+++ src/test/theme-select-object.ts
@@ -8,6 +8,8 @@ import {
 import {tnlTheme} from '../theme-checker/themes/tnl-theme/tnl-theme';
 import {virginTheme} from '../theme-checker/themes/virgin-theme/virgin-theme';
 import {sunTheme} from '../theme-checker/themes/sun-theme/sun-theme';
+import {marketWatchTheme} from '../theme-checker/themes/market-watch-theme/market-watch-theme';
+import {wsjTheme} from '../theme-checker/themes/wsj-theme/wsj-theme';
 
 export const themeObject: {[key: string]: UncompiledTheme} = {
   transparent: newskitLightTheme,
@@ -16,6 +18,8 @@ export const themeObject: {[key: string]: UncompiledTheme} = {
   '#eb1801': sunTheme,
   '#e10a0a': virginTheme,
   '#006699': tnlTheme,
+  '#367F2E': marketWatchTheme,
+  '#0274B6': wsjTheme,
 };
 
 export const getThemeObject = (key: string): UncompiledTheme => {
