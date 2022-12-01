daf160b5a8f2378f308b59e9f9dd6f0cee5739c3
Stoyan Delev <stoyan.delev@news.co.uk>
2022-12-01 10:35:23 +0200

    feat(PPDSC-2597): fix types and update stories

--- src/select/__tests__/select.stories.tsx
+++ src/select/__tests__/select.stories.tsx
@@ -1208,35 +1208,90 @@ export const StorySelectIconOverrides = () => (
 );
 StorySelectIconOverrides.storyName = 'Select Icon overrides';
 
-export const StorySelectWithOverflowScroll = () => (
-  <div style={{height: '90vh', width: '100%', overflow: 'hidden'}}>
-    <div
-      style={{
-        height: '90vh',
-        position: 'relative',
-        overflow: 'auto',
-        border: '3px solid blue',
-        boxSizing: 'border-box',
-      }}
-    >
+/**
+
+PLEASE IGNORE THIS STORY
+ITS JUST FOR TESTING AND WILL BE REMOVED BEFORE MERGE
+ */
+
+function useLocalStorage(key, initialValue) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = React.useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = value => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+}
+
+const temp = Array.from(Array(200).keys()).map(v => String(v));
+
+export const StorySelectWithOverflowScroll = () => {
+  const [selected, setSelected] = useLocalStorage('zone', '');
+
+  return (
+    <div style={{height: '90vh', width: '100%', overflow: 'hidden'}}>
       <div
         style={{
-          padding: '75vh 0',
+          height: '90vh',
           position: 'relative',
-          border: '3px solid red',
+          overflow: 'auto',
+          border: '3px solid blue',
           boxSizing: 'border-box',
         }}
       >
-        <Select>
-          <SelectOption value="A"> Pikachu </SelectOption>
-          <SelectOption value="B"> Charmander </SelectOption>
-          <SelectOption value="C"> Pikachu </SelectOption>
-          <SelectOption value="D"> Pikachu </SelectOption>
-        </Select>
+        <div
+          style={{
+            padding: '75vh 0',
+            position: 'relative',
+            border: '3px solid red',
+            boxSizing: 'border-box',
+          }}
+        >
+          <Select
+            onChange={e => setSelected(e.target.value)}
+            virtualized={10000}
+          >
+            {temp.map(v => (
+              <SelectOption key={v} value={v} selected={v === selected}>
+                {v}
+              </SelectOption>
+            ))}
+          </Select>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default {
   title: 'Components/select',
