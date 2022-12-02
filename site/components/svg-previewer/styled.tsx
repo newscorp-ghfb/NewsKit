cdc2dd782d14c41a8c6102103e8e157868f6c0ae
Luke Finch <lukechristianfinch@gmail.com>
2022-12-02 13:53:53 +0000

    feat(PPDSC-2555): SVG Exporter - Export with CSS vars (#493)
    * feat(PPDSC-2555): remove tsxy stuff, improve ui
    
    * feat(PPDSC-2555): rename tools component folde
    
    * feat(PPDSC-2555): fix select button width
    
    * feat(PPDSC-2555): update snapshots
    
    * feat(PPDSC-2555): Revert "feat(PPDSC-2555): fix select button width"
    
    This reverts commit 28c350108723d3cbf423d481feb2d96f4fdb0014.
    
    * feat(PPDSC-2555): fix missing select
    
    * feat(PPDSC-2555): revert snapshot updates
    
    * feat(PPDSC-2555): fix keys, fix margin
    
    * feat(PPDSC-2555): update tests
    
    * feat(PPDSC-2555): remove old code
    
    * feat(PPDSC-2555): merge conflict
    
    * feat(PPDSC-2555): merge conflicts
    
    * feat(PPDSC-2555): update snapshot

---
+++ site/components/svg-previewer/styled.tsx
@@ -0,0 +1,26 @@
+import {Button, styled, GridLayout, getColorCssFromTheme} from 'newskit';
+
+export const StyledSingleSVGDownloadButton = styled(Button)`
+  margin-right: 20px;
+`;
+
+export const StyledSvgGroupContainer = styled(GridLayout)``;
+
+export const StyledButtonsContainer = styled(GridLayout)`
+  position: fixed;
+  top: var(--headerSize);
+  margin-top: -12px;
+  ${getColorCssFromTheme('background', 'interface020')}
+  ${getColorCssFromTheme('borderBottomColor', 'interface040')}
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+  z-index: 1000;
+`;
+
+export const StyledSingleSvgWrapper = styled.div`
+  padding: 0 10vw;
+  svg {
+    max-width: 80vw;
+    max-height: 80vh;
+  }
+`;
