c8c269236d8ee9eb2a6f78f7d7ce6180f29b3857
Vanals <marco91vanali@gmail.com>
2022-12-16 09:42:24 +0000

    feat(PPDSC-0000): improved feedbacks

--- site/components/vocal-navigator/vocal-navigator-modal.tsx
+++ site/components/vocal-navigator/vocal-navigator-modal.tsx
@@ -32,7 +32,11 @@ const VocalNavigatorModal: React.FC<{isOpen: boolean; setIsOpen: Function}> = ({
   }, [isOpen]);
 
   const askUserConfirmation = () => {
-    speakEnhanced(`Did you say ${transcript}?`)
+    if (transcript === undefined || transcript === '') {
+      speakEnhanced('I am sorry, I did not catch what you said')
+    } else {
+      speakEnhanced(`Did you say ${transcript}?`)
+    }
   };
 
   const {listen, listening, stop} = useSpeechRecognition({
@@ -41,7 +45,7 @@ const VocalNavigatorModal: React.FC<{isOpen: boolean; setIsOpen: Function}> = ({
     },
     onEnd: () => {
       askUserConfirmation();
-      setDisplayConfirmationButton(true);
+      if (transcript !== undefined) setDisplayConfirmationButton(true);
     },
   });
 
@@ -53,6 +57,35 @@ const VocalNavigatorModal: React.FC<{isOpen: boolean; setIsOpen: Function}> = ({
     stop();
   };
 
+  const searchPage = () => {
+    let resultComponent
+    // @ts-ignore Components
+     routes[3].subNav[1].subNav.forEach((nav: {title: string}) => {
+      if (nav.title.toLowerCase() === transcript?.toLowerCase()) {
+        resultComponent = nav.id
+      }
+    });
+    if (resultComponent) return resultComponent
+    
+    let resultFoundation
+    // @ts-ignore Foundations
+    routes[2].subNav[1].subNav.forEach((nav: {title: string}) => {
+      if (nav.title.toLowerCase() === transcript?.toLowerCase()) {
+        resultFoundation = nav.id
+      }
+    });
+    if (resultFoundation) return resultFoundation
+    
+    let resultPreset
+    // @ts-ignore Presets Presets
+    routes[2].subNav[2].subNav.forEach((nav: {title: string}) => {
+      if (nav.title.toLowerCase() === transcript?.toLowerCase()) {
+        resultPreset = nav.id
+      }
+    });
+    if (resultPreset) return resultPreset 
+  };
+
   const handleNoButton = () => {
     speakEnhanced(`I am sorry, I am still learning, please try again`)
     setTranscript('');
@@ -60,31 +93,14 @@ const VocalNavigatorModal: React.FC<{isOpen: boolean; setIsOpen: Function}> = ({
   };
 
   const handleYesButton = () => {
-    searchAndTakeUserToPage()
+    const result = searchPage()
+    if (result === undefined) {
+      speakEnhanced(`I am sorry, I did not find anything about ${transcript}, try searching something else`)
+    } else {
+      speakEnhanced(`Thank you, I am taking you there`)
+      return window.location.href = result;
+    }
   }
-
-  const searchAndTakeUserToPage = () => {
-    // @ts-ignore SEARCHING COMPONENTS DOCS
-    routes[3].subNav[1].subNav.forEach((nav: {title: string}) => {
-      if (nav.title.toLowerCase() === transcript?.toLowerCase()) {
-        window.location.href = `${nav.id}`;
-      }
-    });
-
-    // @ts-ignore Foundations
-    routes[2].subNav[1].subNav.forEach((nav: {title: string}) => {
-      if (nav.title.toLowerCase() === transcript?.toLowerCase()) {
-        window.location.href = `${nav.id}`;
-      }
-    });
-
-    // @ts-ignore Presets Presets
-    routes[2].subNav[2].subNav.forEach((nav: {title: string}) => {
-      if (nav.title.toLowerCase() === transcript?.toLowerCase()) {
-        window.location.href = `${nav.id}`;
-      }
-    });
-  };
 
   return (
     <>
