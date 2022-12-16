4cfaee729ba5776eb08638f15a0df09b7472bf95
Xin00163 <wangxin00163@yahoo.co.uk>
2022-12-16 10:30:53 +0000

    fix(PPPP-0000): show children & parent

--- site/pages/tools/editor-components/elements-list.jsx
+++ site/pages/tools/editor-components/elements-list.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import {styled} from 'newskit';
 import ElementListItemDraggable from './element-list-draggable';
+
+const StyledDiv = styled.div`
+  height: 100%;
+`;
 
 export const ElementsList = ({
   elements,
@@ -8,22 +13,35 @@ export const ElementsList = ({
   onHover,
   onUnhover,
 }) => (
-  <div style={{height: '100%'}}>
-    {elements.map(
-      (element, index) =>
-        element && (
+  <StyledDiv>
+    {elements.map((element, index) =>
+      element.children.length === 0 ? (
+        <ElementListItemDraggable
+          key={element.id}
+          type={element.type}
+          index={index}
+          moveItem={moveItem}
+          id={element.id}
+          onSelect={onSelect}
+          onHover={onHover}
+          onUnhover={onUnhover}
+          name={element.componentName}
+        />
+      ) : (
+        element.children.map((child, childIndex) => (
           <ElementListItemDraggable
-            key={element.id}
-            type={element.type}
-            index={index}
+            key={child.id}
+            type={child.type}
+            index={childIndex}
             moveItem={moveItem}
-            id={element.id}
+            id={child.id}
             onSelect={onSelect}
             onHover={onHover}
             onUnhover={onUnhover}
             name={element.componentName}
           />
-        ),
+        ))
+      ),
     )}
-  </div>
+  </StyledDiv>
 );
