7dbed54d3e5f8c4f8d1aba4370b934e40efa3600
Vanals <marco91vanali@gmail.com>
2022-12-16 14:29:40 +0000

    feat(PPDSC-0000): ignore no ts package

--- site/components/vocal-navigator/vocal-navigator-modal.tsx
+++ site/components/vocal-navigator/vocal-navigator-modal.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+// @ts-ignore
 import {useSpeechSynthesis, useSpeechRecognition} from 'react-speech-kit';
 import {Block} from '../../../src/block';
 import {Button} from '../../../src/button';
