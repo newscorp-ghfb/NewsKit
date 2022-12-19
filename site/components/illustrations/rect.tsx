6dedefab23907099b4c523078f9ca22aeb68cce5
evgenitsn <evgeni.nikolov@news.co.uk>
2022-12-19 15:35:43 +0200

    chore(PPDSC-2592): remove all unused .tsx illustrations

--- site/components/illustrations/rect.tsx
+++
@@ -1,8 +0,0 @@
-import {useTheme} from 'newskit';
-import React, {SVGAttributes} from 'react';
-import {toCSSVar} from './utils';
-
-export const Rect: React.FC<SVGAttributes<SVGRectElement>> = props => {
-  const theme = useTheme();
-  return <rect {...toCSSVar(theme, props)} />;
-};
--- site/components/illustrations/utils.ts
+++ site/components/illustrations/utils.ts
@@ -1,25 +1,3 @@
-import {SVGAttributes} from 'react';
-import {Theme} from '../../../src/theme';
-
-// @ts-ignore
-export const toCSSVar = (theme: Theme, props) => {
-  const inlineStyle = {} as SVGAttributes<SVGPathElement>;
-  const newProps = {...props};
-
-  if (theme.colors[props?.fill!]) {
-    inlineStyle.fill = `var(--color-${props.fill})`;
-    delete newProps.fill;
-  }
-  if (theme.colors[props?.stroke!]) {
-    inlineStyle.stroke = `var(--color-${props.fill})`;
-    delete newProps.stroke;
-  }
-  return {
-    style: inlineStyle,
-    ...newProps,
-  };
-};
-
 const escapeRegExp = (string: string): string =>
   // $& means the whole matched string
   string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
