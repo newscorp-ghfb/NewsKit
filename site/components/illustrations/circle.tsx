4e15f2cbeef12512b3eab33d2996c19f92403c06
Evgeni Nikolov <evgeni.nikolov@news.co.uk>
2022-12-20 10:09:10 +0200

    chore(PPDSC-2592): remove all unused .tsx illustrations (#526)

--- site/components/illustrations/circle.tsx
+++
@@ -1,8 +0,0 @@
-import {useTheme} from 'newskit';
-import React, {SVGAttributes} from 'react';
-import {toCSSVar} from './utils';
-
-export const Circle: React.FC<SVGAttributes<SVGCircleElement>> = props => {
-  const theme = useTheme();
-  return <circle {...toCSSVar(theme, props)} />;
-};
