608851a647926c9dc6b64c41204023af839018f0
Aysha <ayshababur@yahoo.com>
2022-12-29 10:51:12 +0000

    fix(PPDSE-2633): fix-ppdse-2633 updated snaps

--- src/radio-button/__tests__/radio-button.stories.tsx
+++ src/radio-button/__tests__/radio-button.stories.tsx
@@ -294,14 +294,14 @@ export const StoryRadioButtonOverrides = () => {
     spaceStack: 'space060',
     input: {
       stylePreset: 'customRadioInput',
-      // size: '68px',
+      size: '68px',
     },
     feedback: {
       size: '90px',
       stylePreset: 'customRadioFeedback',
     },
     label: {
-      typographyPreset: 'editorialHeadline080',
+      typographyPreset: 'utilityHeading010',
       stylePreset: 'inkSubtle',
     },
     icon: {
