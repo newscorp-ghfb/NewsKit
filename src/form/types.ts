dc10829019578e0467d9686a2700e0e6602e44ae
Stoyan Delev <stoyan.delev@news.co.uk>
2022-12-30 14:21:28 +0200

    chore(PPDSE-0000): add correct type

--- src/form/types.ts
+++ src/form/types.ts
@@ -1,9 +1,9 @@
 import React from 'react';
 import {
+  Resolver,
   SubmitErrorHandler,
   SubmitHandler,
   UseFormReturn,
-  UseFormProps,
 } from 'react-hook-form';
 import {LogicalProps} from '../utils/logical-properties';
 import {MQ} from '../utils/style';
@@ -25,7 +25,7 @@ export interface FormProps
   validationMode?: 'onBlur' | 'onSubmit';
   reValidationMode?: 'onBlur' | 'onSubmit';
   defaultValues?: Record<string, string>;
-  resolver?: UseFormProps['resolver'];
+  resolver?: Resolver;
 }
 
 export interface FormRef {
