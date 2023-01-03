d9acb9d1965800a40aff8ac1f1f71f66c2065308
evgenitsn <evgeni.nikolov@news.co.uk>
2023-01-03 15:22:33 +0200

    docs(PPDSC-2559): build 404 page

--- site/pages/404.tsx
+++ site/pages/404.tsx
@@ -1,59 +1,105 @@
 import React from 'react';
-import {Block, Cell, Image, toNewsKitIcon} from 'newskit';
+import {
+  Button,
+  GridLayout,
+  GridLayoutItem,
+  Hidden,
+  TextBlock,
+  toNewsKitIcon,
+} from 'newskit';
 import {KeyboardArrowLeft as FilledKeyboardArrowLeft} from '@emotion-icons/material/KeyboardArrowLeft';
-import {ContentPrimary, ContentSection} from '../components/content-structure';
 import Layout, {LayoutProps} from '../components/layout';
 import {HeadNextSeo} from '../components/head-next-seo';
-import {Link} from '../components/link';
+import {HeroContainer} from '../components/homepage/hero/styled';
+import {Illustration} from '../components/illustrations/illustration-loader';
 
 const IconFilledKeyboardArrowLeft = toNewsKitIcon(FilledKeyboardArrowLeft);
 
-const Custom404 = ({path, ...props}: LayoutProps) => (
-  <Layout {...props} hideSidebar path={`${path}-new`}>
+const Custom404 = ({path, ...layoutProps}: LayoutProps) => (
+  <Layout {...layoutProps} newPage hideFooter hideSidebar path={`${path}-new`}>
     <HeadNextSeo
       title="404 page"
-      description="Have a question about our design system? 
+      description="Have a question about our design system?
         The NewsKit team is here to help you."
       image={{
         url: 'social/landing.png',
         alt: '404 page',
       }}
     />
-    <Cell xs={12} md={10} mdOffset={1} lg={8} lgOffset={2} xl={6} xlOffset={3}>
-      <Block spaceStack={{xs: 'space045', sm: 'space050', md: 'space070'}}>
-        <Image
-          loadingAspectRatio="16:9"
-          alt="404page"
-          src="static/placeholder-16x9.png"
-        />
-      </Block>
-      <ContentSection sectionName="page not found">
-        <ContentPrimary
-          headline="Page not found"
-          description={
-            <>
-              <Block spaceStack={{xs: 'space050', md: 'space060'}}>
-                We can&apos;t seem to find what you&apos;re looking for. If you
-                typed in the website address, please check it and try again.
-              </Block>
-              <Link
-                data-testid="back-link"
-                href="/"
-                aria-label="back"
-                type="standalone"
-              >
-                <IconFilledKeyboardArrowLeft
-                  overrides={{
-                    size: 'iconSize020',
-                  }}
-                />
-                Go back to the homepage
-              </Link>
-            </>
-          }
-        />
-      </ContentSection>
-    </Cell>
+
+    <HeroContainer>
+      <GridLayout
+        overrides={{
+          maxWidth: '1150px',
+          marginInline: 'auto',
+          width: '100%',
+          paddingInline: {
+            xs: 'space050',
+            sm: 'space070',
+            md: 'space100',
+            lg: 'space080',
+          },
+        }}
+      >
+        <GridLayout
+          columns="1fr minmax(min-content, 530px)"
+          overrides={{marginBlock: {xs: 'space070', lg: 'space000'}}}
+          alignItems="center"
+        >
+          <GridLayoutItem
+            paddingBlock={{
+              xs: 'space000',
+              md: 'space110',
+            }}
+            column={{
+              xs: `1 / span 2`,
+              lg: `1 / span 1`,
+            }}
+          >
+            <TextBlock
+              as="h1"
+              stylePreset="inkBase"
+              paddingInlineEnd={{
+                xs: 'space100',
+              }}
+              typographyPreset={{
+                xs: 'editorialHeadline070',
+                md: 'editorialHeadline080',
+                lg: 'editorialDisplay020',
+              }}
+            >
+              Page not found
+            </TextBlock>
+            <TextBlock
+              marginBlockStart={{xs: 'space060', lg: 'space080'}}
+              stylePreset="inkBase"
+              typographyPreset="editorialSubheadline020"
+              marginInlineEnd={{
+                xs: 'space020',
+                md: 'space120',
+                lg: 'space000',
+              }}
+              paddingInlineEnd={{
+                xs: 'space090',
+                md: 'space100',
+                lg: 'space100',
+              }}
+            >
+              We can&apos;t seem to find what you&apos;re looking for. If you
+              typed in the website address, please check it and try again.
+            </TextBlock>
+            <Button overrides={{marginBlockStart: 'space080'}} href="/">
+              <IconFilledKeyboardArrowLeft /> Back to the homepage
+            </Button>
+          </GridLayoutItem>
+          <Hidden xs sm md>
+            <GridLayoutItem>
+              <Illustration viewBox="0 0 590 879" path="404-hero" />
+            </GridLayoutItem>
+          </Hidden>
+        </GridLayout>
+      </GridLayout>
+    </HeroContainer>
   </Layout>
 );
 
