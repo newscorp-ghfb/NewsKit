c8cd4a5d244972b8c9b11f2f2f8f7b01152b0dce
Vanals <marco91vanali@gmail.com>
2023-01-20 14:57:02 +0000

    feat(PPDSC-000): ok

--- src/theme-checker/themes/storybook-newskit-dark/storybook-newskit-dark-theme.ts
+++ src/theme-checker/themes/storybook-newskit-dark/storybook-newskit-dark-theme.ts
@@ -1,7 +1,5 @@
-import newskitDark from '@newskit-themes/newskit/NK-Dark.json';
 import {createTheme} from '../../../theme';
 
 export const nkDarkThemeFromPublisher = createTheme({
   name: 'nk-dark-theme',
-  overrides: newskitDark,
 });
