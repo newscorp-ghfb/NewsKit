d209ab8535bda921c60347b4e578a5c67dff748b
Ravindren <jravindren@NTS251819MC.local>
2023-01-24 13:30:57 +0530

    feat(PPDSC-2550): form inline link

--- site/pages/components/text-field.tsx
+++ site/pages/components/text-field.tsx
@@ -167,9 +167,7 @@ const formInputPropsFooter = (
     >
       The <InlineCode>name</InlineCode> & <InlineCode>rules</InlineCode> props
       are set on the form input level. To add validation rules, or set the name,{' '}
-      <LinkInline href="/components/form">
-        please refer to the Form component
-      </LinkInline>
+      <LinkInline href="/components/form">see the form component</LinkInline>
     </InlineMessage>
     <InlineMessage
       icon={infoIcon}
