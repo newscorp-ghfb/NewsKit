308dcbed1bd75e114cdcaa5f420dc39a3a0fe490
JohnTParsons <JohnTParsons>
2023-02-07 16:51:45 +0000

    ci(PPDSC-2740): spike staging

--- terraform/s3/dev-newskit/outputs.tf
+++ terraform/s3/dev-newskit/outputs.tf
@@ -1,9 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-output "account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
 output "caller_user" {
   value = data.aws_caller_identity.current.user_id
 }
