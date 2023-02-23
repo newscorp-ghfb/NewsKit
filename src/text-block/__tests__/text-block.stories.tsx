a9f3b894b0a8c6134ad15d78099296b4072df29f
evgenitsn <evgeni.nikolov@news.co.uk>
2023-02-23 09:15:30 +0200

    docs(PPDSC-2821): update TextBlock component storybook scenarios

--- src/text-block/__tests__/text-block.stories.tsx
+++ src/text-block/__tests__/text-block.stories.tsx
@@ -2,111 +2,114 @@ import * as React from 'react';
 import {Story as StoryType} from '@storybook/react';
 import {TextBlock} from '..';
 import {ThemeProvider, CreateThemeArgs} from '../../theme';
-import {
-  StorybookHeading,
-  StorybookSubHeading,
-} from '../../test/storybook-comps';
-import {styled} from '../../utils';
+import {StorybookCase, StorybookPage} from '../../test/storybook-comps';
+import {getColorCssFromTheme, styled} from '../../utils';
 import {createCustomThemeWithBaseThemeSwitch} from '../../test/theme-select-object';
 
 const textBlockCustomThemeObject: CreateThemeArgs = {
   name: 'textblock-custom-theme',
   overrides: {
     stylePresets: {
-      textblockCustom: {
-        base: {
-          color: '{{colors.blue060}}',
-          borderStyle: 'solid',
-          borderWidth: '{{borders.borderWidth010}}',
-        },
-      },
+      textBlockCustom: {base: {color: '{{colors.inkBrand010}}'}},
     },
     typographyPresets: {
-      textblockCustom: {
-        fontFamily: '{{fonts.fontFamily1.fontFamily}}',
-        fontSize: '{{fonts.fontSize030}}',
-        lineHeight: '{{fonts.fontLineHeight030}}',
-        fontWeight: '{{fonts.fontWeight020}}',
-        letterSpacing: '{{fonts.fontLetterSpacing030}}',
-      },
+      textBlockCustom: {fontFamily: '{{fonts.fontFamily020.fontFamily}}'},
     },
   },
 };
 
-const bodyString =
-  'Telling the stories that matter, seeding ideas and stirring emotion. Capturing moments, meaning and magic. Making sense of the world. On the shoulders of giants, in the thick of it, behind the scenes and fighting the good fight. Long form and rapid-fire, pragmatic and poetic, comical and critical. Being at the biggest events with the biggest names noticing the smallest details, and sticking up for the little guy.';
-
-const StyledDiv = styled.div`
-  border: 1px purple dotted;
+const MarginOverridesWrapper = styled.div`
+  border: 1px dashed;
+  ${getColorCssFromTheme('borderColor', 'red060')}
 `;
 
+const TEXT_BLOCK_COLUMNS = '1fr';
+const BODY =
+  'NewsKit provides components, guidelines and standards to enable digital product teams to create high-quality, consistent products quickly. NewsKit is built on modular design principles and backed by best practice guidance for design and development.';
+
 export const StoryTextBlockDefault = () => (
-  <>
-    <StorybookHeading>TextBlock default</StorybookHeading>
-    <TextBlock stylePreset="inkContrast">{bodyString}</TextBlock>
-  </>
+  <StorybookPage columns={TEXT_BLOCK_COLUMNS}>
+    <StorybookCase>
+      <TextBlock stylePreset="inkContrast">{BODY}</TextBlock>
+    </StorybookCase>
+  </StorybookPage>
 );
-StoryTextBlockDefault.storyName = 'default';
+StoryTextBlockDefault.storyName = 'Default';
 
-export const StoryAsDifferentHtmlTag = () => (
-  <>
-    <StorybookHeading>TextBlock</StorybookHeading>
-    <StorybookSubHeading>As h3</StorybookSubHeading>
-    <TextBlock stylePreset="inkContrast" as="h3">
-      {bodyString}
-    </TextBlock>
-
-    <StorybookSubHeading>As div</StorybookSubHeading>
-    <TextBlock stylePreset="inkContrast" as="div">
-      {bodyString}
-    </TextBlock>
-  </>
-);
-StoryAsDifferentHtmlTag.storyName = 'as different html tag';
-
-export const StoryWithOverriddenPresets = () => (
-  <>
-    <StorybookHeading>TextBlock</StorybookHeading>
-    <StorybookSubHeading>With style-preset override</StorybookSubHeading>
-    <TextBlock stylePreset="textblockCustom">{bodyString}</TextBlock>
-    <StorybookSubHeading>With typography-preset override</StorybookSubHeading>
-    <TextBlock
-      typographyPreset="editorialParagraph010"
-      stylePreset="inkContrast"
-    >
-      {bodyString}
-    </TextBlock>
-  </>
-);
-StoryWithOverriddenPresets.storyName = 'with overridden presets';
-
-export const StoryWithLogicalPropsOverrides = () => (
-  <>
-    <StorybookHeading>TextBlock</StorybookHeading>
-    <StorybookSubHeading>With logical padding overrides</StorybookSubHeading>
-    <TextBlock stylePreset="inkContrast" paddingBlock="space040">
-      {bodyString}
-    </TextBlock>
-
-    <StorybookSubHeading>With logical margin overrides</StorybookSubHeading>
-    <StyledDiv>
-      <TextBlock marginBlock="space040" stylePreset="inkContrast">
-        {bodyString}
+export const StoryTextBlockWithDifferentHTMLTag = () => (
+  <StorybookPage columns={TEXT_BLOCK_COLUMNS}>
+    <StorybookCase title="As h3">
+      <TextBlock as="h3" stylePreset="inkContrast">
+        {BODY}
       </TextBlock>
-    </StyledDiv>
-  </>
+    </StorybookCase>
+    <StorybookCase title="As div">
+      <TextBlock as="div" stylePreset="inkContrast">
+        {BODY}
+      </TextBlock>
+    </StorybookCase>
+  </StorybookPage>
 );
-StoryWithLogicalPropsOverrides.storyName = 'with-logical-overrides';
+StoryTextBlockWithDifferentHTMLTag.storyName = 'With different html tag';
+
+export const StoryTextBlockLogicalProps = () => (
+  <StorybookPage columns={TEXT_BLOCK_COLUMNS}>
+    <StorybookCase>
+      <MarginOverridesWrapper>
+        <TextBlock
+          stylePreset="inkContrast"
+          marginBlock="space040"
+          paddingBlock="space040"
+          marginInline="space040"
+          paddingInline="space040"
+        >
+          {BODY}
+        </TextBlock>
+      </MarginOverridesWrapper>
+    </StorybookCase>
+  </StorybookPage>
+);
+StoryTextBlockLogicalProps.storyName = 'Logical props';
+
+export const StoryTextBlockStylingOverrides = () => (
+  <StorybookPage columns={TEXT_BLOCK_COLUMNS}>
+    <StorybookCase>
+      <TextBlock stylePreset="textBlockCustom">{BODY}</TextBlock>
+    </StorybookCase>
+  </StorybookPage>
+);
+StoryTextBlockStylingOverrides.storyName = 'Styling overrides';
+
+export const StoryTextBlockOverrides = () => (
+  <StorybookPage columns={TEXT_BLOCK_COLUMNS}>
+    <StorybookCase title="Typography preset override">
+      <TextBlock typographyPreset="editorialParagraph020">{BODY}</TextBlock>
+    </StorybookCase>
+  </StorybookPage>
+);
+StoryTextBlockOverrides.storyName = 'Overrides';
 
 export default {
-  title: 'Components/text-block',
-  component: () => 'None',
+  title: 'Components/Text block',
+  component: TextBlock,
+  parameters: {
+    nkDocs: {
+      title: 'Text Block',
+      url: 'https://newskit.co.uk/components/text-block',
+      description:
+        'Text block provides a simple way to display text. It supports text cropping, style presets, and typography presets.',
+    },
+  },
   decorators: [
-    (Story: StoryType, context: {globals: {backgrounds: {value: string}}}) => (
+    (
+      Story: StoryType,
+      context: {name: string; globals: {backgrounds: {value: string}}},
+    ) => (
       <ThemeProvider
         theme={createCustomThemeWithBaseThemeSwitch(
           context?.globals?.backgrounds?.value,
           textBlockCustomThemeObject,
+          context?.name,
         )}
       >
         <Story />
