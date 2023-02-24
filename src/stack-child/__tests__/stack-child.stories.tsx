9a9ebe634ad26bbab8e50b9e6fbb043f0417fb9c
Aysha <ayshababur@yahoo.com>
2023-02-24 12:56:53 +0000

    docs(PPDSC-2816): chore/ppdse-2816-added new stack c scenes

--- src/stack-child/__tests__/stack-child.stories.tsx
+++ src/stack-child/__tests__/stack-child.stories.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import {Stack} from '../../stack';
-
-import {styled} from '../../utils/style';
-import {StorybookHeading} from '../../test/storybook-comps';
+import {Story as StoryType} from '@storybook/react';
+import {getColorCssFromTheme, styled} from '../../utils/style';
+import {
+  StorybookCase,
+  StorybookHeading,
+  StorybookPage,
+} from '../../test/storybook-comps';
 import {Tag} from '../../tag/tag';
 import {StackChild} from '..';
 
@@ -15,61 +19,90 @@ const MainContainer = styled.div`
 const Container = styled.div<{
   hasHeight?: boolean;
   hasWidth?: boolean;
+  hasBorder?: boolean;
+  hasBackgroundColor?: boolean;
 }>`
-  border: solid 1px red;
-  height: ${({hasHeight}) => (hasHeight ? '200px' : 'auto')};
-  width: ${({hasWidth}) => (hasWidth ? '400px' : 'auto')};
+  border: ${({hasBorder}) => (hasBorder ? 'dotted 1px red' : 'undefined')};
+  ${({hasBackgroundColor}) =>
+    hasBackgroundColor
+      ? getColorCssFromTheme('backgroundColor', 'blue010')
+      : undefined};
 `;
 
 export default {
-  title: 'Components/stack-child',
-  component: () => 'None',
+  title: 'Components/Stack-Child',
+  component: StackChild,
+  parameters: {},
+  decorators: [(Story: StoryType) => <Story />],
 };
 
-export const StoryStackChildCustomOrder = () => (
-  <MainContainer>
-    <StorybookHeading>StackChild with custom order</StorybookHeading>
-    <Container>
-      <Stack spaceInline="space050" flow="horizontal-top">
-        <StackChild order={3}>
-          <Tag>Child 1</Tag>
-        </StackChild>
-        <StackChild order={1}>
-          <Tag>Child 2</Tag>
-        </StackChild>
-        <StackChild order={1}>
-          <Tag>Child 3</Tag>
-        </StackChild>
-      </Stack>
-    </Container>
-  </MainContainer>
+export const StoryDefault = () => (
+  <StorybookPage columns={{md: 'auto'}}>
+    <StorybookCase title="Default">
+      <Container hasBackgroundColor>
+        <Stack spaceInline="space050" flow="horizontal-top">
+          <StackChild order={3}>
+            <Tag>Child 1</Tag>
+          </StackChild>
+          <StackChild order={1}>
+            <Tag>Child 2</Tag>
+          </StackChild>
+          <StackChild order={1}>
+            <Tag>Child 3</Tag>
+          </StackChild>
+        </Stack>
+      </Container>
+    </StorybookCase>
+  </StorybookPage>
 );
-StoryStackChildCustomOrder.storyName = 'stack-child-custom-order';
+StoryDefault.storyName = 'Default';
+
+export const StoryStackChildCustomOrder = () => (
+  <StorybookPage columns={{md: 'auto'}}>
+    <StorybookCase title="Custom order">
+      <Container hasBackgroundColor>
+        <Stack spaceInline="space050" flow="horizontal-top">
+          <StackChild order={3}>
+            <Tag>Child 1</Tag>
+          </StackChild>
+          <StackChild order={1}>
+            <Tag>Child 2</Tag>
+          </StackChild>
+          <StackChild order={1}>
+            <Tag>Child 3</Tag>
+          </StackChild>
+        </Stack>
+      </Container>
+    </StorybookCase>
+  </StorybookPage>
+);
+StoryStackChildCustomOrder.storyName = 'Custom order';
 
 export const StoryStackChildWithStackList = () => (
-  <MainContainer>
-    <StorybookHeading>StackChild with stack list</StorybookHeading>
-    <Container>
-      <Stack spaceInline="space050" flow="horizontal-top" list>
-        <StackChild order={3}>
-          <Tag>Child 1</Tag>
-        </StackChild>
-        <StackChild order={1}>
-          <Tag>Child 2</Tag>
-        </StackChild>
-        <StackChild order={1}>
-          <Tag>Child 3</Tag>
-        </StackChild>
-      </Stack>
-    </Container>
-  </MainContainer>
+  <StorybookPage columns={{md: 'auto'}}>
+    <StorybookCase title="Stack list">
+      <Container hasBackgroundColor>
+        <Stack spaceInline="space050" flow="horizontal-top" list>
+          <StackChild order={3}>
+            <Tag>Child 1</Tag>
+          </StackChild>
+          <StackChild order={1}>
+            <Tag>Child 2</Tag>
+          </StackChild>
+          <StackChild order={1}>
+            <Tag>Child 3</Tag>
+          </StackChild>
+        </Stack>
+      </Container>
+    </StorybookCase>
+  </StorybookPage>
 );
-StoryStackChildWithStackList.storyName = 'stack-child-with-stack-list';
+StoryStackChildWithStackList.storyName = 'Stack list';
 
 export const StoryStackChildWithLogicalProps = () => (
   <MainContainer>
     <StorybookHeading>StackChild with logical props</StorybookHeading>
-    <Container>
+    <Container hasBorder>
       <Stack spaceInline="space050" flow="horizontal-top">
         <StackChild order={1} paddingInline="30px" paddingBlock="20px">
           <Tag>With padding props</Tag>
@@ -81,4 +114,4 @@ export const StoryStackChildWithLogicalProps = () => (
     </Container>
   </MainContainer>
 );
-StoryStackChildWithLogicalProps.storyName = 'stack-child-with-logical-props';
+StoryStackChildWithLogicalProps.storyName = 'Logical props';
