9f5a8c2cbde0986ae80f543a7f09cab4e71a1522
James Spencer <me@jspenc.com>
2023-02-28 11:43:22 +0000

    build(PPDSC-0000): minor refactor in a11y cy tests

--- cypress/site/functional/accessibility.cy.js
+++ cypress/site/functional/accessibility.cy.js
@@ -13,7 +13,7 @@ describe('Page accessibility', () => {
       cy.mockConsentAndVisit(path);
       cy.wait(1000); // FIX: This accounts for 2mins of the test run time
       cy.injectAxe();
-      if (path === `/theme/foundation/fonts` || path === `/components/card`) {
+      if ([`/theme/foundation/fonts`, `/components/card`].includes(path)) {
         // The typography page is a showcase so we have disabled the heading order rule for this page.
         // More info on the rule here: https://www.w3.org/WAI/tutorials/page-structure/headings/
         // TODO: Need to revisit the below color contrast rule while working on the newskit site remake project
@@ -24,9 +24,11 @@ describe('Page accessibility', () => {
           },
         });
       } else if (
-        path === `/components/tabs` ||
-        path === `/components/banner` ||
-        path === `/theme/presets/typography-presets`
+        [
+          `/components/tabs`,
+          `/components/banner`,
+          `/theme/presets/typography-presets`,
+        ].includes(path)
       ) {
         cy.checkA11y(null, {
           rules: {
