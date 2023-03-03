f408e704e66bb28226001860e7e261fcab47f620
Xin00163 <wangxin00163@yahoo.co.uk>
2023-03-03 15:10:00 +0000

    fix(PPDSC-2802): update layer scenarios

--- src/layer/__tests__/layer.stories.tsx
+++ src/layer/__tests__/layer.stories.tsx
@@ -1,133 +1,202 @@
 import * as React from 'react';
 import {Story as StoryType} from '@storybook/react';
-import {Layer as UnstyledLayer, LayerOrganizer} from '..';
-import {StorybookHeading} from '../../test/storybook-comps';
-import {getColorCssFromTheme, styled} from '../../utils/style';
-
-const Layer = styled(UnstyledLayer)<{color?: string}>`
-  border: 1px dotted;
-  ${({color = 'gray', ...props}) =>
-    getColorCssFromTheme('borderColor', color)(props)};
-`;
+import {Layer, LayerOrganizer} from '..';
+import {
+  getColorCssFromTheme,
+  getTypographyPresetFromTheme,
+  styled,
+} from '../../utils/style';
+import {TextBlock} from '../../text-block';
 
 const Box = styled.div<{
-  width?: string;
   height?: string;
-  border?: string;
-  background?: string;
 }>`
   box-sizing: border-box;
-  border: ${({border}) => `1px solid ${border || 'transparent'}`};
-  background: ${({background}) => background || 'transparent'};
+  border: solid 1px;
+  ${getColorCssFromTheme('borderColor', 'interfaceBrand010')};
   height: ${({height}) => height || '100px'};
-  width: ${({width}) => width || '100px'};
-  padding: 4px;
+  padding: 20px;
 `;
 
-export const StoryBasicLayer = () => (
+const ColorBlock = styled.div<{
+  color?: string;
+  height?: string;
+}>`
+  ${({color}) =>
+    getColorCssFromTheme('backgroundColor', color || 'transparent')}
+  ${getColorCssFromTheme('color', 'inkBase')};
+  ${getTypographyPresetFromTheme('utilityBody020')}
+  height: ${({height}) => height || '80px'};
+  border: solid 1px;
+  ${getColorCssFromTheme('borderColor', 'interfaceBrand010')};
+  margin-bottom: 24px;
+  padding: 20px;
+`;
+
+export const StoryLayerDefault = () => (
   <>
-    <StorybookHeading>Basic layer</StorybookHeading>
-    <Box width="300px" height="200px" border="black">
-      <p>This is is the main content</p>
-      <Layer color="purple040">
-        <Box>Layer outside of main content</Box>
-      </Layer>
-      <Layer color="purple040">
-        <Box>Layer outside of main content</Box>
-      </Layer>
+    <Box>
+      <TextBlock stylePreset="inkBase" typographyPreset="utilityBody020">
+        Basic layer content
+      </TextBlock>
     </Box>
+    <Layer>Layer outside of main content</Layer>
   </>
 );
-StoryBasicLayer.storyName = 'basic layer';
+StoryLayerDefault.storyName = 'Default';
+StoryLayerDefault.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
 export const StoryNestedLayer = () => (
   <>
-    <StorybookHeading>Nested layers</StorybookHeading>
-    <Box width="300px" height="200px" border="black">
-      <p>This is is the main content</p>
-      <Layer color="teal040">
-        <Box width="300px" height="200px">
-          Layer 1
-        </Box>
-        <Layer color="purple040">
-          <Box>Layer 1-1</Box>
-        </Layer>
-        <Layer color="purple040">
-          <Box>Layer 1-2</Box>
-          <Layer color="amber040">
-            <Box>Layer 1-2-1</Box>
-          </Layer>
+    <Box>
+      <TextBlock stylePreset="inkBase" typographyPreset="utilityBody020">
+        Nested Layer
+      </TextBlock>
+    </Box>
+    <Layer>
+      <div>Layer 1</div>
+      <Layer>
+        <div>Layer 1-1</div>
+      </Layer>
+      <Layer>
+        <div>Layer 1-2</div>
+        <Layer>
+          <div>Layer 1-2-1</div>
         </Layer>
       </Layer>
-    </Box>
+    </Layer>
   </>
 );
-StoryNestedLayer.storyName = 'nested layer';
+StoryNestedLayer.storyName = 'Nested layer';
+StoryNestedLayer.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
 export const StoryAppendLayerToCustomParentStory = () => {
   const ref = React.useRef<HTMLDivElement>(null);
   return (
     <>
-      <StorybookHeading>append layer to custom parent</StorybookHeading>
-      <p>Welcome</p>
-      <Box width="300px" height="300px" border="black" ref={ref}>
-        This is the parent div
+      <Box height="350px" ref={ref}>
+        <TextBlock
+          stylePreset="inkBase"
+          typographyPreset="utilityBody020"
+          marginBlockEnd="space040"
+          ref={ref}
+        >
+          This is the parent div
+        </TextBlock>
       </Box>
-      <Layer appendToRef={ref} color="purple040">
-        <Box>Child layer A</Box>
+      <Layer appendToRef={ref}>
+        <ColorBlock color="red010">Child layer A</ColorBlock>
       </Layer>
-      <Layer appendToRef={ref} color="purple040">
-        <Box>Child layer B</Box>
+      <Layer appendToRef={ref}>
+        <ColorBlock color="teal010">Child layer B</ColorBlock>
       </Layer>
     </>
   );
 };
-StoryAppendLayerToCustomParentStory.storyName = 'append-layer-to-custom-parent';
+StoryAppendLayerToCustomParentStory.storyName = 'Append-layer-to-custom-parent';
+StoryAppendLayerToCustomParentStory.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
 export const StoryAppendNestedLayerToCustomParentStory = () => {
   const ref = React.useRef<HTMLDivElement>(null);
 
   return (
     <>
-      <StorybookHeading>Append nested layers to custom parent</StorybookHeading>
-      <p>Welcome</p>
-      <Box width="300px" height="300px" border="black" ref={ref}>
-        This is the parent div
+      <Box height="350px" ref={ref}>
+        <TextBlock
+          stylePreset="inkBase"
+          typographyPreset="utilityBody020"
+          marginBlockEnd="space040"
+        >
+          This is the parent div
+        </TextBlock>
       </Box>
-      <Layer color="teal040">
-        <Box>A and B boxes sit below this box in the code</Box>
-        <Layer appendToRef={ref} color="purple040">
-          <Box>Child layer A</Box>
+      <Layer>
+        <div>A and B boxes sit below this box in the code</div>
+        <Layer appendToRef={ref}>
+          <ColorBlock color="red010">Child layer A</ColorBlock>
         </Layer>
-        <Layer appendToRef={ref} color="purple040">
-          <Box>Child layer B</Box>
+        <Layer appendToRef={ref}>
+          <ColorBlock color="teal010">Child layer B</ColorBlock>
         </Layer>
       </Layer>
     </>
   );
 };
 StoryAppendNestedLayerToCustomParentStory.storyName =
-  'append-nested-layers-to-custom-parent';
+  'Append-nested-layers-to-custom-parent';
+StoryAppendNestedLayerToCustomParentStory.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
-export const StoryTestingLayerPowers = () => {
+export const StoryMultipleLayers = () => {
   const ref = React.useRef<HTMLDivElement>(null);
   return (
     <>
-      <p>Welcome</p>
-      <Box width="300px" height="300px" border="black" ref={ref}>
-        This is the parent div
+      <Box height="350px" ref={ref}>
+        <TextBlock
+          stylePreset="inkBase"
+          typographyPreset="utilityBody020"
+          marginBlockEnd="space040"
+        >
+          This is the parent div
+        </TextBlock>
       </Box>
-
-      <Layer appendToRef={ref} color="purple040">
-        <div className="inside-layer">
+      <Layer appendToRef={ref}>
+        <ColorBlock color="blue010">
           Parent Layer
-          <Layer color="amber040">Child Layer</Layer>
-        </div>
+          <Layer>
+            <ColorBlock>Child Layer</ColorBlock>
+          </Layer>
+        </ColorBlock>
       </Layer>
     </>
   );
 };
-StoryTestingLayerPowers.storyName = 'testing-layer-powers';
+StoryMultipleLayers.storyName = 'Multiple-layers';
+StoryMultipleLayers.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
 const Wrapper = styled.div<{
   width?: string;
@@ -152,27 +221,35 @@ const Wrapper = styled.div<{
 
 export const StoryBasicLayerOrganizer = () => (
   <>
-    <StorybookHeading>basic layer organizer</StorybookHeading>
-    <Box width="300px" height="200px" border="black">
-      <p>This is is the main content</p>
-      <Layer color="purple040">
-        <Box>Layer outside of main content</Box>
-      </Layer>
-      <LayerOrganizer>
-        <Layer color="purple040">
-          <Box>Layer outside of main content</Box>
-        </Layer>
-      </LayerOrganizer>
+    <Box>
+      <TextBlock stylePreset="inkBase" typographyPreset="utilityBody020">
+        Basic layer content
+      </TextBlock>
     </Box>
+    <Layer>Layer outside of layer organizer sibling</Layer>
+    <LayerOrganizer>
+      <Layer>Layer inside layer organizer</Layer>
+    </LayerOrganizer>
   </>
 );
-StoryBasicLayerOrganizer.storyName = 'basic layer organizer';
+StoryBasicLayerOrganizer.storyName = 'Basic layer organizer';
+StoryBasicLayerOrganizer.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
 export const StoryBasicLayerOrganizerZindex = () => (
   <>
-    <StorybookHeading>basic layer organizer zIndex</StorybookHeading>
-    <Box width="300px" height="200px" border="black">
-      This is is the main content
+    <Box height="350px">
+      <TextBlock stylePreset="inkBase" typographyPreset="utilityBody020">
+        Basic layer content
+      </TextBlock>
     </Box>
     <Layer>
       <Wrapper offset="20%" color="amber040">
@@ -198,13 +275,24 @@ export const StoryBasicLayerOrganizerZindex = () => (
     </Layer>
   </>
 );
-StoryBasicLayerOrganizerZindex.storyName = 'basic layer organizer zindex';
+StoryBasicLayerOrganizerZindex.storyName = 'Basic layer organizer zindex';
+StoryBasicLayerOrganizerZindex.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
 export const StoryNestedLayerOrganizerZindex = () => (
   <>
-    <StorybookHeading>nested layer organizer zIndex</StorybookHeading>
-    <Box width="300px" height="200px" border="black">
-      This is is the main content
+    <Box height="350px">
+      <TextBlock stylePreset="inkBase" typographyPreset="utilityBody020">
+        Basic layer content
+      </TextBlock>
     </Box>
     <Layer>
       <Wrapper offset="20%" color="amber040">
@@ -232,7 +320,17 @@ export const StoryNestedLayerOrganizerZindex = () => (
     </Layer>
   </>
 );
-StoryNestedLayerOrganizerZindex.storyName = 'nested layer organizer zindex';
+StoryNestedLayerOrganizerZindex.storyName = 'Nested layer organizer zindex';
+StoryNestedLayerOrganizerZindex.parameters = {
+  previewTabs: {
+    'storybook/canvas/panel': {index: -1},
+    'storybook/docs/panel': {hidden: true},
+  },
+  viewMode: 'story',
+  docs: {
+    page: null,
+  },
+};
 
 export default {
   title: 'Components/Layer',
