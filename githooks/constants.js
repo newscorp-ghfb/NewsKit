3d62ff0bbe54c1f4b099f67c1b5b76e7c452777f
Stoyan Delev <stoyan.delev@news.co.uk>
2023-03-24 07:10:13 +0200

    fix(745): refactor date-time component

--- githooks/constants.js
+++ githooks/constants.js
@@ -1,7 +1,7 @@
 const chalk = require('chalk');
 
 const TYPES = ['docs', 'feat', 'fix', 'refactor', 'revert', 'upkeep', 'spike'];
-const SCOPE_REGEX = /^[A-Z]{2,5}-\d{1,5}$/;
+const SCOPE_REGEX = /^\d{1,5}$/;
 const PLAIN_PREFIX_ERROR_MSG =
   "The commit message should be in the format 'type - fix/feat/etc(ticket-number): message'";
 const FORMATTED_PREFIX_ERROR_MSG = `The commit message should be in the format ${chalk.bold(
