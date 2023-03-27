353f8d2bae2cad842fbfb8297c0fd9ca8b374682
Stoyan Delev <stoyan.delev@news.co.uk>
2023-03-27 14:18:13 +0300

    docs(XXXX-0000): closes 719

--- src/typography/__tests__/presets-cropping.stories.tsx
+++ src/typography/__tests__/presets-cropping.stories.tsx
@@ -1,177 +1,84 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
 import {TextBlock} from '../../text-block';
-import {Cell, Grid} from '../../grid';
-import {useTheme} from '../../theme';
-import {
-  StorybookHeading,
-  StorybookSubHeading,
-} from '../../test/storybook-comps';
+import {TypographyPreset, useTheme} from '../../theme';
+import {StorybookPage, StorybookCase} from '../../test/storybook-comps';
+import {getColorCssFromTheme} from '../../utils';
 
 export default {
-  title: 'Components/typography/presets-cropping',
+  title: 'Components/Text cropping',
   component: () => 'None',
+  parameters: {
+    nkDocs: {
+      title: 'Text Cropping',
+      url: 'https://newskit.co.uk/theme/foundation/fonts/#textcrop',
+      description: '',
+    },
+  },
 };
 
 const StyledTextBlock = styled(TextBlock)`
-  background-color: tomato;
+  ${getColorCssFromTheme('backgroundColor', 'blue010')}
 `;
 
-const renderTextBlockWithPreset = (presetList: Array<string>) =>
-  presetList.map((preset: string) => (
-    <>
-      <StyledTextBlock typographyPreset={preset}>T{preset}</StyledTextBlock>
-      <br />
-    </>
+const renderTextBlockWithPreset = (
+  presetPrefix: string,
+  typographyPresets: Record<string, TypographyPreset>,
+) => {
+  const presetList = Object.keys(typographyPresets).filter(preset =>
+    preset.startsWith(presetPrefix),
+  );
+
+  return presetList.map((preset: string) => (
+    <StorybookCase title={` ${preset}`}>
+      <StyledTextBlock typographyPreset={preset}>
+        The quick brown fox
+      </StyledTextBlock>
+    </StorybookCase>
   ));
-
-export const EditorialCropping1 = () => {
-  const theme = useTheme();
-
-  const editorialDisplayPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('editorialDisplay'));
-
-  const editorialLabelPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('editorialLabel'));
-
-  const editorialHeadlinePresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('editorialHeadline'));
-
-  return (
-    <>
-      <Grid>
-        <Cell xs={6}>
-          <StorybookHeading>Presets cropping</StorybookHeading>
-          <StorybookSubHeading>Editorial</StorybookSubHeading>
-          {renderTextBlockWithPreset(editorialDisplayPresetList)}
-          <br />
-          {renderTextBlockWithPreset(editorialLabelPresetList)}
-        </Cell>
-        <Cell xs={6}>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          {renderTextBlockWithPreset(editorialHeadlinePresetList)}
-        </Cell>
-      </Grid>
-    </>
-  );
 };
 
-EditorialCropping1.storyName = 'editorialCropping1';
+export const EditorialCropping = () => {
+  const {typographyPresets} = useTheme();
 
-export const EditorialCropping2 = () => {
-  const theme = useTheme();
-
-  const editorialSubHeadlinePresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('editorialSubheadline'));
-
-  const editorialParagraphPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('editorialParagraph'));
-
-  const editorialQuotePresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('editorialQuote'));
-
-  const editorialCaptionPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('editorialCaption'));
+  const presetsKeys = [
+    'editorialDisplay',
+    'editorialHeadline',
+    'editorialSubheadline',
+    'editorialParagraph',
+    'editorialQuote',
+    'editorialCaption',
+    'editorialLabel',
+  ];
 
   return (
-    <>
-      <Grid>
-        <Cell xs={6}>
-          <StorybookHeading>Presets cropping</StorybookHeading>
-          <StorybookSubHeading>Editorial</StorybookSubHeading>
-          <br />
-          {renderTextBlockWithPreset(editorialSubHeadlinePresetList)}
-          <br />
-          {renderTextBlockWithPreset(editorialParagraphPresetList)}
-          <br />
-          {renderTextBlockWithPreset(editorialQuotePresetList)}
-        </Cell>
-        <Cell xs={6}>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          {renderTextBlockWithPreset(editorialCaptionPresetList)}
-        </Cell>
-      </Grid>
-    </>
+    <StorybookPage columns="1fr">
+      {presetsKeys.map(presetKey =>
+        renderTextBlockWithPreset(presetKey, typographyPresets),
+      )}
+    </StorybookPage>
   );
 };
-EditorialCropping2.storyName = 'editorialCropping2';
+EditorialCropping.storyName = 'Editorial cropping';
 
 export const UtilityCropping = () => {
-  const theme = useTheme();
+  const {typographyPresets} = useTheme();
 
-  const utilityHeadingPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('utilityHeading'));
-
-  const utilitySubHeadingPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('utilitySubheading'));
-
-  const utilityBodyPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('utilityBody'));
-
-  const utilityLabelPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('utilityLabel'));
-
-  const utilityMetaPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('utilityMeta'));
-
-  const utilityButtonPresetList = Object.keys(
-    theme.typographyPresets,
-  ).filter(preset => preset.startsWith('utilityButton'));
+  const presetsKeys = [
+    'utilityHeading',
+    'utilitySubheading',
+    'utilityBody',
+    'utilityLabel',
+    'utilityButton',
+    'utilityMeta',
+  ];
 
   return (
-    <>
-      <Grid>
-        <Cell xs={6}>
-          <StorybookHeading>Presets cropping</StorybookHeading>
-          <StorybookSubHeading>Utility</StorybookSubHeading>
-          {renderTextBlockWithPreset(utilityHeadingPresetList)}
-          <br />
-          {renderTextBlockWithPreset(utilitySubHeadingPresetList)}
-        </Cell>
-        <Cell xs={6}>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          {renderTextBlockWithPreset(utilityBodyPresetList)}
-          <br />
-          {renderTextBlockWithPreset(utilityLabelPresetList)}
-          <br />
-          {renderTextBlockWithPreset(utilityMetaPresetList)}
-          <br />
-          {renderTextBlockWithPreset(utilityButtonPresetList)}
-        </Cell>
-      </Grid>
-    </>
+    <StorybookPage columns="1fr">
+      {presetsKeys.map(presetKey =>
+        renderTextBlockWithPreset(presetKey, typographyPresets),
+      )}
+    </StorybookPage>
   );
 };
-
-UtilityCropping.storyName = 'utility';
+UtilityCropping.storyName = 'Utility cropping ';
