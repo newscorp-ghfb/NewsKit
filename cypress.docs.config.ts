72cd996a7846117a14b97612d5633da75aa0e0d2
James Spencer <me@jspenc.com>
2023-03-31 16:59:37 +0100

    build(PPDSC-0000): attempt to run via cypress-cloud

--- cypress.docs.config.ts
+++ cypress.docs.config.ts
@@ -1,17 +1,19 @@
 // cypress config must be in project root: https://github.com/cypress-io/cypress/issues/22689
 
 import {defineConfig} from 'cypress';
-
-const {readAllFilesInDir} = require('./cypress/plugins/files');
+import {cloudPlugin} from 'cypress-cloud/plugin';
+import {readAllFilesInDir} from './cypress/plugins/files';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on) {
+    setupNodeEvents(on, config) {
       on('task', {
         readAllFilesInDir(dir) {
           return readAllFilesInDir(dir);
         },
       });
+
+      return cloudPlugin(on, config);
     },
     baseUrl: 'http://localhost:8081',
     specPattern: 'cypress/site/functional/**/*.cy.{js,jsx,ts,tsx}',
