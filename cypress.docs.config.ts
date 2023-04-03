356e7dba6ec57e8b9b5ba3a8231dc5054057adaa
James Spencer <me@jspenc.com>
2023-04-03 11:47:28 +0100

    build(PPDSC-0000): Revert "build(PPDSC-0000): larger instance size"
    This reverts commit a68affed94e15b0af94aa020c3c3f9563a26184e.

--- cypress.docs.config.ts
+++ cypress.docs.config.ts
@@ -1,19 +1,17 @@
 // cypress config must be in project root: https://github.com/cypress-io/cypress/issues/22689
 
 import {defineConfig} from 'cypress';
-import {cloudPlugin} from 'cypress-cloud/plugin';
-import {readAllFilesInDir} from './cypress/plugins/files';
+
+const {readAllFilesInDir} = require('./cypress/plugins/files');
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on) {
       on('task', {
         readAllFilesInDir(dir) {
           return readAllFilesInDir(dir);
         },
       });
-
-      return cloudPlugin(on, config);
     },
     baseUrl: 'http://localhost:8081',
     specPattern: 'cypress/site/functional/**/*.cy.{js,jsx,ts,tsx}',
