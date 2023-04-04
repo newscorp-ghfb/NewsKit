cda32f28f6730e02671d0cfe5a1f75191b4848b1
Stoyan Delev <stoyan.delev@news.co.uk>
2023-04-04 12:26:57 +0300

    docs(752): reverse no filter svg

--- scripts/remove-filters-svg.js
+++ scripts/remove-filters-svg.js
@@ -17,7 +17,25 @@ async function* getFiles(dir) {
 }
 
 function updateFile(filePath) {
+  const regexFilter = new RegExp('filter="url(.*)"');
+  // const fileContent = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'});
+  // const contentWithoutFilters = fileContent.replaceAll(regexFilter, '');
+
+  // const regexDefs = new RegExp(/<defs>.*<\/defs>/, 'gis');
+  // const contentWithoutDefs = contentWithoutFilters.replaceAll(regexDefs, '');
+
+  const newPath = filePath.replace(
+    '/illustrations-original/',
+    '/illustrations/',
+  );
+
   const fileContent = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'});
+
+  // console.log(!regexFilter.test(fileContent), filePath);
+  if (!regexFilter.test(fileContent)) {
+    fs.writeFileSync(newPath, fileContent, {encoding: 'utf8'});
+    return;
+  }
 
   const $ = cheerio.load(fileContent);
 
@@ -31,14 +49,17 @@ function updateFile(filePath) {
 
   const cleanContent = $('svg').parent().html();
 
-  const newPath = filePath.replace(
-    '/illustrations-original/',
-    '/illustrations/',
-  );
   fs.writeFileSync(newPath, cleanContent, {encoding: 'utf8'});
 }
 
 (async () => {
+  // const file1 =
+  //   '/Users/sdelev/projects/newskit/newskit/site/public/static/illustrations-original/about/contact-us-hero-illustration.svg';
+  // updateFile(file1);
+  // const file2 =
+  //   '/Users/sdelev/projects/newskit/newskit/site/public/static/illustrations-original/about/why-use-newskit-hero-illustration.svg';
+  // updateFile(file2);
+
   for await (const filePath of getFiles(
     './site/public/static/illustrations-original/',
   )) {
