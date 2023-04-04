e440ba07e04478e7c4e44d4245da10fce1d0520b
Stoyan Delev <stoyan.delev@news.co.uk>
2023-04-04 12:10:41 +0300

    docs(752): clean svgs

--- scripts/remove-filters-svg.js
+++ scripts/remove-filters-svg.js
@@ -2,6 +2,7 @@
 const {resolve} = require('path');
 const {readdir} = require('fs').promises;
 const fs = require('fs');
+const cheerio = require('cheerio');
 
 async function* getFiles(dir) {
   const dirents = await readdir(dir, {withFileTypes: true});
@@ -16,21 +17,47 @@ async function* getFiles(dir) {
 }
 
 function updateFile(filePath) {
-  const regex = new RegExp('filter="url(.*)"', 'gi');
+  // const regexFilter = new RegExp(' filter="url(.*)"', 'gi');
+  // const fileContent = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'});
+  // const contentWithoutFilters = fileContent.replaceAll(regexFilter, '');
+
+  // const regexDefs = new RegExp(/<defs>.*<\/defs>/, 'gis');
+  // const contentWithoutDefs = contentWithoutFilters.replaceAll(regexDefs, '');
+
   const fileContent = fs.readFileSync(filePath, {encoding: 'utf8', flag: 'r'});
-  const newContent = fileContent.replaceAll(regex, '');
+
+  const $ = cheerio.load(fileContent);
+
+  // remove filter definitions
+  $('filter').remove();
+
+  // remove filter with url
+  $('[filter*="url"]').each((i, el) => {
+    $(el).removeAttr('filter');
+  });
+
+  const cleanContent = $('svg').parent().html();
+
   const newPath = filePath.replace(
     '/illustrations-original/',
     '/illustrations/',
   );
-  fs.writeFileSync(newPath, newContent, {encoding: 'utf8'});
+  fs.writeFileSync(newPath, cleanContent, {encoding: 'utf8'});
 }
 
 (async () => {
+  // const file1 =
+  //   '/Users/sdelev/projects/newskit/newskit/site/public/static/illustrations-original/about/contact-us-hero-illustration.svg';
+  // updateFile(file1);
+  // const file2 =
+  //   '/Users/sdelev/projects/newskit/newskit/site/public/static/illustrations-original/about/why-use-newskit-hero-illustration.svg';
+  // updateFile(file2);
+
   for await (const filePath of getFiles(
-    // './site/public/static/illustrations/about/',
     './site/public/static/illustrations-original/',
   )) {
-    updateFile(filePath);
+    if (filePath.endsWith('.svg')) {
+      updateFile(filePath);
+    }
   }
 })();
