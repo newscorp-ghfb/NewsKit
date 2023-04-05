c907e41d1f9cf313bc6889347514acdf834d43ee
Vanals <marco91vanali@gmail.com>
2023-04-05 13:41:32 +0100

    fix(803): enhanced branch-name-to-url

--- scripts/branch-name-to-url.js
+++ scripts/branch-name-to-url.js
@@ -13,17 +13,23 @@ module.exports.branchNameToUrl = branchName => {
 
   const removeNonAz09Dash = /[^a-zA-Z0-9 -]/;
 
-  const sitePath = branchName
-    .split('/')[1]
-    .toLowerCase()
-    .trim()
-    .replace(removeNonAz09Dash);
+  const sitePath = () => {
+    if (branchName.includes('snyk-upgrade')) {
+      return branchName;
+    }
+    return branchName
+      .split('/')[1]
+      .toLowerCase()
+      .trim()
+      .replace(removeNonAz09Dash);
+  };
+  const sitePathValue = sitePath();
 
-  if (!sitePath) {
+  if (!sitePathValue) {
     process.stderr.write(`Could not parse branch:${branchName}`);
     process.exitCode = 1;
     return '';
   }
 
-  return sitePath;
+  return sitePathValue;
 };
