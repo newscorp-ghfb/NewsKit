6513b7e140bec5fadfdbffa9eb874e611e1d2305
Stoyan Delev <stoyan.delev@news.co.uk>
2023-04-06 07:34:59 +0300

    docs(752): update percy config to storybook params

--- percy.config.js
+++ percy.config.js
@@ -1,13 +1,6 @@
-const SKIP_GITHUB_CHECK = process.env.SKIP_PERCY_CHECK === 'true';
-
-console.log('whaaaaat is this----------------------------------');
-console.log({SKIP_GITHUB_CHECK});
 module.exports = {
   version: 2,
   snapshot: {
     widths: [375, 1280],
-  },
-  storybook: {
-    ...(SKIP_GITHUB_CHECK ? {include: 'skip-percy-tests'} : {}),
   },
 };
