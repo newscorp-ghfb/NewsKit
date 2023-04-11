92bf709cab8ed494a60109f0d12e44218f85318e
Stoyan Delev <mutebg@users.noreply.github.com>
2023-04-11 15:10:13 +0300

    feat:(726) card composable (#802)
    * spike(PPDSC-2577): card composable
    
    * spike(PPDSC-2577): add card
    
    * spike(PPDSC-2577): play around
    
    * spike(PPDSC-2577): add areas
    
    * spike(PPDSC-2577): add areas
    
    * chore(PPDSC-2747): stories added
    
    * chore(PPDSC-2747): fix build
    
    * chore(PPDSC-2747): img component
    
    * chore(PPDSC-2747): typo
    
    * chore(PPDSC-2747): chore(PPDSC-2754: storybook, order to overrides
    
    * chore(PPDSC-2747): update style overrides
    
    * chore(PPDSC-2747): chore/ppdse-2747-added areas & variations
    
    * chore(PPDSC-2747): grid for stories with multiple cases
    
    * chore(PPDSC-2747): chore/ppdse-2747-variations-inset-done
    
    * chore(PPDSC-2747): chore/ppdse-2747-added areas on stories
    
    * chore(PPDSC-2747): chore/ppdse-2747-layout and span done
    
    * chore(PPDSC-2747): combine overrides stories
    
    * chore(PPDSC-2747): chore/ppdse-2747-switched stories
    
    * chore(PPDSC-2747): chore/ppdse-2747-replaced text
    
    * chore(PPDSC-2747): chore/ppdse-2747-fixed box shadow
    
    * chore(PPDSC-2747): chore/ppdse-2747-testing max width
    
    * chore(PPDSC-2747): chore/ppdse-2747-fixed layout columnns
    
    * chore(PPDSC-2747): chore/ppdse-2747-switched columnn colors
    
    * chore(PPDSC-2747): chore/ppdse-2747-added default vis
    
    * chore(PPDSC-2747): requested changes to overrides story
    
    * chore(PPDSC-2747): chore/ppdse-2747-fixed focus visible
    
    * chore(PPDSC-2747): simplify default story
    
    * chore(PPDSC-2747): correct responsive card story
    
    * chore(PPDSC-2747): update snapshots
    
    * chore(PPDSC-2747): chore/ppdse-2747-removed preset
    
    * chore(PPDSC-2747): chore/ppdse-2747-removed preset-again
    
    * chore(PPDSC-2747): chore/ppdse-2747-updated snaps
    
    * chore(PPDSC-2747): chore/ppdse-2747-updated complier
    
    * chore(PPDSC-2747): chore/ppdse-2747-updated
    
    * chore(PPDSC-2747): added types
    
    * chore(PPDSC-2747): rename to card-composable
    
    * chore(PPDSC-2747): fix Default layout
    
    * chore(PPDSC-2747): update snapshots
    
    * chore(PPDSC-2747): fixed type
    
    * chore(PPDSC-2747): added space on headline
    
    * chore(PPDSC-2747): added space on flag
    
    * chore(PPDSC-2747): fixed height thing
    
    * chore(PPDSC-2747): added space
    
    * chore(PPDSC-2747): remove cardcoverlink experiment ref
    
    * chore(PPDSC-2747): adjust link focus rectangle sizes
    
    * chore(PPDSC-2747): remove spacing overrides
    
    * chore(PPDSC-2747): update button
    
    * feat(726): refactor
    
    * feat(726): update stories
    
    * feat(726): add ref and theme
    
    * feat(726): update stories
    
    * feat(726): fix images
    
    * feat(726): add unit tests
    
    * feat(726): change to use LinkStandalone
    
    * feat(726): remove headlineLink
    
    * feat(726): fix tests
    
    * feat(726): address design feedback
    
    * feat(726): fix docs snapshots
    
    * feat(726): add defaults to all comps
    
    * feat(726): address design feedback
    
    * feat(726): add defaults to card-link
    
    * feat(726): update snapshots
    
    * feat(726): add transition preset
    
    * feat(726): address feedback
    
    ---------
    
    Co-authored-by: mikef <mstuartflood@gmail.com>
    Co-authored-by: JohnTParsons <JohnTParsons>
    Co-authored-by: Aysha <ayshababur@yahoo.com>

---
+++ src/card-composable/types.ts
@@ -0,0 +1,32 @@
+import {ReactNode} from 'react';
+import {GridLayoutProps} from '../grid-layout/types';
+import {ImageProps} from '../image';
+import {MQ} from '../utils';
+import {LinkProps} from '../link';
+import {TransitionToken} from '../theme';
+
+export type StylableGridLayout = GridLayoutProps & {
+  overrides?: {
+    stylePreset?: MQ<string>;
+    transitionPreset?: TransitionToken | TransitionToken[];
+  };
+};
+
+export type ComponentWithOverrides = {
+  overrides?: object;
+};
+
+export type CardComposableProps = StylableGridLayout;
+
+export type CardMediaProps = {
+  media?: ImageProps;
+  children?: ReactNode;
+};
+
+export type CardContentProps = StylableGridLayout;
+
+export type CardActionsProps = StylableGridLayout;
+
+export type CardLinkProps = LinkProps & {
+  expand?: boolean;
+};
