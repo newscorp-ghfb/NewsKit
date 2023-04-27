86ff034256e20a0818561d975c4ab9f7e36b501e
Mike Flood <mstuartflood@gmail.com>
2023-04-27 17:50:57 +0100

    docs(811): remove double focus

--- site/components/text-crop-calculator/components/font-selector/index.tsx
+++ site/components/text-crop-calculator/components/font-selector/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import {Tab, Tabs} from 'newskit';
+import {Tab, Tabs, styled} from 'newskit';
 import GoogleFontSelector from './google-font-selector';
 import SystemFontSelector from './system-font-selector';
 import UrlFontSelector from './url-font-selector';
 import FileUploadSelector from './file-upload-selector';
 import FontInjector from './font-injector';
 
+const StyledTabs = styled(Tabs)`
+  div[role='tabpanel']:focus-within {
+    outline: none;
+  }
+`;
+
 const FontSelector = () => (
   <>
-    <Tabs>
+    <StyledTabs>
       <Tab label="Google Fonts" key="google-fonts">
         <GoogleFontSelector />
       </Tab>
@@ -21,7 +27,7 @@ const FontSelector = () => (
       <Tab label="Upload" key="upload">
         <FileUploadSelector />
       </Tab>
-    </Tabs>
+    </StyledTabs>
     <FontInjector />
   </>
 );
