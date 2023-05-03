554390025f132cc553e0d14c030f064891005703
mikef <mstuartflood@gmail.com>
2023-05-03 14:22:05 +0100

    fix(821): update docs

--- site/pages/components/pagination.tsx
+++ site/pages/components/pagination.tsx
@@ -435,6 +435,12 @@ const PaginationComponent = (layoutProps: LayoutProps) => (
               description:
                 'If provided, overrides the stylePreset of the pagination',
             },
+            {
+              attribute: 'spaceBetween',
+              type: 'MQ<string>',
+              description:
+                'If provided, this overrides the space applied between pagination items.',
+            },
             ...(commonLogicalProps() as OverridesRowsProps[]),
           ],
         },
