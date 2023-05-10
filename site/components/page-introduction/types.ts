0dcfd336a7e807fad6d9cca8b78b48683fbd923c
John Parsons <JohnTParsons@users.noreply.github.com>
2023-05-10 10:48:37 +0100

    docs(812): add accessibility statement to docs site (#879)
    * docs(812): add accessibility statement to docs site
    
    * docs(874): interface  status colours show negative as positive
    
    ---------
    
    Co-authored-by: JohnTParsons <JohnTParsons>

--- site/components/page-introduction/types.ts
+++ site/components/page-introduction/types.ts
@@ -1,6 +1,8 @@
 import {ImageProps} from 'newskit';
 
 export interface PageIntroductionProps {
+  nameAs?: 'h1' | 'h2';
+  introductionAs?: 'h1' | 'h2' | 'h3';
   type?: string;
   name: string;
   introduction: React.ReactNode;
