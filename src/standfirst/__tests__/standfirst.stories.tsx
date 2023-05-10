ab1540962102bf263772171ea1ee493b1d48129f
Ravindren <jravindren@NTS251819MC.local>
2023-05-11 00:26:04 +0530

    docs(723): removed react fragements

--- src/standfirst/__tests__/standfirst.stories.tsx
+++ src/standfirst/__tests__/standfirst.stories.tsx
@@ -43,66 +43,60 @@ export const StoryWithAsSpan = () => (
 StoryWithAsSpan.storyName = 'Standfirst as span';
 
 export const StoryLogicalProps = () => (
-  <>
-    <StorybookPage columns="1fr">
-      <StorybookCase>
-        <MarginOverridesWrapper>
-          <Standfirst
-            overrides={{
-              styledText: {
-                paddingInline: '20px',
-                paddingBlock: '10px',
-              },
-            }}
-          >
-            {CONTENT}
-          </Standfirst>
-        </MarginOverridesWrapper>
-      </StorybookCase>
-    </StorybookPage>
-  </>
-);
-StoryLogicalProps.storyName = 'Logical props';
-
-export const StoryWithStylingOverrides = () => (
-  <>
-    <StorybookPage columns="1fr">
-      <StorybookCase>
+  <StorybookPage columns="1fr">
+    <StorybookCase>
+      <MarginOverridesWrapper>
         <Standfirst
           overrides={{
             styledText: {
-              stylePreset: 'inkBrand010',
+              paddingInline: '20px',
+              paddingBlock: '10px',
             },
           }}
         >
           {CONTENT}
         </Standfirst>
-      </StorybookCase>
-    </StorybookPage>
-  </>
+      </MarginOverridesWrapper>
+    </StorybookCase>
+  </StorybookPage>
+);
+StoryLogicalProps.storyName = 'Logical props';
+
+export const StoryWithStylingOverrides = () => (
+  <StorybookPage columns="1fr">
+    <StorybookCase>
+      <Standfirst
+        overrides={{
+          styledText: {
+            stylePreset: 'inkBrand010',
+          },
+        }}
+      >
+        {CONTENT}
+      </Standfirst>
+    </StorybookCase>
+  </StorybookPage>
 );
 StoryWithStylingOverrides.storyName = 'Styling overrides';
 
 export const StoryWithOverrides = () => (
-  <>
-    <StorybookPage columns="1fr">
-      <StorybookCase title="Typography preset override">
-        <Standfirst
-          as="span"
-          overrides={{
-            styledText: {
-              typographyPreset: 'utilitySubheading050',
-            },
-          }}
-        >
-          NewsKit provides components, guidelines and standards to enable
-          digital product teams to create high-quality, consistent products
-          quickly. NewsKit is built on modular design principles and backed by
-          best practice guidance for design and development.
-        </Standfirst>
-      </StorybookCase>
-    </StorybookPage>
-  </>
+  <StorybookPage columns="1fr">
+    <StorybookCase title="Typography preset override">
+      <Standfirst
+        as="span"
+        overrides={{
+          styledText: {
+            typographyPreset: 'utilitySubheading050',
+          },
+        }}
+      >
+        NewsKit provides components, guidelines and standards to enable digital
+        product teams to create high-quality, consistent products quickly.
+        NewsKit is built on modular design principles and backed by best
+        practice guidance for design and development.
+      </Standfirst>
+    </StorybookCase>
+  </StorybookPage>
 );
 StoryWithOverrides.storyName = 'Overrides';
 
