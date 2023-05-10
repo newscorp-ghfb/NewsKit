71f1936e55614e403ee52300c008a00d09e54058
JohnTParsons <JohnTParsons>
2023-05-10 09:25:45 +0100

    docs(812): revert new tests in header-navigation.cy.js

--- cypress/site/functional/header-navigation.cy.js
+++ cypress/site/functional/header-navigation.cy.js
@@ -14,14 +14,6 @@ describe('Documentation Site - header-navigation component', () => {
       cy.mockConsentAndVisit('/about/contact-us');
       cy.get(indicatorID).should('have.attr', 'aria-current');
     });
-    it('pages should have header links', () => {
-      cy.mockConsentAndVisit('/');
-      cy.contains('a', 'About').should('exist');
-      cy.contains('a', 'Guides').should('exist');
-      cy.contains('a', 'Theme').should('exist');
-      cy.contains('a', 'Components').should('exist');
-      cy.contains('a', 'Patterns').should('exist');
-    });
   });
 
   describe('Mobile view', () => {
@@ -35,16 +27,6 @@ describe('Documentation Site - header-navigation component', () => {
 
       cy.get(hamburgerIconTestID).first().click();
       cy.get(sidebarTestID).should('exist').and('not.be.visible');
-    });
-    it('pages should have header links', () => {
-      cy.mockConsentAndVisit('/about/contact-us');
-      // For reliability, wait for sidebar to be visible before doing header tests
-      cy.get(sidebarTestID).should('be.visible');
-      cy.contains('a', 'About').should('exist');
-      cy.contains('a', 'Guides').should('exist');
-      cy.contains('a', 'Theme').should('exist');
-      cy.contains('a', 'Components').should('exist');
-      cy.contains('a', 'Patterns').should('exist');
     });
   });
 });
