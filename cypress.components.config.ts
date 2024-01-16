ccba24b99ecdca88fcb10c398ecd027e977610dd
kamenboev <kamen.boev@news.co.uk>
2024-01-16 16:10:49 +0200

    fix(WWP-3314): reduce timeout

--- cypress.components.config.ts
+++ cypress.components.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
     specPattern: 'cypress/components/**/*.cy.{js,jsx,ts,tsx}',
-    defaultCommandTimeout: 40000,
+    defaultCommandTimeout: 10000,
   },
 });
