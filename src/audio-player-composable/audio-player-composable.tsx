aab8e3085b5b88f3561c36e72010147e3fa3eb86
lukejsikuade <luke.sikuade@thetimes.co.uk>
2024-03-07 14:43:56 +0000

    fix(921): additional useEffect for resetting the time

--- src/audio-player-composable/audio-player-composable.tsx
+++ src/audio-player-composable/audio-player-composable.tsx
@@ -74,15 +74,19 @@ export const AudioPlayerComposable = ({
   });
 
   useEffect(() => {
+    setCurrentTime(0);
+    setDisplayDuration(0);
+  }, [src]);
+
+  useEffect(() => {
     // On render onTimeUpdate will be fired and initialTime will be set as a value for currentTime state.
     // I can't set this one to the setCurrentTime state directly as the audioElement time
     // will still be 0, currentTime will be overridden to 0 and the audio will start from 0
+
     if (audioRef && audioRef.current && duration) {
       audioRef.current.currentTime = initialTime;
     }
-    setCurrentTime(0);
-    setDisplayDuration(0);
-  }, []);
+  }, [duration, initialTime]);
 
   const {
     audioEvents,
